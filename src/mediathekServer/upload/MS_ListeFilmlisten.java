ca715fee5bf5ea740caff8be831ac604f86943da
W.Xaver <w.xaver@googlemail.com>
2013-02-06 19:06:43 +0100

    Aufbau der Liste FilmlistenUrls

--- src/mediathekServer/upload/MS_ListeFilmlisten.java
+++ src/mediathekServer/upload/MS_ListeFilmlisten.java
@@ -20,31 +20,16 @@
 package mediathekServer.upload;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.Iterator;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamWriter;
 import mediathek.controller.filmeLaden.importieren.DatenFilmlistenServer;
 import mediathek.controller.filmeLaden.importieren.DatenUrlFilmliste;
 import mediathek.controller.filmeLaden.importieren.FilmlistenSuchen;
 import mediathek.controller.filmeLaden.importieren.ListeDownloadUrlsFilmlisten;
-import mediathek.tool.Konstanten;
 import mediathekServer.tool.MS_Daten;
 import mediathekServer.tool.MS_Konstanten;
 import mediathekServer.tool.MS_Log;
-import mediathekServer.tool.MS_XmlSchreiben;
 
 public class MS_ListeFilmlisten {
-
-    private static XMLOutputFactory outFactory;
-    private static XMLStreamWriter writer;
-    private static OutputStreamWriter out = null;
-    private static final String TAG_LISTE = "Mediathek";
-    private static final String TAG_SERVER = "Server";
-    private static final String TAG_SERVER_URL_PRIO_1 = "Download_Filme_1";
-    private static final String TAG_SERVER_DATUM = "Datum";
-    private static final String TAG_SERVER_ZEIT = "Zeit";
 
     public static File filmlisteEintragen(String urlDatei, DatenUrlFilmliste input) {
         ListeDownloadUrlsFilmlisten listeFilmUpdateServer = new ListeDownloadUrlsFilmlisten();
@@ -70,59 +55,12 @@ public class MS_ListeFilmlisten {
         }
         listeFilmUpdateServer.add(input);
         // Liste in Datei schreiben
-        return ListeFilmlistenSchreiben(listeFilmUpdateServer);
-    }
-
-    private static File ListeFilmlistenSchreiben(ListeDownloadUrlsFilmlisten listeFilmUpdateServer) {
-        File tmpFile = null;
+        File f = null;
         try {
-            tmpFile = File.createTempFile("mediathek", null);
-            tmpFile.deleteOnExit();
-            outFactory = XMLOutputFactory.newInstance();
-            out = new OutputStreamWriter(new FileOutputStream(tmpFile), Konstanten.KODIERUNG_UTF);
-            writer = outFactory.createXMLStreamWriter(out);
-            writer.writeStartDocument("UTF-8", "1.0");
-            writer.writeCharacters("\n");//neue Zeile
-            writer.writeStartElement(TAG_LISTE);
-            writer.writeCharacters("\n");//neue Zeile
-            Iterator<DatenUrlFilmliste> it = listeFilmUpdateServer.iterator();
-            while (it.hasNext()) {
-                DatenUrlFilmliste d = it.next();
-                writer.writeStartElement(TAG_SERVER);
-                writer.writeCharacters("\n");
-                // Tags schreiben: URL
-                writer.writeCharacters("\t");// Tab
-                writer.writeStartElement(TAG_SERVER_URL_PRIO_1);
-                writer.writeCharacters(d.arr[FilmlistenSuchen.FILM_UPDATE_SERVER_URL_NR]);
-                writer.writeEndElement();
-                writer.writeCharacters("\n");
-                // fertig
-                // Tags schreiben: Datum
-                writer.writeCharacters("\t");// Tab
-                writer.writeStartElement(TAG_SERVER_DATUM);
-                writer.writeCharacters(d.arr[FilmlistenSuchen.FILM_UPDATE_SERVER_DATUM_NR]);
-                writer.writeEndElement();
-                writer.writeCharacters("\n");
-                // fertig
-                // Tags schreiben: Zeit
-                writer.writeCharacters("\t");// Tab
-                writer.writeStartElement(TAG_SERVER_ZEIT);
-                writer.writeCharacters(d.arr[FilmlistenSuchen.FILM_UPDATE_SERVER_ZEIT_NR]);
-                writer.writeEndElement();
-                writer.writeCharacters("\n");
-                // fertig
-                writer.writeEndElement();
-                writer.writeCharacters("\n");
-            }
-            // Schließen
-            writer.writeEndElement();
-            writer.writeEndDocument();
-            writer.flush();
-            writer.close();
-            MS_Log.systemMeldung("geschrieben!");
+            f = FilmlistenSuchen.ListeFilmlistenSchreiben(listeFilmUpdateServer);
         } catch (Exception ex) {
-            MS_Log.fehlerMeldung(645421039, MS_XmlSchreiben.class.getName(), "xmlDatenSchreiben", ex);
+            MS_Log.fehlerMeldung(347895642, MS_ListeFilmlisten.class.getName(), urlDatei, ex);
         }
-        return tmpFile;
+        return f;
     }
 }
