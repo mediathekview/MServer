6e50b0ffb4695951f0ea62fc6791d734649a4e4a
pidoubleyou <pidoubleyou@arcor.de>
2017-01-04 22:50:57 +0100

    VideoDTO  und VideoDTODeserializer erweitert

--- src/main/java/mServer/crawler/sender/newsearch/VideoDTO.java
+++ src/main/java/mServer/crawler/sender/newsearch/VideoDTO.java
@@ -5,7 +5,35 @@ package mServer.crawler.sender.newsearch;
  */
 public class VideoDTO
 {
+    private String description;
+    private int duration;
+    private String title;
     private String topic;
+    private String websiteUrl;
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String aDescription) {
+        description = aDescription;
+    }
+    
+    public int getDuration() {
+        return duration;
+    }
+    
+    public void setDuration(int aDuration) {
+        duration = aDuration;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String aTitle) {
+        title = aTitle;
+    }
 
     public String getTopic() {
         return topic;
@@ -13,5 +41,13 @@ public class VideoDTO
     
     public void setTopic(String aTopic) {
         topic = aTopic;
+    }
+    
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+    
+    public void setWebsiteUrl(String aWebsiteUrl) {
+        websiteUrl = aWebsiteUrl;
     }
 }
--- src/main/java/mServer/crawler/sender/newsearch/ZDFVideoDTODeserializer.java
+++ src/main/java/mServer/crawler/sender/newsearch/ZDFVideoDTODeserializer.java
@@ -2,14 +2,26 @@ package mServer.crawler.sender.newsearch;
 
 import com.google.gson.*;
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A JSON deserializer to gather the needed information for a {@link VideoDTO}.
  */
 public class ZDFVideoDTODeserializer implements JsonDeserializer<VideoDTO> {
 
+    private static final String JSON_ELEMENT_BEGIN = "airtimeBegin";
     private static final String JSON_ELEMENT_BRAND = "http://zdf.de/rels/brand";
+    private static final String JSON_ELEMENT_BROADCAST = "http://zdf.de/rels/cmdm/broadcasts";
+    private static final String JSON_ELEMENT_DURATION = "duration";
+    private static final String JSON_ELEMENT_MAINVIDEO  = "mainVideoContent";
+    private static final String JSON_ELEMENT_PROGRAMMITEM = "programmeItem";
+    private static final String JSON_ELEMENT_SHARING_URL = "http://zdf.de/rels/sharing-url";
+    private static final String JSON_ELEMENT_SUBTITLE = "subtitle";
+    private static final String JSON_ELEMENT_TARGET = "http://zdf.de/rels/target";
     private static final String JSON_ELEMENT_TITLE = "title";
+    private static final String JSON_ELEMENT_TEASERTEXT = "teasertext";
     
     @Override
     public VideoDTO deserialize(final JsonElement aJsonElement, final Type aTypeOfT, final JsonDeserializationContext aJsonDeserializationContext) throws JsonParseException {
@@ -21,7 +33,84 @@ public class ZDFVideoDTODeserializer implements JsonDeserializer<VideoDTO> {
             JsonElement topic = brand.get(JSON_ELEMENT_TITLE);
             dto.setTopic(topic.getAsString());
         }       
+        // nicht immer gesetzt!! muss ein anderer Weg sein!!!
+        JsonElement teaserText = object.get(JSON_ELEMENT_TEASERTEXT);
+        if(teaserText != null) {
+            dto.setDescription(teaserText.getAsString());
+        }
+
+        JsonArray programmItem = object.getAsJsonArray(JSON_ELEMENT_PROGRAMMITEM);
+        JsonObject programmItemTarget = null;
+        
+        if(programmItem != null) {
+            if(programmItem.size() > 1) {
+                throw new RuntimeException("Element does not contain programmitem" + object.getAsString());
+            }
+        
+            if(programmItem.size() == 1) {
+                programmItemTarget = programmItem.get(0).getAsJsonObject().get(JSON_ELEMENT_TARGET).getAsJsonObject();
+            }
+        }
+        setTitle(dto, object, programmItemTarget);        
+        
+        JsonObject mainVideo = object.get(JSON_ELEMENT_MAINVIDEO).getAsJsonObject();
+        JsonObject targetMainVideo = mainVideo.get(JSON_ELEMENT_TARGET).getAsJsonObject();
+        int duration = targetMainVideo.get(JSON_ELEMENT_DURATION).getAsInt();
+        dto.setDuration(duration);
+        
+        String websiteUrl = object.get(JSON_ELEMENT_SHARING_URL).getAsString();
+        dto.setWebsiteUrl(websiteUrl);
         
         return dto;
     }
+    
+    private void setTitle(VideoDTO dto, JsonObject object, JsonObject target) {
+        JsonElement titleElement = object.get(JSON_ELEMENT_TITLE);
+        if(titleElement != null) {
+            dto.setTitle(titleElement.getAsString());
+        } else {
+            String title = target.get(JSON_ELEMENT_TITLE).getAsString();
+            String subTitle = target.get(JSON_ELEMENT_SUBTITLE).getAsString();
+
+            if(subTitle.isEmpty()) {
+                dto.setTitle(title);
+            } else {
+                dto.setTitle(title + " - " + subTitle);
+            }       
+        }
+    }
+    
+     private String getAirtimeBegin(JsonObject programmItemTarget) {
+
+        JsonArray broadcastArray = programmItemTarget.getAsJsonArray(JSON_ELEMENT_BROADCAST);
+         
+        if(broadcastArray.size() != 1) {
+            throw new RuntimeException("Element does not contain broadcast" + programmItemTarget.getAsString());
+        }
+
+        String begin = broadcastArray.get(0).getAsJsonObject().get(JSON_ELEMENT_BEGIN).getAsString();
+        return begin;
+    } 
+     
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");//2016-10-29T16:15:00+02:00
+    private final SimpleDateFormat sdfOutTime = new SimpleDateFormat("HH:mm:ss");
+    private final SimpleDateFormat sdfOutDay = new SimpleDateFormat("dd.MM.yyyy");
+
+    private String convertDate(String dateValue) {
+        try {
+            Date filmDate = sdf.parse(dateValue);
+            return sdfOutDay.format(filmDate);
+        } catch (ParseException ex) {
+            throw new RuntimeException("Date parse exception: " + dateValue);
+        }
+    }
+
+    private String convertTime(String dateValue) {
+        try {
+            Date filmDate = sdf.parse(dateValue);
+            return sdfOutTime.format(filmDate);
+        } catch (ParseException ex) {
+            throw new RuntimeException("Date parse exception: " + dateValue);
+        }
+    }     
 }
