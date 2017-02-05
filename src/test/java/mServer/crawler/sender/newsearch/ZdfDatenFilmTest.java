ae32299368028178b2c898c2783c0127efa6ae4e
pidoubleyou <pidoubleyou@arcor.de>
2017-02-05 23:24:36 +0100

    erster JUnit-Test

---
+++ src/test/java/mServer/crawler/sender/newsearch/ZdfDatenFilmTest.java
@@ -0,0 +1,39 @@
+package mServer.crawler.sender.newsearch;
+
+import java.util.Arrays;
+import java.util.Collection;
+import mSearch.Const;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ZdfDatenFilmTest {
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {   
+            { "my topic", "url", "zdfmy topicurl" },
+            { "topic", "https://rodlzdf-and-so-on.de/12/11.mp4", "zdftopichttps://rodlzdf-and-so-on.de/12/11.mp4" },
+            { "topic", "https://nrodlzdf-and-so-on.de/12/11.mp4", "zdftopichttps://rodlzdf-and-so-on.de/12/11.mp4" },
+            { "topic", "http://rodlzdf-and-so-on.de/12/11.mp4", "zdftopichttp://rodlzdf-and-so-on.de/12/11.mp4" },
+            { "topic", "http://nrodlzdf-and-so-on.de/12/11.mp4", "zdftopichttp://rodlzdf-and-so-on.de/12/11.mp4" },
+        });
+    }
+    
+    private final ZdfDatenFilm target;
+    private final String expectedIndex;
+    
+    public ZdfDatenFilmTest(String topic, String url, String expectedIndex) {
+        target = new ZdfDatenFilm(Const.ZDF, topic, "", "", url, "", "", "", 0, "");
+        this.expectedIndex = expectedIndex;
+    }
+    
+    @Test
+    public void testGetIndex() {
+        assertEquals(expectedIndex, target.getIndex());
+    }
+}
