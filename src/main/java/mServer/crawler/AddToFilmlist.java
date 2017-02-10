7373c1807913f9961cb6c182d921b4c7ca2aa670
W.Xaver <w.xaver@googlemail.com>
2017-02-10 13:03:47 +0100

    test

--- src/main/java/mServer/crawler/AddToFilmlist.java
+++ src/main/java/mServer/crawler/AddToFilmlist.java
@@ -5,28 +5,26 @@
  */
 package mServer.crawler;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Collectors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-
+import java.util.stream.Collectors;
 import mSearch.Config;
 import mSearch.daten.DatenFilm;
 import mSearch.daten.ListeFilme;
 import mSearch.tool.FileSize;
 import mSearch.tool.Log;
 
-
-
 /**
  *
  * @author emil
  */
 public class AddToFilmlist {
-   
+
     private static final int MIN_SIZE_ADD_OLD = 5;
 
     final int COUNTER_MAX = 20;
@@ -34,6 +32,7 @@ public class AddToFilmlist {
     AtomicInteger treffer = new AtomicInteger(0);
     ListeFilme vonListe;
     ListeFilme listeEinsortieren;
+    Collection<DatenFilm> filteredOnline = new ArrayList<>();
 
     public AddToFilmlist(ListeFilme vonListe, ListeFilme listeEinsortieren) {
         this.vonListe = vonListe;
@@ -68,9 +67,9 @@ public class AddToFilmlist {
         // nach "Thema-Titel" suchen
         Collection<DatenFilm> filteredTopicTitle = new CopyOnWriteArrayList<>();
         filteredTopicTitle.addAll(listeEinsortieren.parallelStream()
-            .filter(film -> (null == vonListe.istInFilmListe(film.arr[DatenFilm.FILM_SENDER], film.arr[DatenFilm.FILM_THEMA], film.arr[DatenFilm.FILM_TITEL])))
-            .collect(Collectors.toList()));
-       
+                .filter(film -> (null == vonListe.istInFilmListe(film.arr[DatenFilm.FILM_SENDER], film.arr[DatenFilm.FILM_THEMA], film.arr[DatenFilm.FILM_TITEL])))
+                .collect(Collectors.toList()));
+
         Log.sysLog("===== Liste einsortieren Hash =====");
         Log.sysLog("Liste einsortieren, Anzahl: " + listeEinsortieren.size());
         Log.sysLog("Liste einsortieren, entfernt: " + (listeEinsortieren.size() - filteredTopicTitle.size()));
@@ -81,14 +80,14 @@ public class AddToFilmlist {
         Collection<DatenFilm> filteredUrl = new CopyOnWriteArrayList<>();
 
         Collection<String> filmUrls = vonListe.parallelStream()
-        .map(DatenFilm::getUrl)
-        .collect(Collectors.toList());
+                .map(DatenFilm::getUrl)
+                .collect(Collectors.toList());
 
         int size = filteredTopicTitle.size();
-        
+
         filteredUrl.addAll(filteredTopicTitle.parallelStream()
-            .filter(film -> !filmUrls.contains(DatenFilm.getUrl(film)))
-            .collect(Collectors.toList()));
+                .filter(film -> !filmUrls.contains(DatenFilm.getUrl(film)))
+                .collect(Collectors.toList()));
 
         Log.sysLog("===== Liste einsortieren URL =====");
         Log.sysLog("Liste einsortieren, Anzahl: " + size);
@@ -97,20 +96,24 @@ public class AddToFilmlist {
 
         // Rest nehmen wir wenn noch online
         // Prüfung auf online erst am Ende durchführen, damit jeder Film nur einmalig geprüft wird
-        Collection<DatenFilm> filteredOnline = new CopyOnWriteArrayList<>();
-        filteredOnline.addAll(filteredUrl.parallelStream().filter(f -> 
-            !Config.getStop() && FileSize.laengeLong(f.arr[DatenFilm.FILM_URL]) > MIN_SIZE_ADD_OLD
-        )
-        .collect(Collectors.toList()));
-        filteredOnline.parallelStream().forEach(f ->{
-            if(!Config.getStop())
-            {
-                initFilm(f);
-            }
-        });        
+        laufen = true;
+        new Thread(new Ti()).start();
+
+        Collection<SizeAction> actions = new ArrayList<>();
+        filteredUrl.forEach((video) -> {
+            SizeAction newTask = new SizeAction(video);
+            actions.add(newTask);
+        });
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        try {
+            actions.forEach(e -> executor.execute(e));
+        } catch (Exception ex) {
+            Log.sysLog("ExecutorService");
+        }
+
         vonListe.addAll(filteredOnline);
-        
-        
+        laufen = false;
         Log.sysLog("===== Liste einsortieren: Noch online =====");
         Log.sysLog("Liste einsortieren, Anzahl: " + filteredOnline.size());
         Log.sysLog("Liste einsortieren, entfernt: " + (filteredOnline.size() - treffer.get()));
@@ -125,4 +128,113 @@ public class AddToFilmlist {
         film.init();
     }
 
+    private class SizeAction implements Runnable {
+
+        DatenFilm f;
+
+        public SizeAction(DatenFilm f) {
+            this.f = f;
+        }
+
+        public void run() {
+            if (!Config.getStop()) {
+                addCount();
+                if (FileSize.laengeLong(f.arr[DatenFilm.FILM_URL]) > MIN_SIZE_ADD_OLD) {
+                    initFilm(f);
+                    filteredOnline.add(f);
+                }
+            }
+        }
+    }
+
+    public synchronized int addOldList_() {
+        // in eine vorhandene Liste soll eine andere Filmliste einsortiert werden
+        // es werden nur Filme die noch nicht vorhanden sind, einsortiert
+        counter = 0;
+        treffer = new AtomicInteger(0);
+
+        // ==============================================
+        // nach "Thema-Titel" suchen
+        Collection<DatenFilm> filteredTopicTitle = new CopyOnWriteArrayList<>();
+        filteredTopicTitle.addAll(listeEinsortieren.parallelStream()
+                .filter(film -> (null == vonListe.istInFilmListe(film.arr[DatenFilm.FILM_SENDER], film.arr[DatenFilm.FILM_THEMA], film.arr[DatenFilm.FILM_TITEL])))
+                .collect(Collectors.toList()));
+
+        Log.sysLog("===== Liste einsortieren Hash =====");
+        Log.sysLog("Liste einsortieren, Anzahl: " + listeEinsortieren.size());
+        Log.sysLog("Liste einsortieren, entfernt: " + (listeEinsortieren.size() - filteredTopicTitle.size()));
+        Log.sysLog("");
+
+        // ==============================================
+        // nach "URL" suchen
+        Collection<DatenFilm> filteredUrl = new CopyOnWriteArrayList<>();
+
+        Collection<String> filmUrls = vonListe.parallelStream()
+                .map(DatenFilm::getUrl)
+                .collect(Collectors.toList());
+
+        int size = filteredTopicTitle.size();
+
+        filteredUrl.addAll(filteredTopicTitle.parallelStream()
+                .filter(film -> !filmUrls.contains(DatenFilm.getUrl(film)))
+                .collect(Collectors.toList()));
+
+        Log.sysLog("===== Liste einsortieren URL =====");
+        Log.sysLog("Liste einsortieren, Anzahl: " + size);
+        Log.sysLog("Liste einsortieren, entfernt: " + (size - filteredUrl.size()));
+        Log.sysLog("");
+
+        count = 0;
+        laufen = true;
+        new Thread(new Ti()).start();
+
+        // Rest nehmen wir wenn noch online
+        // Prüfung auf online erst am Ende durchführen, damit jeder Film nur einmalig geprüft wird
+        Collection<DatenFilm> filteredOnline = new CopyOnWriteArrayList<>();
+        filteredOnline.addAll(filteredUrl.parallelStream().filter(f
+                -> addCount() && !Config.getStop() && FileSize.laengeLong(f.arr[DatenFilm.FILM_URL]) > MIN_SIZE_ADD_OLD
+        )
+                .collect(Collectors.toList()));
+        filteredOnline.parallelStream().forEach(f -> {
+            if (!Config.getStop()) {
+                initFilm(f);
+            }
+        });
+        vonListe.addAll(filteredOnline);
+        laufen = false;
+
+        Log.sysLog("===== Liste einsortieren: Noch online =====");
+        Log.sysLog("Liste einsortieren, Anzahl: " + filteredOnline.size());
+        Log.sysLog("Liste einsortieren, entfernt: " + (filteredOnline.size() - treffer.get()));
+        Log.sysLog("");
+        Log.sysLog("In Liste einsortiert: " + treffer.get());
+        Log.sysLog("");
+        return treffer.get();
+    }
+
+    static int count = 0;
+    static boolean laufen = true;
+
+    private synchronized boolean addCount() {
+        ++count;
+        return true;
+    }
+
+    class Ti implements Runnable {
+
+        // nur zum Testen
+        int t = 0;
+
+        @Override
+        public synchronized void run() {
+            while (laufen) {
+                ++t;
+                System.out.println(t + " Count : " + count);
+                try {
+                    wait(1000);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+    }
 }
