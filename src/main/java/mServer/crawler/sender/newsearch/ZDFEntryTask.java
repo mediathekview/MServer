ca19f729b94b998b5a17dfc0476f76dacc7eb2d2
Nicklas Wiegandt <nicklas@wiegandt.net>
2017-03-13 17:46:24 +0100

    # Fixed #117

--- src/main/java/mServer/crawler/sender/newsearch/ZDFEntryTask.java
+++ src/main/java/mServer/crawler/sender/newsearch/ZDFEntryTask.java
@@ -7,59 +7,78 @@ import com.google.gson.JsonSyntaxException;
 import com.sun.jersey.api.client.WebResource;
 
 import java.util.concurrent.RecursiveTask;
+
 import mSearch.Config;
 import mSearch.tool.Log;
 
 /**
  * Searches all information required for a film
  */
-public class ZDFEntryTask extends RecursiveTask<VideoDTO> {
+public class ZDFEntryTask extends RecursiveTask<VideoDTO>
+{
 
     private static final long serialVersionUID = 1L;
-    
+
     private final ZDFClient client;
     private final ZDFEntryDTO zdfEntryDTO;
     private final Gson gson;
-    
-    public ZDFEntryTask(ZDFEntryDTO aEntryDto) {
+
+    public ZDFEntryTask(ZDFEntryDTO aEntryDto)
+    {
         client = new ZDFClient();
-        zdfEntryDTO = aEntryDto;                
+        zdfEntryDTO = aEntryDto;
         gson = new GsonBuilder()
                 .registerTypeAdapter(VideoDTO.class, new ZDFVideoDTODeserializer())
                 .registerTypeAdapter(DownloadDTO.class, new ZDFDownloadDTODeserializer())
                 .create();
     }
-    
+
     @Override
-    protected VideoDTO compute() {
+    protected VideoDTO compute()
+    {
 
         VideoDTO dto = null;
 
-        if(!Config.getStop()) {
-            try {
+        if (!Config.getStop())
+        {
+            try
+            {
                 // read film details
                 String infoUrl = zdfEntryDTO.getEntryGeneralInformationUrl();
                 WebResource webResourceInfo = client.createResource(infoUrl);
                 JsonObject baseObjectInfo = client.execute(webResourceInfo, ZDFClient.ZDFClientMode.VIDEO);
-                if(baseObjectInfo != null) {
+                if (baseObjectInfo != null)
+                {
                     dto = gson.fromJson(baseObjectInfo, VideoDTO.class);
-                    if(dto != null) {
+                    try
+                    {
+                        if (baseObjectInfo.getAsString().contains("Varg Venum"))
+                        {
+                            System.out.println("found varg venum");
+                        }
+                    } catch (Exception e)
+                    {
+                    }
+                    if (dto != null)
+                    {
                         // read download details
                         String downloadUrl = zdfEntryDTO.getEntryDownloadInformationUrl();
                         WebResource webResourceDownload = client.createResource(downloadUrl);
                         JsonObject baseObjectDownload = client.execute(webResourceDownload, ZDFClient.ZDFClientMode.VIDEO);
-                        if(baseObjectDownload != null) {
+                        if (baseObjectDownload != null)
+                        {
                             DownloadDTO downloadDto = gson.fromJson(baseObjectDownload, DownloadDTO.class);
                             dto.setDownloadDto(downloadDto);
                         }
                     }
                 }
-            } catch (Exception ex) {
+            } catch (Exception ex)
+            {
                 Log.errorLog(496583202, ex, "Exception parsing " + zdfEntryDTO.getEntryGeneralInformationUrl());
                 dto = null;
             }
         }
-        
+
         return dto;
-    }    
+    }
 }
