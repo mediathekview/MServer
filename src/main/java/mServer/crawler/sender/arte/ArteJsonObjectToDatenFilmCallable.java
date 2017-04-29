b513e57638c8d9c4f9e1bd865012a358c936d3b3
pidoubleyou <pidoubleyou@arcor.de>
2017-04-29 13:55:12 +0200

    Fix für Datum und Uhrzeit

--- src/main/java/mServer/crawler/sender/arte/ArteJsonObjectToDatenFilmCallable.java
+++ src/main/java/mServer/crawler/sender/arte/ArteJsonObjectToDatenFilmCallable.java
@@ -1,17 +1,11 @@
 package mServer.crawler.sender.arte;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.FormatStyle;
-import java.time.temporal.ChronoField;
 import java.util.Locale;
 import java.util.concurrent.Callable;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import mServer.crawler.CrawlerTool;
 import mServer.crawler.sender.newsearch.Qualities;
@@ -26,15 +20,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
-import de.mediathekview.mlib.Const;
 import de.mediathekview.mlib.daten.DatenFilm;
 import de.mediathekview.mlib.tool.MVHttpClient;
+import java.text.ParseException;
+import org.apache.commons.lang3.time.FastDateFormat;
 
 public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
 {
     private static final Logger LOG = LogManager.getLogger(ArteJsonObjectToDatenFilmCallable.class);
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.GERMANY);
-    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofLocalizedTime(FormatStyle.MEDIUM).withLocale(Locale.GERMANY);
     private static final String JSON_OBJECT_KEY_PROGRAM = "program";
     private static final String JSON_ELEMENT_KEY_TITLE = "title";
     private static final String JSON_ELEMENT_KEY_SUBTITLE = "subtitle";
@@ -42,13 +35,16 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
     private static final String JSON_ELEMENT_KEY_PROGRAM_ID = "programId";
     private static final String ARTE_VIDEO_INFORMATION_URL_PATTERN = "https://api.arte.tv/api/player/v1/config/%s/%s";
     private static final String JSON_ELEMENT_KEY_SHORT_DESCRIPTION = "shortDescription";
-    
-    //1. Non-Capture Group duration, 2. Positive lookbehind for 0 or more non word charackters, 3. One or more numbers, 4. positive lookahed closing span tag
-    private static final String REGEX_PATTERN_DATETIME = "(?!:<li class=\"main-programming-broadcast-date\">)(?<=\\W*)\\w*,\\s*\\d+\\.\\s\\w+\\s*\\w+\\s*\\d*\\.\\d*\\s*\\w+(?=\\s*<\\/li>)";
+    private static final String JSON_ELEMENT_BROADCAST = "broadcastBeginRounded";
     
     private final JsonObject jsonObject;
     private final String langCode;
     private final String senderName;
+    
+    private final FastDateFormat sdfOutTime = FastDateFormat.getInstance("HH:mm:ss");
+    private final FastDateFormat sdfOutDay = FastDateFormat.getInstance("dd.MM.yyyy");
+    
+    private final FastDateFormat sdfEditorialDate = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ssX");//2016-10-29T16:15:00.000+02:00
     
     public ArteJsonObjectToDatenFilmCallable(JsonObject aJsonObjec, String aLangCode, String aSenderName) {
         jsonObject = aJsonObjec;
@@ -79,7 +75,7 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
                 //https://api.arte.tv/api/player/v1/config/[language:de/fr]/[programId]
                 String programId = programObject.get(JSON_ELEMENT_KEY_PROGRAM_ID).getAsString();
                 String videosUrl = String.format(ARTE_VIDEO_INFORMATION_URL_PATTERN, langCode, programId);
-    
+                
                 Gson gson = new GsonBuilder().registerTypeAdapter(ArteVideoDTO.class, new ArteVideoDeserializer()).create();
     
                 OkHttpClient httpClient = MVHttpClient.getInstance().getHttpClient();
@@ -93,16 +89,23 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
                     if(responseFilmDetails.isSuccessful() && responseVideoDetails.isSuccessful())
                     {
                         ArteVideoDTO video = gson.fromJson(responseVideoDetails.body().string(), ArteVideoDTO.class);
-                        String filmDetails = responseFilmDetails.body().string();
-                        LocalDateTime dateTime = readDateTime(filmDetails);
                         
                         //The duration as time so it can be formatted and co.
                         LocalTime durationAsTime = durationAsTime(video.getDurationInSeconds());
                        
                         if (video.getVideoUrls().containsKey(Qualities.NORMAL))
                         {
+                            String date = "", time = "";
+                            
+                            if(jsonObject.has(JSON_ELEMENT_BROADCAST)) {
+                                String broadcastBegin = jsonObject.get(JSON_ELEMENT_BROADCAST).getAsString();
+    
+                                date = convertDate(broadcastBegin, sdfEditorialDate);
+                                time = convertTime(broadcastBegin, sdfEditorialDate);
+                            }
+                            
                             film = new DatenFilm(senderName, thema, urlWeb, titel, video.getUrl(Qualities.NORMAL), "" /*urlRtmp*/,
-                                    dateTime.format(DATE_FORMATTER), dateTime.format(TIME_FORMATTER), durationAsTime.toSecondOfDay(), beschreibung);
+                                    date, time, durationAsTime.toSecondOfDay(), beschreibung);
                             if (video.getVideoUrls().containsKey(Qualities.HD))
                             {
                                 CrawlerTool.addUrlHd(film, video.getUrl(Qualities.HD), "");
@@ -127,27 +130,22 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
         return film;
     }
     
-    
-    private LocalDateTime readDateTime(String aFilmDetails) throws IOException
-    {
-        DateTimeFormatter dateTimeFormatter = new DateTimeFormatterBuilder().appendPattern("EEEE', 'd'. 'MMMM' um 'H.mm' Uhr'")
-            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-            .parseDefaulting(ChronoField.YEAR, 2017)
-            .toFormatter(Locale.GERMANY); 
-            Matcher matcher = Pattern.compile(REGEX_PATTERN_DATETIME).matcher(aFilmDetails);
-        if(matcher.find())
-        {
-            LocalDateTime localDateTime = LocalDateTime.parse(matcher.group().trim().replaceAll("\\n", "").replaceAll("\\s{2,}", " "),dateTimeFormatter);
-            return localDateTime;
-        } else {
-            return LocalDateTime.of(LocalDate.now(),LocalTime.MIDNIGHT);
+        private String convertDate(String dateValue, FastDateFormat sdf) {
+        try {
+            return sdfOutDay.format(sdf.parse(dateValue));
+        } catch (ParseException ex) {
+            throw new RuntimeException("Date parse exception: " + dateValue);
         }
-        
-        
     }
-    
+
+    private String convertTime(String dateValue, FastDateFormat sdf) {
+        try {
+            return sdfOutTime.format(sdf.parse(dateValue));
+        } catch (ParseException ex) {
+            throw new RuntimeException("Date parse exception: " + dateValue);
+        }
+    }  
+     
     private LocalTime durationAsTime(long aDurationInSeconds) throws IOException
     {
         LocalTime localTime = LocalTime.MIN;
