fec72363a6656a2da1fdb18432eb45ff9eda806d
pidoubleyou <pidoubleyou@arcor.de>
2017-07-10 01:30:41 +0200

    Logik für Ermittlung des Datums anhand von catchupRights

--- src/main/java/mServer/crawler/sender/arte/ArteJsonObjectToDatenFilmCallable.java
+++ src/main/java/mServer/crawler/sender/arte/ArteJsonObjectToDatenFilmCallable.java
@@ -16,8 +16,13 @@ import com.google.gson.JsonObject;
 
 import de.mediathekview.mlib.daten.DatenFilm;
 import de.mediathekview.mlib.tool.MVHttpClient;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
 import mServer.crawler.CrawlerTool;
 import mServer.crawler.sender.newsearch.Qualities;
+import mServer.tool.DateWithoutTimeComparer;
 import mServer.tool.MserverDatumZeit;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -40,6 +45,8 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
     private static final String JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_2 = "broadcastProgrammings";
     private static final String JSON_ELEMENT_BROADCAST = "broadcastBeginRounded";
     private static final String JSON_ELEMENT_BROADCASTTYPE = "broadcastType";
+    private static final String JSON_ELEMENT_BROADCAST_CATCHUPRIGHTS_BEGIN = "catchupRightsBegin";
+    private static final String JSON_ELEMENT_BROADCAST_CATCHUPRIGHTS_END = "catchupRightsEnd";
     private static final String BROADCASTTTYPE_FIRST = "FIRST_BROADCAST";
     private static final String BROADCASTTTYPE_MINOR_RE = "MINOR_REBROADCAST";
     private static final String BROADCASTTTYPE_MAJOR_RE = "MAJOR_REBROADCAST";
@@ -47,13 +54,16 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
     private final JsonObject jsonObject;
     private final String langCode;
     private final String senderName;
+    private final Calendar today;
     
-    private final FastDateFormat broadcastDate = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ssX");//2016-10-29T16:15:00Z
+    private final FastDateFormat broadcastDateFormat = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ssX");//2016-10-29T16:15:00Z
     
     public ArteJsonObjectToDatenFilmCallable(JsonObject aJsonObjec, String aLangCode, String aSenderName) {
         jsonObject = aJsonObjec;
         langCode = aLangCode;
         senderName = aSenderName;
+        
+        today = Calendar.getInstance();
     }
 
     @Override
@@ -134,13 +144,11 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
              */
             JsonObject jsonObjectVideoDetails2 = gson.fromJson(responseVideoDetails2.body().string(), JsonObject.class);
             if(jsonObjectVideoDetails2.isJsonObject() && 
-                jsonObjectVideoDetails2.get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_1).getAsJsonArray().size() > 0 &&
-                jsonObjectVideoDetails2.get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_1).getAsJsonArray().get(0).getAsJsonObject()
-                  .get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_2).getAsJsonArray().size() > 0) {
+                jsonObjectVideoDetails2.get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_1).getAsJsonArray().size() > 0) {
                 
-                JsonArray broadcastArray = jsonObjectVideoDetails2
-                    .get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_1).getAsJsonArray().get(0).getAsJsonObject()
-                    .get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_2).getAsJsonArray();
+                JsonObject programElement = jsonObjectVideoDetails2
+                    .get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_1).getAsJsonArray().get(0).getAsJsonObject();
+                JsonArray broadcastArray = programElement.get(JSON_ELEMENT_BROADCAST_ELTERNKNOTEN_2).getAsJsonArray();
                 
                 if(broadcastArray.size() > 0) {
                     String broadcastBeginFirst = "";
@@ -149,23 +157,27 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
                     
                     // nach Priorität der BroadcastTypen den relevanten Eintrag suchen
                     // FIRST_BROADCAST => MAJOR_REBROADCAST => MINOR_REBROADCAST
+                    // dabei die "aktuellste" Ausstrahlung verwenden
                     for(int i = 0; i < broadcastArray.size(); i++) {
                         JsonObject broadcastObject = broadcastArray.get(i).getAsJsonObject();
                         if(broadcastObject.has(JSON_ELEMENT_BROADCASTTYPE) && 
                                 broadcastObject.has(JSON_ELEMENT_BROADCAST)) {
-                            String type = broadcastObject.get(JSON_ELEMENT_BROADCASTTYPE).getAsString();
-                            switch(type) {
-                                case BROADCASTTTYPE_FIRST:
-                                    broadcastBeginFirst = broadcastObject.get(JSON_ELEMENT_BROADCAST).getAsString();
-                                    break;
-                                case BROADCASTTTYPE_MAJOR_RE:
-                                    broadcastBeginMajor = broadcastObject.get(JSON_ELEMENT_BROADCAST).getAsString();
-                                    break;
-                                case BROADCASTTTYPE_MINOR_RE:
-                                    broadcastBeginMinor = broadcastObject.get(JSON_ELEMENT_BROADCAST).getAsString();
-                                    break;
-                                default:
-                                    LOG.debug("New broadcasttype: " + type);
+                            String value = this.getBroadcastDate(broadcastObject);
+                            if(!value.isEmpty()) {
+                                String type = broadcastObject.get(JSON_ELEMENT_BROADCASTTYPE).getAsString();
+                                switch(type) {
+                                    case BROADCASTTTYPE_FIRST:
+                                        broadcastBeginFirst = value;
+                                        break;
+                                    case BROADCASTTTYPE_MAJOR_RE:
+                                        broadcastBeginMajor = value;
+                                        break;
+                                    case BROADCASTTTYPE_MINOR_RE:
+                                        broadcastBeginMinor = value;
+                                        break;
+                                    default:
+                                        LOG.debug("New broadcasttype: " + type);
+                                }
                             }
                         }
                     }
@@ -177,11 +189,70 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
                     } else if(!broadcastBeginMinor.isEmpty()) {
                         broadcastBegin = broadcastBeginMinor;
                     }
+                } else {
+                    // keine Ausstrahlungen verfügbar => catchupRightsBegin verwenden
+                    JsonElement elementBegin = programElement.get(JSON_ELEMENT_BROADCAST_CATCHUPRIGHTS_BEGIN);
+                    if(!elementBegin.isJsonNull()) {
+                        broadcastBegin = elementBegin.getAsString();
+                    }
                 }
             }
           }
         }      
         return broadcastBegin;
+    }
+    
+    /**
+     * Liefert den Beginn der Ausstrahlung, 
+     * wenn 
+     *  - heute im Zeitraum von CatchUpRights liegt 
+     *  - oder heute vor dem Zeitraum liegt
+     *  - oder CatchUpRights nicht gesetzt ist und die Ausstrahlung in der Vergangenheit liegt
+     * @param broadcastObject 
+     * @return der Beginn der Ausstrahlung oder ""
+     */
+    private String getBroadcastDate(JsonObject broadcastObject) {
+        String broadcastDate = "";
+        
+        JsonElement elementBegin = broadcastObject.get(JSON_ELEMENT_BROADCAST_CATCHUPRIGHTS_BEGIN);
+        JsonElement elementEnd = broadcastObject.get(JSON_ELEMENT_BROADCAST_CATCHUPRIGHTS_END);
+        if (!elementBegin.isJsonNull() && !elementEnd.isJsonNull()) {
+            String begin = elementBegin.getAsString();
+            String end = elementEnd.getAsString();
+
+            try {
+                Calendar beginDate = Calendar.getInstance();
+                beginDate.setTime(broadcastDateFormat.parse(begin));
+                Calendar endDate = Calendar.getInstance();
+                endDate.setTime(broadcastDateFormat.parse(end));
+
+                if(DateWithoutTimeComparer.compare(today, beginDate) >= 0 && DateWithoutTimeComparer.compare(today, endDate) <= 0) {
+                    // wenn das heutige Datum zwischen begin und end liegt,
+                    // dann ist es die aktuelle Ausstrahlung
+                    broadcastDate = broadcastObject.get(JSON_ELEMENT_BROADCAST).getAsString();
+                } else if(DateWithoutTimeComparer.compare(today, beginDate) < 0) {
+                    // ansonsten die zukünftige verwenden
+                    broadcastDate = broadcastObject.get(JSON_ELEMENT_BROADCAST).getAsString();
+                }
+                
+            } catch (ParseException ex) {
+                LOG.debug(ex);
+            }           
+        } else {
+            String broadcast = broadcastObject.get(JSON_ELEMENT_BROADCAST).getAsString();
+            
+            try {
+                Calendar broadcastCal = Calendar.getInstance();
+                broadcastCal.setTime(broadcastDateFormat.parse(broadcast));
+                
+                if(DateWithoutTimeComparer.compare(today, broadcastCal) >= 0) {
+                    broadcastDate = broadcast;
+                }
+            } catch (ParseException ex) {
+                LOG.debug(ex);
+            }            
+        }
+        return broadcastDate;
     }
     
     private static String getSubject(JsonObject programObject) {
@@ -236,8 +307,8 @@ public class ArteJsonObjectToDatenFilmCallable implements Callable<DatenFilm>
     
     private DatenFilm createFilm(String thema, String urlWeb, String titel, ArteVideoDTO video, String broadcastBegin, LocalTime durationAsTime, String beschreibung) {
         
-        String date = MserverDatumZeit.formatDate(broadcastBegin, broadcastDate);
-        String time = MserverDatumZeit.formatTime(broadcastBegin, broadcastDate);
+        String date = MserverDatumZeit.formatDate(broadcastBegin, broadcastDateFormat);
+        String time = MserverDatumZeit.formatTime(broadcastBegin, broadcastDateFormat);
         
         DatenFilm film = new DatenFilm(senderName, thema, urlWeb, titel, video.getUrl(Qualities.NORMAL), "" /*urlRtmp*/,
                 date, time, durationAsTime.toSecondOfDay(), beschreibung);
