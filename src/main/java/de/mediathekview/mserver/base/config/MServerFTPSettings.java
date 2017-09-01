eb78189b0dd3e7b7e002c791425272fe6156c9cb
Nicklas Wiegandt <nicklas@wiegandt.net>
2017-09-01 22:18:16 +0200

    - Fixed default configuration file "generating".

--- src/main/java/de/mediathekview/mserver/base/config/MServerFTPSettings.java
+++ src/main/java/de/mediathekview/mserver/base/config/MServerFTPSettings.java
@@ -1,181 +1,140 @@
 package de.mediathekview.mserver.base.config;
 
+import java.util.EnumMap;
 import java.util.Map;
 
 import de.mediathekview.mlib.filmlisten.FilmlistFormats;
 
-public class MServerFTPSettings
-{
+public class MServerFTPSettings {
 
-    private Boolean ftpEnabled;
-    private String ftpUrl;
-    private Map<FilmlistFormats, String> ftpTargetFilePaths;
-    private Integer ftpPort;
-    private String ftpUsername;
-    private String ftpPassword;
+	private Boolean ftpEnabled;
+	private String ftpUrl;
+	private Map<FilmlistFormats, String> ftpTargetFilePaths;
+	private Integer ftpPort;
+	private String ftpUsername;
+	private String ftpPassword;
 
-    public MServerFTPSettings()
-    {
-        super();
-        ftpEnabled = false;
-    }
+	public MServerFTPSettings() {
+		super();
+		ftpTargetFilePaths = new EnumMap<>(FilmlistFormats.class);
+		ftpEnabled = false;
+	}
 
-    public Boolean getFtpEnabled()
-    {
-        return ftpEnabled;
-    }
+	public Boolean getFtpEnabled() {
+		return ftpEnabled;
+	}
 
-    public void setFtpEnabled(final Boolean ftpEnabled)
-    {
-        this.ftpEnabled = ftpEnabled;
-    }
+	public void setFtpEnabled(final Boolean ftpEnabled) {
+		this.ftpEnabled = ftpEnabled;
+	}
 
-    public String getFtpUrl()
-    {
-        return ftpUrl;
-    }
+	public String getFtpUrl() {
+		return ftpUrl;
+	}
 
-    public void setFtpUrl(final String ftpUrl)
-    {
-        this.ftpUrl = ftpUrl;
-    }
+	public void setFtpUrl(final String ftpUrl) {
+		this.ftpUrl = ftpUrl;
+	}
 
-    public Map<FilmlistFormats, String> getFtpTargetFilePaths()
-    {
-        return ftpTargetFilePaths;
-    }
+	public Map<FilmlistFormats, String> getFtpTargetFilePaths() {
+		return ftpTargetFilePaths;
+	}
 
-    public void setFtpTargetFilePaths(final Map<FilmlistFormats, String> ftpTargetFilePaths)
-    {
-        this.ftpTargetFilePaths = ftpTargetFilePaths;
-    }
+	public void setFtpTargetFilePaths(final Map<FilmlistFormats, String> ftpTargetFilePaths) {
+		this.ftpTargetFilePaths = ftpTargetFilePaths;
+	}
 
-    public Integer getFtpPort()
-    {
-        return ftpPort;
-    }
+	public Integer getFtpPort() {
+		return ftpPort;
+	}
 
-    public void setFtpPort(final Integer ftpPort)
-    {
-        this.ftpPort = ftpPort;
-    }
+	public void setFtpPort(final Integer ftpPort) {
+		this.ftpPort = ftpPort;
+	}
 
-    public String getFtpUsername()
-    {
-        return ftpUsername;
-    }
+	public String getFtpUsername() {
+		return ftpUsername;
+	}
 
-    public void setFtpUsername(final String ftpUsername)
-    {
-        this.ftpUsername = ftpUsername;
-    }
+	public void setFtpUsername(final String ftpUsername) {
+		this.ftpUsername = ftpUsername;
+	}
 
-    public String getFtpPassword()
-    {
-        return ftpPassword;
-    }
+	public String getFtpPassword() {
+		return ftpPassword;
+	}
 
-    public void setFtpPassword(final String ftpPassword)
-    {
-        this.ftpPassword = ftpPassword;
-    }
+	public void setFtpPassword(final String ftpPassword) {
+		this.ftpPassword = ftpPassword;
+	}
 
-    @Override
-    public int hashCode()
-    {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (ftpEnabled == null ? 0 : ftpEnabled.hashCode());
-        result = prime * result + (ftpPassword == null ? 0 : ftpPassword.hashCode());
-        result = prime * result + (ftpPort == null ? 0 : ftpPort.hashCode());
-        result = prime * result + (ftpTargetFilePaths == null ? 0 : ftpTargetFilePaths.hashCode());
-        result = prime * result + (ftpUrl == null ? 0 : ftpUrl.hashCode());
-        result = prime * result + (ftpUsername == null ? 0 : ftpUsername.hashCode());
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (ftpEnabled == null ? 0 : ftpEnabled.hashCode());
+		result = prime * result + (ftpPassword == null ? 0 : ftpPassword.hashCode());
+		result = prime * result + (ftpPort == null ? 0 : ftpPort.hashCode());
+		result = prime * result + (ftpTargetFilePaths == null ? 0 : ftpTargetFilePaths.hashCode());
+		result = prime * result + (ftpUrl == null ? 0 : ftpUrl.hashCode());
+		result = prime * result + (ftpUsername == null ? 0 : ftpUsername.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(final Object obj)
-    {
-        if (this == obj)
-        {
-            return true;
-        }
-        if (obj == null)
-        {
-            return false;
-        }
-        if (!(obj instanceof MServerFTPSettings))
-        {
-            return false;
-        }
-        final MServerFTPSettings other = (MServerFTPSettings) obj;
-        if (ftpEnabled == null)
-        {
-            if (other.ftpEnabled != null)
-            {
-                return false;
-            }
-        }
-        else if (!ftpEnabled.equals(other.ftpEnabled))
-        {
-            return false;
-        }
-        if (ftpPassword == null)
-        {
-            if (other.ftpPassword != null)
-            {
-                return false;
-            }
-        }
-        else if (!ftpPassword.equals(other.ftpPassword))
-        {
-            return false;
-        }
-        if (ftpPort == null)
-        {
-            if (other.ftpPort != null)
-            {
-                return false;
-            }
-        }
-        else if (!ftpPort.equals(other.ftpPort))
-        {
-            return false;
-        }
-        if (ftpTargetFilePaths == null)
-        {
-            if (other.ftpTargetFilePaths != null)
-            {
-                return false;
-            }
-        }
-        else if (!ftpTargetFilePaths.equals(other.ftpTargetFilePaths))
-        {
-            return false;
-        }
-        if (ftpUrl == null)
-        {
-            if (other.ftpUrl != null)
-            {
-                return false;
-            }
-        }
-        else if (!ftpUrl.equals(other.ftpUrl))
-        {
-            return false;
-        }
-        if (ftpUsername == null)
-        {
-            if (other.ftpUsername != null)
-            {
-                return false;
-            }
-        }
-        else if (!ftpUsername.equals(other.ftpUsername))
-        {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof MServerFTPSettings)) {
+			return false;
+		}
+		final MServerFTPSettings other = (MServerFTPSettings) obj;
+		if (ftpEnabled == null) {
+			if (other.ftpEnabled != null) {
+				return false;
+			}
+		} else if (!ftpEnabled.equals(other.ftpEnabled)) {
+			return false;
+		}
+		if (ftpPassword == null) {
+			if (other.ftpPassword != null) {
+				return false;
+			}
+		} else if (!ftpPassword.equals(other.ftpPassword)) {
+			return false;
+		}
+		if (ftpPort == null) {
+			if (other.ftpPort != null) {
+				return false;
+			}
+		} else if (!ftpPort.equals(other.ftpPort)) {
+			return false;
+		}
+		if (ftpTargetFilePaths == null) {
+			if (other.ftpTargetFilePaths != null) {
+				return false;
+			}
+		} else if (!ftpTargetFilePaths.equals(other.ftpTargetFilePaths)) {
+			return false;
+		}
+		if (ftpUrl == null) {
+			if (other.ftpUrl != null) {
+				return false;
+			}
+		} else if (!ftpUrl.equals(other.ftpUrl)) {
+			return false;
+		}
+		if (ftpUsername == null) {
+			if (other.ftpUsername != null) {
+				return false;
+			}
+		} else if (!ftpUsername.equals(other.ftpUsername)) {
+			return false;
+		}
+		return true;
+	}
 }
--- src/main/java/de/mediathekview/mserver/ui/config/MServerConfigUI.java
+++ src/main/java/de/mediathekview/mserver/ui/config/MServerConfigUI.java
@@ -1,7 +1,7 @@
 package de.mediathekview.mserver.ui.config;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,7 +11,6 @@ import java.util.List;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.yaml.snakeyaml.Yaml;
 
 import de.mediathekview.mlib.messages.listener.LogMessageListener;
 import de.mediathekview.mlib.messages.listener.MessageListener;
@@ -22,104 +21,86 @@ import de.mediathekview.mserver.base.messages.ServerMessages;
 import de.mediathekview.mserver.crawler.CrawlerManager;
 import de.mediathekview.mserver.progress.listeners.ProgressLogMessageListener;
 
-public final class MServerConfigUI
-{
+public final class MServerConfigUI {
 
-    private static final Logger LOG = LogManager.getLogger(MServerConfigUI.class);
-    private static final String CONFIG_FILE_NAME = "MServer-Config.yaml";
-    private static final String ARGUMENT_GCONF = "-gconf";
-    private final LogMessageListener logMessageListener;
-    private final MServerConfigDTO config;
+	private static final Logger LOG = LogManager.getLogger(MServerConfigUI.class);
+	private static final String CONFIG_FILE_NAME = "MServer-Config.yaml";
+	private static final String ARGUMENT_GCONF = "-gconf";
+	private final LogMessageListener logMessageListener;
+	private final MServerConfigDTO config;
 
-    public static void main(final String[] args)
-    {
-        new MServerConfigUI().start(args);
-    }
+	public static void main(final String[] args) {
+		new MServerConfigUI().start(args);
+	}
 
-    public MServerConfigUI()
-    {
-        super();
-        config = MServerConfigManager.getInstance().getConfig();
-        final MServerLogSettingsDTO logSettings = config.getLogSettings();
-        logSettings.setLogActivateConsole(true);
+	public MServerConfigUI() {
+		super();
+		config = MServerConfigManager.getInstance().getConfig();
+		final MServerLogSettingsDTO logSettings = config.getLogSettings();
+		logSettings.setLogActivateConsole(true);
 
-        logMessageListener = new LogMessageListener();
+		logMessageListener = new LogMessageListener();
 
-        final Level configLevel = logSettings.getLogLevelConsole();
-        if (configLevel == null || !logLevelInfoOrLower(configLevel))
-        {
-            logSettings.setLogLevelConsole(Level.INFO);
-        }
+		final Level configLevel = logSettings.getLogLevelConsole();
+		if (configLevel == null || !logLevelInfoOrLower(configLevel)) {
+			logSettings.setLogLevelConsole(Level.INFO);
+		}
 
-    }
+	}
 
-    private boolean logLevelInfoOrLower(final Level configLevel)
-    {
-        return Level.INFO.equals(configLevel) || Level.DEBUG.equals(configLevel) || Level.ALL.equals(configLevel);
-    }
+	private boolean logLevelInfoOrLower(final Level configLevel) {
+		return Level.INFO.equals(configLevel) || Level.DEBUG.equals(configLevel) || Level.ALL.equals(configLevel);
+	}
 
-    void start(final String[] aProgramAgruments)
-    {
-        if (interpretProgramArguments(aProgramAgruments))
-        {
-            final CrawlerManager manager = CrawlerManager.getInstance();
-            addListeners(manager);
-            manager.start();
-            manager.importFilmlist();
-            manager.saveFilmlist();
-            manager.uploadFilmlist();
-        }
-    }
+	void start(final String[] aProgramAgruments) {
+		if (interpretProgramArguments(aProgramAgruments)) {
+			final CrawlerManager manager = CrawlerManager.getInstance();
+			addListeners(manager);
+			manager.start();
+			manager.importFilmlist();
+			manager.saveFilmlist();
+			manager.uploadFilmlist();
+		}
+	}
 
-    private void addListeners(final CrawlerManager manager)
-    {
-        final List<ProgressLogMessageListener> progressListeners = new ArrayList<>();
-        progressListeners.add(new ProgressLogMessageListener());
+	private void addListeners(final CrawlerManager manager) {
+		final List<ProgressLogMessageListener> progressListeners = new ArrayList<>();
+		progressListeners.add(new ProgressLogMessageListener());
 
-        final List<MessageListener> messageListeners = new ArrayList<>();
-        messageListeners.add(logMessageListener);
-        manager.addAllProgressListener(progressListeners);
-        manager.addAllMessageListener(messageListeners);
-    }
+		final List<MessageListener> messageListeners = new ArrayList<>();
+		messageListeners.add(logMessageListener);
+		manager.addAllProgressListener(progressListeners);
+		manager.addAllMessageListener(messageListeners);
+	}
 
-    private boolean interpretProgramArguments(final String[] aProgramAgruments)
-    {
-        if (aProgramAgruments != null && aProgramAgruments.length > 0)
-        {
-            if (aProgramAgruments.length > 1)
-            {
-                logMessageListener.consumeMessage(ServerMessages.UI_TO_MANY_ARGUMENTS);
-            }
+	private boolean interpretProgramArguments(final String[] aProgramAgruments) {
+		if (aProgramAgruments != null && aProgramAgruments.length > 0) {
+			if (aProgramAgruments.length > 1) {
+				logMessageListener.consumeMessage(ServerMessages.UI_TO_MANY_ARGUMENTS);
+			}
 
-            if (ARGUMENT_GCONF.equals(aProgramAgruments[0]))
-            {
-                generateDefaultConfiguration();
-            }
-            else
-            {
-                logMessageListener.consumeMessage(ServerMessages.UI_UNKNOWN_ARGUMENT, aProgramAgruments[0],
-                        ARGUMENT_GCONF);
-                return false;
-            }
-        }
+			if (ARGUMENT_GCONF.equals(aProgramAgruments[0])) {
+				generateDefaultConfiguration();
+			} else {
+				logMessageListener.consumeMessage(ServerMessages.UI_UNKNOWN_ARGUMENT, aProgramAgruments[0],
+						ARGUMENT_GCONF);
+				return false;
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    private void generateDefaultConfiguration()
-    {
-        final Path configFilePath = Paths.get(CONFIG_FILE_NAME);
-        try
-        {
-            final Yaml yaml = new Yaml();
-            yaml.dump(config, Files.newBufferedWriter(configFilePath, StandardCharsets.UTF_8));
-        }
-        catch (final IOException ioException)
-        {
-            LOG.error("The default configuration can't be generated.", ioException);
-            logMessageListener.consumeMessage(ServerMessages.UI_GENERATE_DEFAULT_CONFIG_FILE_FAILED,
-                    configFilePath.toAbsolutePath().toString());
-        }
-    }
+	private void generateDefaultConfiguration() {
+		final Path configFilePath = Paths.get(CONFIG_FILE_NAME);
+		try {
+			Path defaultConfigFile = Paths.get(this.getClass().getClassLoader().getResource(CONFIG_FILE_NAME).toURI());
+			Files.copy(defaultConfigFile, configFilePath);
+		} catch (final IOException | URISyntaxException ioException) {
+			LOG.error("The default configuration can't be generated.", ioException);
+			logMessageListener.consumeMessage(ServerMessages.UI_GENERATE_DEFAULT_CONFIG_FILE_FAILED,
+					configFilePath.toAbsolutePath().toString());
+		}
+	}
 
 }
