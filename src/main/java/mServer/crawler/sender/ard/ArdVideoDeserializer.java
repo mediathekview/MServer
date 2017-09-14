4657da7f0b84433731a81227367cc09f7f46267a
pidoubleyou <pidoubleyou@arcor.de>
2017-09-14 23:45:09 +0200

    Deserializer für Video-URLs erweitert
    Deserializer für Video-URLs erweitert, um gleiche URLs wie bisher zu
    gewährleisten, sofern diese korrekt waren

--- src/main/java/mServer/crawler/sender/ard/ArdVideoDeserializer.java
+++ src/main/java/mServer/crawler/sender/ard/ArdVideoDeserializer.java
@@ -7,6 +7,8 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 import mServer.crawler.sender.newsearch.Qualities;
 
 public class ArdVideoDeserializer implements JsonDeserializer<ArdVideoDTO> {
@@ -16,6 +18,8 @@ public class ArdVideoDeserializer implements JsonDeserializer<ArdVideoDTO> {
     private static final String JSON_OBJECT_MEDIASTREAMARRAY = "_mediaStreamArray";
     private static final String JSON_ELEMENT_QUALITY = "_quality";
     private static final String JSON_ELEMENT_STREAM = "_stream";
+    private static final String JSON_ELEMENT_WIDTH = "_width";
+    private static final String JSON_ELEMENT_HEIGHT = "_height";
     
     @Override
     public ArdVideoDTO deserialize(JsonElement aJsonElement, Type aType, JsonDeserializationContext aContext) throws JsonParseException {
@@ -24,13 +28,20 @@ public class ArdVideoDeserializer implements JsonDeserializer<ArdVideoDTO> {
         if (aJsonElement.isJsonObject()) {
             JsonObject jsonObject = aJsonElement.getAsJsonObject();
             if (jsonObject.has(JSON_OBJECT_MEDIAARRAY)) {
+                Map<Qualities, UrlWidthHeightDTO> urlMap = new HashMap<>();
+                
                 JsonArray mediaArray = jsonObject.get(JSON_OBJECT_MEDIAARRAY).getAsJsonArray();
                 mediaArray.forEach(mediaArrayItem -> {
                     JsonObject item = mediaArrayItem.getAsJsonObject();
                     JsonArray streamArray = item.get(JSON_OBJECT_MEDIASTREAMARRAY).getAsJsonArray();
                     streamArray.forEach(streamItem -> {
-                        deserializeMediaStreamArrayItem(streamItem.getAsJsonObject(), dto);
+                        deserializeMediaStreamArrayItem(streamItem.getAsJsonObject(), urlMap);
                     });
+                });
+                
+                // URLs setzen
+                urlMap.forEach((key, value) -> {
+                    dto.addVideo(key, value.url);
                 });
             }
         }
@@ -38,23 +49,56 @@ public class ArdVideoDeserializer implements JsonDeserializer<ArdVideoDTO> {
         return dto;
     }    
     
-    private static void deserializeMediaStreamArrayItem(JsonObject stream, ArdVideoDTO dto) {
+    private void deserializeMediaStreamArrayItem(JsonObject stream, Map<Qualities, UrlWidthHeightDTO> urlMap) {
+        
         String qualityValue = stream.get(JSON_ELEMENT_QUALITY).getAsString();
-        String url;
 
-        JsonElement streamElement = stream.get(JSON_ELEMENT_STREAM);
-        if(streamElement.isJsonArray()) {
-            url = streamElement.getAsJsonArray().get(0).getAsString();
-        } else {
-            url = streamElement.getAsString();
-        }
+        UrlWidthHeightDTO urlInfos = deserializeUrlInfos(stream);
 
-        Qualities quality = convertQuality(qualityValue);
-        if (quality != null) {
-            if (dto.getUrl(quality) == null) {
-                dto.addVideo(quality, addMissingHttpPrefixIfNecessary(url));
+        Qualities quality = convertQuality(qualityValue, urlInfos);
+        // nur relevante Qualitäten berücksichtigen und nur http-Links (manchmal sind "mp3:"-Links enthalten)
+        if (quality != null && urlInfos.url.startsWith(TEXT_START_HTTP)) {
+            if (urlMap.containsKey(quality)) {
+                UrlWidthHeightDTO actualUrlInfos = urlMap.get(quality);
+                
+                // prüfen, ob die Auflösung besser ist, als die bisher für die Qualität hinterlegte
+                if (urlInfos.width > 0 && urlInfos.width > actualUrlInfos.width) {
+                   urlMap.put(quality, urlInfos);
+                } else if (quality == Qualities.NORMAL) {
+                    // bei normaler Auflösung ist aus irgendeinem Grund der letzte
+                    // Eintrag immer der beste!
+                    urlMap.put(quality, urlInfos);
+                }
+            } else {
+                urlMap.put(quality, urlInfos);
             }
         }        
+    }
+    
+    private UrlWidthHeightDTO deserializeUrlInfos(JsonObject stream) {
+        UrlWidthHeightDTO dto = new UrlWidthHeightDTO();
+        
+        // Url ermitteln
+        // Wenn es ein Array ist, den letzten Eintrag nehmen, der hat im Zweifel
+        // die bessere Auflösung!
+        JsonElement streamElement = stream.get(JSON_ELEMENT_STREAM);
+        if(streamElement.isJsonArray()) {
+            JsonArray streamArray = streamElement.getAsJsonArray();
+            dto.url = streamArray.get(streamArray.size()-1).getAsString();
+        } else {
+            dto.url = streamElement.getAsString();
+        }
+        dto.url = addMissingHttpPrefixIfNecessary(dto.url);
+        
+        // Auflösung ermitteln
+        if( stream.has(JSON_ELEMENT_WIDTH)) {
+            dto.width = stream.get(JSON_ELEMENT_WIDTH).getAsInt();
+        }
+        if( stream.has(JSON_ELEMENT_HEIGHT)) {
+            dto.height = stream.get(JSON_ELEMENT_HEIGHT).getAsInt();
+        }        
+        
+        return dto;
     }
     
     private static String addMissingHttpPrefixIfNecessary(String aUrl) {
@@ -65,15 +109,30 @@ public class ArdVideoDeserializer implements JsonDeserializer<ArdVideoDTO> {
         return aUrl;
     }
 
-    private static Qualities convertQuality(String quality) {
+    private static Qualities convertQuality(String quality, UrlWidthHeightDTO urlInfos) {
         switch(quality) {
             case "1":
                 return Qualities.SMALL;
             case "2":
                 return Qualities.NORMAL;
             case "3":
-                return Qualities.HD;
+                // Beste Qualität, aber nicht immer ist die Auflösung auch HD
+                // Die Prüfung auf den Dateinamen der URL filtert noch zusätzlich
+                // Filme heraus, die eine Auflösung von 960x... haben
+                if ((urlInfos.width == 0 && urlInfos.height == 0 && !urlInfos.url.substring(urlInfos.url.lastIndexOf("/") + 1).startsWith("960"))
+                    || (urlInfos.width >= 1280 && urlInfos.height >= 720)) {
+                    return Qualities.HD;
+                } else {
+                    return Qualities.NORMAL;
+                }
         }
         return null;
+    }
+    
+    // Hilfsklasse für Zwischenspeichern von URL und zugehörigen Auflösungsinfos
+    private class UrlWidthHeightDTO {
+        public String url = "";
+        public int width;
+        public int height;
     }
 }
--- src/test/developTest/java/mServer/crawler/sender/ard/ArdVideoDeserializerTest.java
+++ src/test/developTest/java/mServer/crawler/sender/ard/ArdVideoDeserializerTest.java
@@ -18,7 +18,11 @@ public class ArdVideoDeserializerTest {
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-            { "/ard/ard_video.json", "http://wdrmedien-a.akamaihd.net/medp/ondemand/weltweit/fsk0/147/1471174/1471174_16874993.mp4", "http://wdrmedien-a.akamaihd.net/medp/ondemand/weltweit/fsk0/147/1471174/1471174_16874994.mp4", "http://wdrmedien-a.akamaihd.net/medp/ondemand/weltweit/fsk0/147/1471174/1471174_16874995.mp4" }
+            { "/ard/ard_video_without_hd.json", "http://wdrmedien-a.akamaihd.net/medp/ondemand/weltweit/fsk0/147/1471174/1471174_16874993.mp4", "http://wdrmedien-a.akamaihd.net/medp/ondemand/weltweit/fsk0/147/1471174/1471174_16874995.mp4", null },
+            { "/ard/ard_video_with_hd.json", "http://ondemand.mdr.de/mp4dyn/1/FCMS-14ec1b13-c7f6-4cc4-8b72-681cd22da39a-e9ebd6e42ce1_14.mp4", "http://ondemand.mdr.de/mp4dyn/1/FCMS-14ec1b13-c7f6-4cc4-8b72-681cd22da39a-c7cca1d51b4b_14.mp4", "http://ondemand.mdr.de/mp4dyn/1/FCMS-14ec1b13-c7f6-4cc4-8b72-681cd22da39a-15e7604ea4a4_14.mp4" },
+            { "/ard/ard_video_normal_use_last.json", "https://mediastorage01.sr-online.de/Video/UD/DOKU/1505155201_20170911_KANDIDATENCHECK_LUKSIC_M.mp4", "https://srstorage01-a.akamaihd.net/Video/UD/DOKU/1505155201_20170911_KANDIDATENCHECK_LUKSIC_L.mp4", "https://srstorage01-a.akamaihd.net/Video/UD/DOKU/1505155201_20170911_KANDIDATENCHECK_LUKSIC_P.mp4" },
+            { "/ard/ard_video_use_http_url.json", "http://cdn-storage.br.de/iLCpbHJGNL9zu6i6NL97bmWH_-bf/_-0S/_Abg5-xg5U1S/0f131ba9-c8e1-4368-be7b-799a75df221f_2.mp3", null, null },
+            { "/ard/ard_video_with_quality_3_no_hd.json", "http://pd-videos.daserste.de/int/2017/09/08/7f35d2d7-9854-4187-b406-c56b9292de79/512-1.mp4", "http://pd-videos.daserste.de/int/2017/09/08/7f35d2d7-9854-4187-b406-c56b9292de79/960-1.mp4", null }
         });
     }
     
