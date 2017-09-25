94cec589f3a4fcac321c73763497da86b6da1c99
pidoubleyou <pidoubleyou@arcor.de>
2017-09-25 19:08:13 +0200

    Neue Logik für Sendungsseite

---
+++ src/main/java/mServer/crawler/sender/hr/HrSendungDeserializer.java
@@ -0,0 +1,109 @@
+package mServer.crawler.sender.hr;
+
+import de.mediathekview.mlib.Const;
+import de.mediathekview.mlib.daten.DatenFilm;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class HrSendungDeserializer {
+    
+    private static final String QUERY_BROADCAST = "li.c-airdates__entry";
+    private static final String QUERY_DESCRIPTION = "p.copytext__text";
+    private static final String QUERY_TITLE = "p.c-programHeader__subline";
+    private static final String HTML_TAG_SOURCE = "source";
+    private static final String HTML_TAG_STRONG = "strong";
+    private static final String HTML_TAG_TIME = "time";
+    private static final String HTML_TAG_VIDEO = "video";
+    private static final String HTML_ATTRIBUTE_DURATION = "data-duration";
+    private static final String HTML_ATTRIBUTE_SRC = "src";
+
+    public DatenFilm deserialize(String theme, String documentUrl, Document document) {
+        
+        String broadcast = getBroadcast(document);
+        String title = getTitle(document);
+        String videoUrl = getVideoUrl(document);
+        long duration = getDuration(document);
+        String description = getDescription(document);
+        
+        DatenFilm film = new DatenFilm(Const.HR, theme, documentUrl, title, videoUrl, "", broadcast, broadcast, duration, description);
+        
+        return film;
+    }
+    
+    private String getBroadcast(Document document) {
+        String broadcast = "";
+        
+        Elements broadcastElements = document.select(QUERY_BROADCAST);
+        
+        for(int i = 0; i < broadcastElements.size(); i++) {
+            Elements children = broadcastElements.get(i).children();
+        
+            for(int j = 0; j < children.size(); j++) {
+                Element child = children.get(j);
+                
+                if(child.tagName().compareToIgnoreCase(HTML_TAG_TIME) == 0) {
+                    broadcast = child.text();
+                }
+            }
+        }
+        
+        return broadcast;
+    }
+    
+    private String getDescription(Document document) {
+        String desc = "";
+        
+        Element descElement = document.select(QUERY_DESCRIPTION).first();
+        if(descElement != null) {
+            Elements children = descElement.children();
+            if(children.size() > 0) {
+                for (int i = 0; i < children.size(); i++) {
+                    if (children.get(i).tagName().compareToIgnoreCase(HTML_TAG_STRONG) == 0) {
+                        desc = children.get(i).text();
+                    }
+                }
+            } else {
+                desc = descElement.text();
+            }
+        }
+        
+        return desc;
+    }
+    
+    private long getDuration(Document document) {
+        String duration = "";
+        
+        Element durationElement = document.select(HTML_TAG_VIDEO).first();
+        if (durationElement != null) {
+            duration = durationElement.attr(HTML_ATTRIBUTE_DURATION);
+        }
+        
+        if (duration != null && !duration.isEmpty()) {
+            return Long.parseLong(duration);
+        } 
+        return 0;
+    }
+    
+    private String getTitle(Document document) {
+        String title = "";
+        
+        Element titleElement = document.select(QUERY_TITLE).first();
+        if(titleElement != null) {
+            title = titleElement.text();
+        }
+        
+        return title;
+    }
+    
+        private String getVideoUrl(Document document) {
+        String url = "";
+        
+        Element urlElement = document.select(HTML_TAG_SOURCE).first();
+        if(urlElement != null) {
+            url = urlElement.attr(HTML_ATTRIBUTE_SRC);
+        }
+        
+        return url;
+    }
+}
