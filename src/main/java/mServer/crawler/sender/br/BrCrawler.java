f672aade253df9df4548bc134d1f88bc59ae3567
pidoubleyou <pidoubleyou@arcor.de>
2017-11-03 23:16:21 +0100

    ForkJoinPool explizit beenden

--- src/main/java/mServer/crawler/sender/br/BrCrawler.java
+++ src/main/java/mServer/crawler/sender/br/BrCrawler.java
@@ -7,9 +7,11 @@ import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.TimeUnit;
 import mServer.crawler.CrawlerTool;
 import mServer.crawler.FilmeSuchen;
 import mServer.crawler.sender.MediathekReader;
@@ -24,7 +26,7 @@ public class BrCrawler extends MediathekReader {
   private final ForkJoinPool forkJoinPool;
   
   public BrCrawler(FilmeSuchen ssearch, int startPrio) {
-    super(ssearch, SENDERNAME, 1, 100, startPrio);
+    super(ssearch, SENDERNAME, 0, 100, startPrio);
         
     forkJoinPool = new ForkJoinPool(Runtime.getRuntime().availableProcessors() * 4);
   }
@@ -33,18 +35,44 @@ public class BrCrawler extends MediathekReader {
   protected void addToList() {
     meldungStart();
     
-    RecursiveTask<Set<DatenFilm>> filmTask = createCrawlerTask();
-    Set<DatenFilm> films = forkJoinPool.invoke(filmTask);
+    try {
+      RecursiveTask<Set<DatenFilm>> filmTask = createCrawlerTask();
+      Set<DatenFilm> films = forkJoinPool.invoke(filmTask);
+
+      LOG.info("BR Filme einsortieren...");
+
+      films.forEach(film -> {
+        if(!Config.getStop()) {
+          addFilm(film);
+        }
+      });
+
+      LOG.info("BR Film einsortieren fertig");
+    } finally {
+      //explicitely shutdown the pool
+      shutdownAndAwaitTermination(forkJoinPool, 60, TimeUnit.SECONDS);
+    }
     
-    films.forEach(film -> {
-      if(!Config.getStop()) {
-        addFilm(film);
-      }
-    });
+    LOG.info("BR fertig");
     
     meldungThreadUndFertig();
   }
 
+  void shutdownAndAwaitTermination(ExecutorService pool, long delay, TimeUnit delayUnit) {
+    pool.shutdown();
+    try {
+      if (!pool.awaitTermination(delay, delayUnit)) {
+        pool.shutdownNow();
+        if (!pool.awaitTermination(delay, delayUnit)) {
+          LOG.info("Pool nicht beendet");
+        }
+      }
+    } catch (InterruptedException ie) {
+      pool.shutdownNow();
+      Thread.currentThread().interrupt();
+    }
+  }
+  
   private RecursiveTask<Set<String>> createAllSendungenOverviewCrawler() {
     return new BrAllSendungenTask(this, forkJoinPool);
   }
