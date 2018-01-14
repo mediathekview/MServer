89a7489f108483c305a8d3e962c9bade9b1b9e1d
Nicklas Wiegandt <nicklas@wiegandt.net>
2018-01-14 15:03:52 +0100

    Begon with new Arte crawler #311

---
+++ src/main/java/de/mediathekview/mserver/crawler/arte/ArteCrawler_FR.java
@@ -0,0 +1,24 @@
+package de.mediathekview.mserver.crawler.arte;
+
+import java.util.Collection;
+import java.util.concurrent.ForkJoinPool;
+import de.mediathekview.mlib.daten.Sender;
+import de.mediathekview.mlib.messages.listener.MessageListener;
+import de.mediathekview.mserver.base.config.MServerConfigManager;
+import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
+
+public class ArteCrawler_FR extends ArteCrawler {
+
+  public ArteCrawler_FR(final ForkJoinPool aForkJoinPool,
+      final Collection<MessageListener> aMessageListeners,
+      final Collection<SenderProgressListener> aProgressListeners,
+      final MServerConfigManager rootConfig) {
+    super(aForkJoinPool, aMessageListeners, aProgressListeners, rootConfig);
+  }
+
+  @Override
+  public Sender getSender() {
+    return Sender.ARTE_FR;
+  }
+
+}
