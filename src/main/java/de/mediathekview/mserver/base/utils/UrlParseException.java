29e350df01e903f49c392d5c35f5685229a7e3c1
pidoubleyou <pidoubleyou@arcor.de>
2018-01-20 18:38:51 +0100

    UrlUtils erweitert um getUrlParamterValue

---
+++ src/main/java/de/mediathekview/mserver/base/utils/UrlParseException.java
@@ -0,0 +1,8 @@
+package de.mediathekview.mserver.base.utils;
+
+public class UrlParseException extends Exception {
+
+  public UrlParseException(final String aMessage) {
+    super(aMessage);
+  }  
+}
