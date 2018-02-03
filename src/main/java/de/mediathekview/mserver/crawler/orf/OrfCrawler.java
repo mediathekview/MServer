b2e865d79a1881919ebbc1effa7ce21c9a847d3c
pidoubleyou <pidoubleyou@arcor.de>
2018-02-04 00:02:35 +0100

    ORF: Sendung verpasst angebunden

--- src/main/java/de/mediathekview/mserver/crawler/orf/OrfCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/orf/OrfCrawler.java
@@ -6,9 +6,14 @@ import de.mediathekview.mlib.messages.listener.MessageListener;
 import de.mediathekview.mserver.base.config.MServerConfigManager;
 import de.mediathekview.mserver.base.messages.ServerMessages;
 import de.mediathekview.mserver.crawler.basic.AbstractCrawler;
+import de.mediathekview.mserver.crawler.basic.CrawlerUrlDTO;
+import de.mediathekview.mserver.crawler.orf.tasks.OrfDayTask;
 import de.mediathekview.mserver.crawler.orf.tasks.OrfFilmDetailTask;
 import de.mediathekview.mserver.crawler.orf.tasks.OrfLetterPageTask;
 import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -36,7 +41,20 @@ public class OrfCrawler extends AbstractCrawler {
     try {
       OrfLetterPageTask letterTask = new OrfLetterPageTask();
       ConcurrentLinkedQueue<OrfTopicUrlDTO> shows = forkJoinPool.submit(letterTask).get();
-     
+
+      printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+
+      OrfDayTask dayTask = new OrfDayTask(this, getDayUrls());
+      Set<OrfTopicUrlDTO> shows1 = forkJoinPool.submit(dayTask).get();
+      
+      printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows1.size());
+      
+      shows1.forEach(show -> {
+        if (!shows.contains(show)) {
+          shows.add(show);
+        }
+      });
+      
       printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
       getAndSetMaxCount(shows.size());
       
@@ -45,5 +63,15 @@ public class OrfCrawler extends AbstractCrawler {
       LOG.fatal("Exception in ORF crawler.", ex);
     }
     return null;
+  }
+  
+  private ConcurrentLinkedQueue<CrawlerUrlDTO> getDayUrls() {
+    final ConcurrentLinkedQueue<CrawlerUrlDTO> urls = new ConcurrentLinkedQueue<>();
+    for (int i = 0; i < crawlerConfig.getMaximumDaysForSendungVerpasstSection(); i++) {
+      urls.add(new CrawlerUrlDTO(OrfConstants.URL_DAY + 
+          LocalDateTime.now().minus(i, ChronoUnit.DAYS).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
+    }
+
+    return urls;
   }
 }
