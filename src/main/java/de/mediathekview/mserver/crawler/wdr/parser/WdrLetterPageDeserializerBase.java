a4ec4771e930a3975f5a69588373cb1d65dbd4a6
pidoubleyou <pidoubleyou@arcor.de>
2018-02-18 09:17:30 +0100

    WDR: Fix Typisierung LetterPage-URLs

--- src/main/java/de/mediathekview/mserver/crawler/wdr/parser/WdrLetterPageDeserializerBase.java
+++ src/main/java/de/mediathekview/mserver/crawler/wdr/parser/WdrLetterPageDeserializerBase.java
@@ -4,10 +4,17 @@ import org.jsoup.nodes.Element;
 
 public abstract class WdrLetterPageDeserializerBase {
   
-  private static final String SELECTOR_URL_TYPE = "p.teasertext > strong";
+  private static final String SELECTOR_URL_TYPE1 = "p.teasertext > strong";
+  private static final String SELECTOR_URL_TYPE2 = "> strong";
 
   protected boolean isFileUrl(final Element aTopicElement) {
-    Element typeElement = aTopicElement.select(SELECTOR_URL_TYPE).first();
+    Element typeElement = aTopicElement.select(SELECTOR_URL_TYPE1).first();
+    if (typeElement != null) {
+      final String type = typeElement.text();
+      return type.equalsIgnoreCase("video");
+    }
+    
+    typeElement = aTopicElement.select(SELECTOR_URL_TYPE2).first();
     if (typeElement != null) {
       final String type = typeElement.text();
       return type.equalsIgnoreCase("video");
