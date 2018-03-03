5ccd5c1d71744a111b202ce757cf77aba30680a9
pidoubleyou <pidoubleyou@arcor.de>
2018-03-03 18:27:27 +0100

    ZDF: alten Crawler entfernt

--- src/main/java/mServer/crawler/sender/zdf/NoDownloadInformationException.java
+++
@@ -1,9 +0,0 @@
-package mServer.crawler.sender.zdf;
-
-public class NoDownloadInformationException extends Exception
-{
-    public NoDownloadInformationException()
-    {
-        super("A film without any download information was found.");
-    }
-}
