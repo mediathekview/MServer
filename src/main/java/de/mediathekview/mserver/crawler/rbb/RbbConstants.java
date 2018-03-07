e397b4dc56f6c337de9874533346f9a994a770e6
pidoubleyou <pidoubleyou@arcor.de>
2018-03-07 23:51:44 +0100

    RBB: Sendung verpasst angebunden

--- src/main/java/de/mediathekview/mserver/crawler/rbb/RbbConstants.java
+++ src/main/java/de/mediathekview/mserver/crawler/rbb/RbbConstants.java
@@ -8,6 +8,16 @@ public final class RbbConstants {
   public static final String URL_BASE = "http://mediathek.rbb-online.de";
 
   /**
+   * maximum number of days to go back from today supported by RBB if using {@Link URL_DAY_PAGE}.
+   */
+  public static final int MAX_SUPPORTED_DAYS_PAST = 6;
+
+  /**
+   * The url of the videos broadcasted on a day. Parameter: number of days to go back from today (0-6)
+   */
+  public static final String URL_DAY_PAGE = URL_BASE + "/tv/sendungVerpasst?tag=%d&topRessort=tv";
+
+  /**
    * The topic page for letters A-K.
    */
   public static final String URL_TOPICS_A_K = URL_BASE + "/tv/sendungen-a-z?cluster=a-k";
@@ -22,5 +32,6 @@ public final class RbbConstants {
    */
   public static final String URL_VIDEO_JSON = URL_BASE + "/play/media/%s?devicetype=pc&features=hls";
 
-  private RbbConstants() {}
+  private RbbConstants() {
+  }
 }
--- src/main/java/de/mediathekview/mserver/crawler/rbb/RbbCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/rbb/RbbCrawler.java
@@ -7,7 +7,7 @@ import de.mediathekview.mserver.base.config.MServerConfigManager;
 import de.mediathekview.mserver.base.messages.ServerMessages;
 import de.mediathekview.mserver.crawler.basic.AbstractCrawler;
 import de.mediathekview.mserver.crawler.basic.CrawlerUrlDTO;
-import de.mediathekview.mserver.crawler.basic.TopicUrlDTO;
+import de.mediathekview.mserver.crawler.rbb.tasks.RbbDayTask;
 import de.mediathekview.mserver.crawler.rbb.tasks.RbbFilmTask;
 import de.mediathekview.mserver.crawler.rbb.tasks.RbbTopicOverviewTask;
 import de.mediathekview.mserver.crawler.rbb.tasks.RbbTopicsOverviewTask;
@@ -26,9 +26,9 @@ public class RbbCrawler extends AbstractCrawler {
   private static final Logger LOG = LogManager.getLogger(RbbCrawler.class);
 
   public RbbCrawler(ForkJoinPool aForkJoinPool,
-          Collection<MessageListener> aMessageListeners,
-          Collection<SenderProgressListener> aProgressListeners,
-          MServerConfigManager rootConfig) {
+      Collection<MessageListener> aMessageListeners,
+      Collection<SenderProgressListener> aProgressListeners,
+      MServerConfigManager rootConfig) {
     super(aForkJoinPool, aMessageListeners, aProgressListeners, rootConfig);
   }
 
@@ -40,8 +40,16 @@ public class RbbCrawler extends AbstractCrawler {
   @Override
   protected RecursiveTask<Set<Film>> createCrawlerTask() {
     try {
-      ConcurrentLinkedQueue<TopicUrlDTO> shows = new ConcurrentLinkedQueue<>();
+      ConcurrentLinkedQueue<CrawlerUrlDTO> shows = new ConcurrentLinkedQueue<>();
       shows.addAll(getTopicsPageEntries());
+
+      printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+
+      getDaysEntries().forEach(show -> {
+        if (!shows.contains(show)) {
+          shows.add(show);
+        }
+      });
 
       printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
       getAndSetMaxCount(shows.size());
@@ -54,12 +62,32 @@ public class RbbCrawler extends AbstractCrawler {
     return null;
   }
 
-  private Set<TopicUrlDTO> getTopicsPageEntries() throws ExecutionException, InterruptedException {
+  private Set<CrawlerUrlDTO> getDaysEntries() throws ExecutionException, InterruptedException {
+    RbbDayTask dayTask = new RbbDayTask(this, getDayUrls());
+    Set<CrawlerUrlDTO> shows = forkJoinPool.submit(dayTask).get();
+
+    printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+
+    return shows;
+  }
+
+  private ConcurrentLinkedQueue<CrawlerUrlDTO> getDayUrls() {
+    final ConcurrentLinkedQueue<CrawlerUrlDTO> urls = new ConcurrentLinkedQueue<>();
+    for (int i = 0;
+        i <= crawlerConfig.getMaximumDaysForSendungVerpasstSection() && i <= RbbConstants.MAX_SUPPORTED_DAYS_PAST;
+        i++) {
+      urls.add(new CrawlerUrlDTO(String.format(RbbConstants.URL_DAY_PAGE, i)));
+    }
+
+    return urls;
+  }
+
+  private Set<CrawlerUrlDTO> getTopicsPageEntries() throws ExecutionException, InterruptedException {
     final ConcurrentLinkedQueue<CrawlerUrlDTO> topicOverviews = new ConcurrentLinkedQueue<>();
     topicOverviews.add(new CrawlerUrlDTO(RbbConstants.URL_TOPICS_A_K));
     topicOverviews.add(new CrawlerUrlDTO(RbbConstants.URL_TOPICS_L_Z));
 
-    final ConcurrentLinkedQueue<TopicUrlDTO> topicPages = new ConcurrentLinkedQueue<>();
+    final ConcurrentLinkedQueue<CrawlerUrlDTO> topicPages = new ConcurrentLinkedQueue<>();
 
     RbbTopicsOverviewTask overviewTask = new RbbTopicsOverviewTask(this, topicOverviews);
     topicPages.addAll(forkJoinPool.submit(overviewTask).get());
