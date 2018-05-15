3ba8058fa353bbec3b83c31b7de90c879143dc38
pidoubleyou <pidoubleyou@arcor.de>
2018-05-15 22:31:06 +0200

    ARTE: Verarbeitung der Kategorien umgestellt auf neue API

--- src/main/java/mServer/crawler/sender/arte/ArteCategoryFilmsDTO.java
+++ src/main/java/mServer/crawler/sender/arte/ArteCategoryFilmsDTO.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 
 public class ArteCategoryFilmsDTO {
 
-    private final ArrayList<String> programIds = new ArrayList<>();
-    private int pages;
-    
-    public void addProgramId(String aProgramId) {
-        programIds.add(aProgramId);
-    }
-    
-    public ArrayList<String> getProgramIds() {
-        return programIds;
-    }
+  private final ArrayList<String> programIds = new ArrayList<>();
+  private boolean hasNextPage;
 
-    public int getPages() {
-        return pages;
-    }
-    
-    public void setPages(int aPages) {
-        pages = aPages;
-    }
+  public void addProgramId(String aProgramId) {
+    programIds.add(aProgramId);
+  }
+
+  public ArrayList<String> getProgramIds() {
+    return programIds;
+  }
+
+  public boolean hasNextPage() {
+    return hasNextPage;
+  }
+
+  public void setNextPage(boolean aNextPage) {
+    hasNextPage = aNextPage;
+  }
 }
