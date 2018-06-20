2f6d049e597f8b815de683d688825e7294626bca
Nicklas Wiegandt <nicklas@wiegandt.net>
2018-06-20 21:28:05 +0200

    Fixes #373

--- src/main/java/mServer/crawler/sender/orf/OrfConstants.java
+++ src/main/java/mServer/crawler/sender/orf/OrfConstants.java
@@ -2,20 +2,16 @@ package mServer.crawler.sender.orf;
 
 public final class OrfConstants {
 
-  private OrfConstants() {
-  }
-
-  public static final String URL_BASE = "http://tvthek.orf.at";
+  public static final String URL_BASE = "https://tvthek.orf.at";
 
   /**
-   * URL für die Sendungen eines Tages Muss am Ende noch um das Datum dd.MM.yyyy
-   * ergänzt werden
+   * URL für die Sendungen eines Tages Muss am Ende noch um das Datum dd.MM.yyyy ergänzt werden
    */
   public static final String URL_DAY = URL_BASE + "/schedule/";
 
   /**
-   * Basis-URL für Übersichtsseite nach Buchstaben Muss am Ende noch um
-   * Buchstabe bzw. 0 ergänzt werden
+   * Basis-URL für Übersichtsseite nach Buchstaben Muss am Ende noch um Buchstabe bzw. 0 ergänzt
+   * werden
    */
   public static final String URL_SHOW_LETTER_PAGE = URL_BASE + "/profiles/letter/";
 
@@ -25,8 +21,8 @@ public final class OrfConstants {
   public static final String URL_SHOW_LETTER_PAGE_A = URL_SHOW_LETTER_PAGE + "A";
 
   /**
-   * URL für verpasste Sendungen eines Tages Muss am Ende noch um Datum ergänzt
-   * werden im Format DD.MM.YYYY
+   * URL für verpasste Sendungen eines Tages Muss am Ende noch um Datum ergänzt werden im Format
+   * DD.MM.YYYY
    */
   public static final String URL_DATE = URL_BASE + "/schedule/";
 
@@ -34,4 +30,6 @@ public final class OrfConstants {
    * URL für Übersichtsseite des Archivs
    */
   public static final String URL_ARCHIVE = URL_BASE + "/archive";
+
+  private OrfConstants() {}
 }
