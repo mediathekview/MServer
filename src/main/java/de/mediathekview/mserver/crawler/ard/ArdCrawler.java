64d1ceb1b6c0a43bd2d4a25cc6c23acad53cd03f
Nicklas Wiegandt <nicklas@wiegandt.net>
2018-06-21 22:33:16 +0200

    Begon to fix #350

--- src/main/java/de/mediathekview/mserver/crawler/ard/ArdCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/ard/ArdCrawler.java
@@ -2,15 +2,19 @@ package de.mediathekview.mserver.crawler.ard;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
 import java.util.concurrent.RecursiveTask;
+import java.util.stream.Collectors;
 import de.mediathekview.mlib.daten.Film;
 import de.mediathekview.mlib.daten.Sender;
 import de.mediathekview.mlib.messages.listener.MessageListener;
 import de.mediathekview.mserver.base.CategoriesAZ;
 import de.mediathekview.mserver.base.config.MServerConfigManager;
+import de.mediathekview.mserver.crawler.ard.tasks.ArdGetKanaeleTask;
 import de.mediathekview.mserver.crawler.ard.tasks.ArdSendungTask;
 import de.mediathekview.mserver.crawler.ard.tasks.ArdSendungenOverviewPageCrawler;
 import de.mediathekview.mserver.crawler.ard.tasks.ArdSendungsfolgenOverviewPageCrawler;
@@ -23,10 +27,13 @@ public class ArdCrawler extends AbstractCrawler {
   private static final String ARD_CATEGORY_BASE_URL =
       ARD_BASE_URL + "/tv/sendungen-a-z?buchstabe=%s";
   private static final String ARD_DAY_BASE_URL = ARD_BASE_URL + "/tv/sendungVerpasst?tag=%d";
+  private static final String ARD_DAY_KANAL_BASE_URL =
+      ARD_BASE_URL + "/tv/sendungVerpasst?tag=%d&kanal=%s";
 
   public ArdCrawler(final ForkJoinPool aForkJoinPool,
       final Collection<MessageListener> aMessageListeners,
-      final Collection<SenderProgressListener> aProgressListeners, final MServerConfigManager rootConfig) {
+      final Collection<SenderProgressListener> aProgressListeners,
+      final MServerConfigManager rootConfig) {
     super(aForkJoinPool, aMessageListeners, aProgressListeners, rootConfig);
   }
 
@@ -38,24 +45,43 @@ public class ArdCrawler extends AbstractCrawler {
   private RecursiveTask<Set<ArdSendungBasicInformation>> createCategoriesOverviewPageCrawler() {
     final ConcurrentLinkedQueue<CrawlerUrlDTO> categoryUrlsToCrawl = new ConcurrentLinkedQueue<>();
     Arrays.stream(CategoriesAZ.values())
-        .map(c -> new CrawlerUrlDTO(String.format(ARD_CATEGORY_BASE_URL, c.getKey())))
+        .map(category -> new CrawlerUrlDTO(String.format(ARD_CATEGORY_BASE_URL, category.getKey())))
         .forEach(categoryUrlsToCrawl::offer);
     return new ArdSendungenOverviewPageCrawler(this, categoryUrlsToCrawl);
   }
 
-  private RecursiveTask<Set<ArdSendungBasicInformation>> createDaysOverviewPageCrawler() {
+  private RecursiveTask<Set<ArdSendungBasicInformation>> createDaysOverviewPageCrawler(
+      final Set<String> kanaele) {
     final ConcurrentLinkedQueue<CrawlerUrlDTO> dayUrlsToCrawl = new ConcurrentLinkedQueue<>();
-    for (int i = 0; i < crawlerConfig.getMaximumDaysForSendungVerpasstSection(); i++) {
-      dayUrlsToCrawl.offer(new CrawlerUrlDTO(String.format(ARD_DAY_BASE_URL, i)));
-    }
+    dayUrlsToCrawl.addAll(createDayUrlsToCrawl(Optional.empty()));
+    dayUrlsToCrawl.addAll(kanaele.stream().map(Optional::of).map(this::createDayUrlsToCrawl)
+        .flatMap(ConcurrentLinkedQueue::stream).collect(Collectors.toSet()));
     return new ArdSendungsfolgenOverviewPageCrawler(this, dayUrlsToCrawl);
+  }
+
+  private ConcurrentLinkedQueue<CrawlerUrlDTO> createDayUrlsToCrawl(final Optional<String> kanal) {
+    final ConcurrentLinkedQueue<CrawlerUrlDTO> dayUrlsToCrawl = new ConcurrentLinkedQueue<>();
+
+    for (int i = 0; i < crawlerConfig.getMaximumDaysForSendungVerpasstSection(); i++) {
+      if (kanal.isPresent()) {
+        dayUrlsToCrawl.offer(new CrawlerUrlDTO(String.format(ARD_DAY_KANAL_BASE_URL, i, kanal)));
+      } else {
+        dayUrlsToCrawl.offer(new CrawlerUrlDTO(String.format(ARD_DAY_BASE_URL, i)));
+      }
+    }
+    return dayUrlsToCrawl;
+
   }
 
   @Override
   protected RecursiveTask<Set<Film>> createCrawlerTask() {
+    final Future<Set<String>> kanaele =
+        forkJoinPool.submit(new ArdGetKanaeleTask(this, String.format(ARD_DAY_BASE_URL, 0)));
+
     final RecursiveTask<Set<ArdSendungBasicInformation>> categoriesTask =
         createCategoriesOverviewPageCrawler();
-    final RecursiveTask<Set<ArdSendungBasicInformation>> daysTask = createDaysOverviewPageCrawler();
+    final RecursiveTask<Set<ArdSendungBasicInformation>> daysTask =
+        createDaysOverviewPageCrawler(kanaele.get());
     forkJoinPool.execute(categoriesTask);
     forkJoinPool.execute(daysTask);
 
