ac9e8723e08ff5f82acfc2f31bf19dc0c0e9d777
pidoubleyou <pidoubleyou@arcor.de>
2018-07-06 19:27:25 +0200

    Phönix: Deserializer für Sendungsübersicht

--- src/main/java/de/mediathekview/mserver/crawler/phoenix/PhoenixConstants.java
+++ src/main/java/de/mediathekview/mserver/crawler/phoenix/PhoenixConstants.java
@@ -5,6 +5,7 @@ public final class PhoenixConstants {
 
   public static final String URL_BASE = "https://www.phoenix.de";
 
+  public static final String URL_FILM_DETAIL_JSON = "/response/id/";
   public static final String URL_FILM_DETAIL_XML = "/php/mediaplayer/data/beitrags_details.php?ak=web&ptmd=true&id=";
 
   public static final String URL_VIDEO_DETAILS_HOST = "https://tmd.phoenix.de";
