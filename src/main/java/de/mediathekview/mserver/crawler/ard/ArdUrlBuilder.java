7a2b8b572e63407a92541f4a0341f92c21fa864c
pidoubleyou <pidoubleyou@arcor.de>
2018-12-08 16:58:49 +0100

    ARD: URL-Encoding

--- src/main/java/de/mediathekview/mserver/crawler/ard/ArdUrlBuilder.java
+++ src/main/java/de/mediathekview/mserver/crawler/ard/ArdUrlBuilder.java
@@ -1,9 +1,14 @@
 package de.mediathekview.mserver.crawler.ard;
 
+import static org.glassfish.jersey.uri.UriComponent.Type.QUERY_PARAM;
+
+import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import org.glassfish.jersey.uri.UriComponent;
 
 public class ArdUrlBuilder {
+
   private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
 
   private final String baseUrl;
@@ -14,51 +19,61 @@ public class ArdUrlBuilder {
   private String hashKey;
   private LocalDateTime startDate;
   private LocalDateTime endDate;
-  
+
   public ArdUrlBuilder(final String baseUrl, final String clientName) {
     this.baseUrl = baseUrl;
     this.clientName = clientName;
   }
-  
+
   public ArdUrlBuilder addSearchDate(final LocalDateTime date) {
-    startDate = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), 0, 0, 0);
-    endDate = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), 23, 59, 59);
+    // if start date has time of 00:00:00, the api won't return any result
+    LocalDateTime dateAfter = date.plusDays(1);
+    startDate = LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(), 4, 30, 0);
+    endDate = LocalDateTime.of(dateAfter.getYear(), dateAfter.getMonth(), dateAfter.getDayOfMonth(), 4, 29, 59);
     return this;
   }
-  
+
   public ArdUrlBuilder addClipId(final String id, final String deviceType) {
     this.clipId = id;
     this.deviceType = deviceType;
     return this;
   }
-  
+
   public ArdUrlBuilder addSavedQuery(int queryVersion, final String hashKey) {
     this.queryVersion = queryVersion;
     this.hashKey = hashKey;
     return this;
   }
-  
+
   public String build() {
-    return String.format("%s?variables={\"client\":\"%s\"%s}%s", baseUrl, clientName, buildOptionalVariables(), buildExtensionsParameter());
+    return String.format("%s?%s%s", baseUrl, buildVariables(), buildExtensionsParameter());
   }
-  
+
+  private String buildVariables() {
+    String json = String.format("{\"client\":\"%s\"%s}", clientName, buildOptionalVariables());
+    return String.format("variables=%s", UriComponent.encode(json, QUERY_PARAM));
+  }
+
   private String buildOptionalVariables() {
+
     if (clipId != null) {
       return String.format(",\"clipId\":\"%s\",\"deviceType\":\"%s\"", clipId, deviceType);
     }
     if (startDate != null) {
-      return String.format(",\"startDateTime\":\"%sZ\",\"endDateTime\":\"%sZ\"", startDate.format(DATE_TIME_FORMATTER), endDate.format(DATE_TIME_FORMATTER));
+      return String.format(",\"startDateTime\":\"%sZ\",\"endDateTime\":\"%sZ\"", startDate.format(DATE_TIME_FORMATTER),
+          endDate.format(DATE_TIME_FORMATTER));
     }
-    
+
     return "";
   }
-  
+
   private String buildExtensionsParameter() {
-    
+
     if (queryVersion > 0) {
-      return String.format("&extensions={\"persistedQuery\":{\"version\":%d,\"sha256Hash\":\"%s\"}}", queryVersion, hashKey);
+      String json = String.format("{\"persistedQuery\":{\"version\":%d,\"sha256Hash\":\"%s\"}}", queryVersion, hashKey);
+      return (String.format("&extensions=%s", UriComponent.encode(json, QUERY_PARAM)));
     }
-    
+
     return "";
   }
 }
--- src/test/java/de/mediathekview/mserver/crawler/ard/ArdUrlBuilderTest.java
+++ src/test/java/de/mediathekview/mserver/crawler/ard/ArdUrlBuilderTest.java
@@ -1,8 +1,11 @@
 package de.mediathekview.mserver.crawler.ard;
 
-import java.time.LocalDateTime;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+
+import java.time.LocalDateTime;
+import org.glassfish.jersey.uri.UriComponent;
+import org.glassfish.jersey.uri.UriComponent.Type;
 import org.junit.Test;
 
 
@@ -10,43 +13,50 @@ public class ArdUrlBuilderTest {
 
   @Test
   public void testWithoutOptionalParameters() {
-    final String expectedUrl = "https://api.ardmediathek.de/public-gateway?variables={\"client\":\"test\"}";
-    
+    final String expectedUrl =
+        "https://api.ardmediathek.de/public-gateway?variables=" + UriComponent.encode("{\"client\":\"test\"}", Type.QUERY_PARAM);
+
     final String actualUrl = new ArdUrlBuilder("https://api.ardmediathek.de/public-gateway", "test").build();
-    
+
     assertThat(actualUrl, equalTo(expectedUrl));
   }
-  
+
   @Test
   public void testClipParameter() {
-    final String expectedUrl = "https://my.api.de?variables={\"client\":\"test\",\"clipId\":\"jd795kjl8\",\"deviceType\":\"screen\"}";
-    
+    final String expectedUrl =
+        "https://my.api.de?variables=" + UriComponent
+            .encode("{\"client\":\"test\",\"clipId\":\"jd795kjl8\",\"deviceType\":\"screen\"}", Type.QUERY_PARAM);
+
     final String actualUrl = new ArdUrlBuilder("https://my.api.de", "test")
-            .addClipId("jd795kjl8", "screen")
-            .build();
-    
+        .addClipId("jd795kjl8", "screen")
+        .build();
+
     assertThat(actualUrl, equalTo(expectedUrl));
   }
-  
+
   @Test
   public void testSearchDateParameter() {
-    final String expectedUrl = "https://my.api.de?variables={\"client\":\"test\",\"startDateTime\":\"2018-12-07T00:00:00.000Z\",\"endDateTime\":\"2018-12-07T23:59:59.000Z\"}";
-    
+    final String expectedUrl = "https://my.api.de?variables=" + UriComponent
+        .encode("{\"client\":\"test\",\"startDateTime\":\"2018-12-07T04:30:00.000Z\",\"endDateTime\":\"2018-12-08T04:29:59.000Z\"}",
+            Type.QUERY_PARAM);
+
     final String actualUrl = new ArdUrlBuilder("https://my.api.de", "test")
-            .addSearchDate(LocalDateTime.of(2018, 12, 7, 12, 55, 36))
-            .build();
-    
+        .addSearchDate(LocalDateTime.of(2018, 12, 7, 12, 55, 36))
+        .build();
+
     assertThat(actualUrl, equalTo(expectedUrl));
   }
-  
+
   @Test
   public void testSavedQuery() {
-    final String expectedUrl = "https://my.api.de?variables={\"client\":\"test\"}&extensions={\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"s86d73d\"}}";
-    
+    final String expectedUrl =
+        "https://my.api.de?variables=" + UriComponent.encode("{\"client\":\"test\"}", Type.QUERY_PARAM) + "&extensions=" + UriComponent
+            .encode("{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"s86d73d\"}}", Type.QUERY_PARAM);
+
     final String actualUrl = new ArdUrlBuilder("https://my.api.de", "test")
-            .addSavedQuery(1, "s86d73d")
-            .build();
-    
+        .addSavedQuery(1, "s86d73d")
+        .build();
+
     assertThat(actualUrl, equalTo(expectedUrl));
   }
 }
