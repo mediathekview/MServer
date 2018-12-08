66b16dfef0b75ddf36303f795be6abc997f111d0
pidoubleyou <pidoubleyou@arcor.de>
2018-12-08 16:59:35 +0100

    ARD: erste Crawlerimplementierung

--- src/main/java/de/mediathekview/mserver/crawler/ard/ArdCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/ard/ArdCrawler.java
@@ -4,19 +4,25 @@ import de.mediathekview.mlib.daten.Film;
 import de.mediathekview.mlib.daten.Sender;
 import de.mediathekview.mlib.messages.listener.MessageListener;
 import de.mediathekview.mserver.base.config.MServerConfigManager;
+import de.mediathekview.mserver.base.messages.ServerMessages;
+import de.mediathekview.mserver.crawler.ard.tasks.ArdDayPageTask;
+import de.mediathekview.mserver.crawler.ard.tasks.ArdFilmDetailTask;
 import de.mediathekview.mserver.crawler.basic.AbstractCrawler;
 import de.mediathekview.mserver.crawler.basic.CrawlerUrlDTO;
 import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ArdCrawler extends AbstractCrawler {
+
   private static final Logger LOG = LogManager.getLogger(ArdCrawler.class);
 
   public ArdCrawler(final ForkJoinPool aForkJoinPool,
@@ -36,12 +42,13 @@ public class ArdCrawler extends AbstractCrawler {
     final ConcurrentLinkedQueue<CrawlerUrlDTO> dayUrlsToCrawl = new ConcurrentLinkedQueue<>();
 
     LocalDateTime startDate = LocalDateTime.now().minusDays(crawlerConfig.getMaximumDaysForSendungVerpasstSection());
-    for (int i = 0; i < crawlerConfig.getMaximumDaysForSendungVerpasstSection() + crawlerConfig.getMaximumDaysForSendungVerpasstSectionFuture(); i++) {
+    for (int i = 0;
+        i <= crawlerConfig.getMaximumDaysForSendungVerpasstSection() + crawlerConfig.getMaximumDaysForSendungVerpasstSectionFuture(); i++) {
       final String url = new ArdUrlBuilder(ArdConstants.BASE_URL, ArdConstants.DEFAULT_CLIENT)
-              .addSearchDate(startDate.plusDays(i))
-              .addSavedQuery(ArdConstants.QUERY_DAY_SEARCH_VERSION, ArdConstants.QUERY_DAY_SEARCH_HASH)
-              .build();
-      
+          .addSearchDate(startDate.plusDays(i))
+          .addSavedQuery(ArdConstants.QUERY_DAY_SEARCH_VERSION, ArdConstants.QUERY_DAY_SEARCH_HASH)
+          .build();
+
       dayUrlsToCrawl.offer(new CrawlerUrlDTO(url));
     }
     return dayUrlsToCrawl;
@@ -50,7 +57,27 @@ public class ArdCrawler extends AbstractCrawler {
 
   @Override
   protected RecursiveTask<Set<Film>> createCrawlerTask() {
-    ConcurrentLinkedQueue<CrawlerUrlDTO> dayUrls = createDayUrlsToCrawl();
+
+    try {
+      ConcurrentLinkedQueue<ArdFilmInfoDto> shows = new ConcurrentLinkedQueue<>();
+      shows.addAll(getDaysEntries());
+
+      printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+      getAndSetMaxCount(shows.size());
+
+      return new ArdFilmDetailTask(this, shows);
+    } catch (InterruptedException | ExecutionException ex) {
+      LOG.fatal("Exception in ARD crawler.", ex);
+    }
     return null;
+  }
+
+  private Set<ArdFilmInfoDto> getDaysEntries() throws InterruptedException, ExecutionException {
+    ArdDayPageTask dayTask = new ArdDayPageTask(this, createDayUrlsToCrawl());
+    Set<ArdFilmInfoDto> shows = forkJoinPool.submit(dayTask).get();
+
+    printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+
+    return shows;
   }
 }
