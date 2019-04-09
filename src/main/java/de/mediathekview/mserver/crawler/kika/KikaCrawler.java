15bd8d7ba25641e06edc597039cbbbc665c1bad6
pidoubleyou <pidoubleyou@arcor.de>
2019-04-09 22:04:18 +0200

    KIKA: Crawler umgestellt

--- src/main/java/de/mediathekview/mserver/crawler/kika/KikaCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/kika/KikaCrawler.java
@@ -14,6 +14,7 @@ import de.mediathekview.mserver.crawler.kika.tasks.KikaSendungVerpasstTask;
 import de.mediathekview.mserver.crawler.kika.tasks.KikaSendungsfolgeVideoDetailsTask;
 import de.mediathekview.mserver.crawler.kika.tasks.KikaSendungsfolgeVideoUrlTask;
 import de.mediathekview.mserver.crawler.kika.tasks.KikaTopicLandingPageTask;
+import de.mediathekview.mserver.crawler.kika.tasks.KikaTopicOverviewPageTask;
 import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
 import java.time.LocalDateTime;
 import java.util.Collection;
@@ -28,13 +29,9 @@ import org.apache.logging.log4j.Logger;
 
 public class KikaCrawler extends AbstractCrawler {
   private static final Logger LOG = LogManager.getLogger(KikaCrawler.class);
-  // http://www.kika.de/sendungen/ipg/ipg102.html#date-22122017
-  public static final String BASE_URL = "http://www.kika.de/";
-  public static final String SENDUNGEN_OVERVIEW_PAGE_URL =
-      BASE_URL + "sendungen/sendungenabisz100.html";
 
-
-  public KikaCrawler(final ForkJoinPool aForkJoinPool,
+  public KikaCrawler(
+      final ForkJoinPool aForkJoinPool,
       final Collection<MessageListener> aMessageListeners,
       final Collection<SenderProgressListener> aProgressListeners,
       final MServerConfigManager aRootConfig) {
@@ -55,53 +52,18 @@ public class KikaCrawler extends AbstractCrawler {
     } catch (ExecutionException | InterruptedException ex) {
       LOG.fatal("Exception in KIKA crawler.", ex);
     }
-
-    /*
-    // Gathers all Sendungen
-    final Set<CrawlerUrlDTO> sendungenOverviewPageUrls = new HashSet<>();
-    sendungenOverviewPageUrls.add(new CrawlerUrlDTO(SENDUNGEN_OVERVIEW_PAGE_URL));
-    final KikaPagedOverviewPageTask sendungenOverviewPageTask =
-        new KikaPagedOverviewPageTask(this, new ConcurrentLinkedQueue<>(sendungenOverviewPageUrls));
-
-    // Gathers the URLs for the Sendungsfolgen overview page from the Sendungen pages.
-    final Set<CrawlerUrlDTO> sendungOverviewPageUrls =
-        forkJoinPool.invoke(sendungenOverviewPageTask);
-    printMessage(ServerMessages.DEBUG_KIKA_SENDUNGSFOLGEN_OVERVIEWPAGES,
-        sendungOverviewPageUrls.size(), getSender().getName());
-    final KikaSendungOverviewPageTask sendungOverviewPageTask =
-        new KikaSendungOverviewPageTask(this, new ConcurrentLinkedQueue<>(sendungOverviewPageUrls));
-    // Gathers the Sendungsfolgen URLs from the Sendungsfolgen overview page.
-    final Set<CrawlerUrlDTO> sendungsfolgenOverviewPageUrls =
-        forkJoinPool.invoke(sendungOverviewPageTask);
-    printMessage(ServerMessages.DEBUG_KIKA_SENDUNGSFOLGEN_URLS,
-        sendungsfolgenOverviewPageUrls.size(), getSender().getName());
-    final KikaPagedOverviewPageTask sendungsfolgenOverviewPageTask = new KikaPagedOverviewPageTask(
-        this, new ConcurrentLinkedQueue<>(sendungsfolgenOverviewPageUrls));
-
-    final ForkJoinTask<Set<CrawlerUrlDTO>> featureSendungsFolgenUrls =
-        forkJoinPool.submit(sendungsfolgenOverviewPageTask);
-    printMessage(ServerMessages.DEBUG_KIKA_SENDUNG_VERPASST_OVERVIEWPAGES, getSender().getName());
-    final KikaSendungVerpasstOverviewUrlTask sendungVerpasstOverviewUrlTask =
-        new KikaSendungVerpasstOverviewUrlTask(this);
     try {
-      final Set<CrawlerUrlDTO> sendungVerpasstOverviewUrls =
-          forkJoinPool.submit(sendungVerpasstOverviewUrlTask).get();
-      printMessage(ServerMessages.DEBUG_KIKA_SENDUNG_VERPASST_PAGES,
-          sendungVerpasstOverviewUrls.size(), getSender().getName());
-      final KikaSendungVerpasstTask sendungVerpasstTask = new KikaSendungVerpasstTask(this,
-          new ConcurrentLinkedQueue<>(sendungVerpasstOverviewUrls));
-      sendungsfolgenUrls.addAll(forkJoinPool.invoke(sendungVerpasstTask));
-    } catch (InterruptedException | ExecutionException exception) {
-      LOG.fatal(
-          "Something wen't terrible wrong on gathering the \"verpasste Sendungen\" overview page URLs.");
-      printErrorMessage();
+      getLetterEntries()
+          .forEach(
+              show -> {
+                if (!sendungsfolgenUrls.contains(show)) {
+                  sendungsfolgenUrls.add(show);
+                }
+              });
+    } catch (ExecutionException | InterruptedException ex) {
+      LOG.fatal("Exception in KIKA crawler.", ex);
     }
-    try {
-      sendungsfolgenUrls.addAll(featureSendungsFolgenUrls.get());
-    } catch (InterruptedException | ExecutionException exception) {
-      LOG.fatal("Something wen't terrible wrong on gathering the Sendungsfolgen.");
-      printErrorMessage();
-    }*/
+
     printMessage(ServerMessages.DEBUG_KIKA_SENDUNGSFOLGEN_URL_CONVERTING, getSender().getName());
     final KikaSendungsfolgeVideoUrlTask sendungsfolgeVideoUrlsTask =
         new KikaSendungsfolgeVideoUrlTask(this, sendungsfolgenUrls);
@@ -109,40 +71,50 @@ public class KikaCrawler extends AbstractCrawler {
         forkJoinPool.invoke(sendungsfolgeVideoUrlsTask);
     printMessage(ServerMessages.DEBUG_KIKA_CONVERTING_FINISHED, getSender().getName());
     getAndSetMaxCount(sendungsfolgeVideoUrls.size());
-    return new KikaSendungsfolgeVideoDetailsTask(this,
-        new ConcurrentLinkedQueue<>(sendungsfolgeVideoUrls));
+    return new KikaSendungsfolgeVideoDetailsTask(
+        this, new ConcurrentLinkedQueue<>(sendungsfolgeVideoUrls));
   }
 
   private Set<CrawlerUrlDTO> getLetterEntries() throws InterruptedException, ExecutionException {
     ConcurrentLinkedQueue<CrawlerUrlDTO> letterPageUrls = new ConcurrentLinkedQueue<>();
     letterPageUrls.add(new CrawlerUrlDTO(KikaConstants.URL_TOPICS_PAGE));
-    final KikaLetterPageUrlTask letterUrlTask = new KikaLetterPageUrlTask(this, letterPageUrls, KikaConstants.BASE_URL);
+    final KikaLetterPageUrlTask letterUrlTask =
+        new KikaLetterPageUrlTask(this, letterPageUrls, KikaConstants.BASE_URL);
     final Set<CrawlerUrlDTO> letterUrls = forkJoinPool.submit(letterUrlTask).get();
 
-    final KikaLetterPageTask letterTask = new KikaLetterPageTask(this, new ConcurrentLinkedQueue<>(letterUrls), KikaConstants.BASE_URL);
+    final KikaLetterPageTask letterTask =
+        new KikaLetterPageTask(
+            this, new ConcurrentLinkedQueue<>(letterUrls), KikaConstants.BASE_URL);
     final Set<CrawlerUrlDTO> topicUrls = forkJoinPool.submit(letterTask).get();
-    
-    final KikaTopicLandingPageTask landingTask = new KikaTopicLandingPageTask(this, new ConcurrentLinkedQueue<>(topicUrls), BASE_URL);
+
+    final KikaTopicLandingPageTask landingTask =
+        new KikaTopicLandingPageTask(
+            this, new ConcurrentLinkedQueue<>(topicUrls), KikaConstants.BASE_URL);
     final Set<CrawlerUrlDTO> topicOverviewUrls = forkJoinPool.submit(landingTask).get();
-    
-    return null;
+
+    final KikaTopicOverviewPageTask topicOverviewTask =
+        new KikaTopicOverviewPageTask(
+            this, new ConcurrentLinkedQueue<>(topicOverviewUrls), KikaConstants.BASE_URL);
+    return forkJoinPool.submit(topicOverviewTask).get();
   }
-  
+
   private Set<CrawlerUrlDTO> getDaysEntries() throws ExecutionException, InterruptedException {
     final Set<CrawlerUrlDTO> filmUrls = new HashSet<>();
 
     final KikaSendungVerpasstOverviewUrlTask daysOverviewUrlTask =
         new KikaSendungVerpasstOverviewUrlTask(this, LocalDateTime.now());
 
-    final Set<CrawlerUrlDTO> daysUrls =
-        forkJoinPool.submit(daysOverviewUrlTask).get();
-    printMessage(ServerMessages.DEBUG_KIKA_SENDUNG_VERPASST_PAGES, daysUrls.size(), getSender().getName());
+    final Set<CrawlerUrlDTO> daysUrls = forkJoinPool.submit(daysOverviewUrlTask).get();
+    printMessage(
+        ServerMessages.DEBUG_KIKA_SENDUNG_VERPASST_PAGES, daysUrls.size(), getSender().getName());
 
-    final KikaSendungVerpasstTask dayTask = new KikaSendungVerpasstTask(this,
-        new ConcurrentLinkedQueue<>(daysUrls), KikaConstants.BASE_URL);
+    final KikaSendungVerpasstTask dayTask =
+        new KikaSendungVerpasstTask(
+            this, new ConcurrentLinkedQueue<>(daysUrls), KikaConstants.BASE_URL);
     filmUrls.addAll(forkJoinPool.invoke(dayTask));
 
-    printMessage(ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), filmUrls.size());
+    printMessage(
+        ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), filmUrls.size());
 
     return filmUrls;
   }
