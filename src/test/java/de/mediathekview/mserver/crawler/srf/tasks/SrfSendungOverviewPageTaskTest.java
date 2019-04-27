514fb30783a2c8df4aed7ac035da52b2d16d0e80
pidoubleyou <pidoubleyou@arcor.de>
2019-04-27 20:28:52 +0200

    SRF: Test aktiviert, da sie lauffähig sind

--- src/test/java/de/mediathekview/mserver/crawler/srf/tasks/SrfSendungOverviewPageTaskTest.java
+++ src/test/java/de/mediathekview/mserver/crawler/srf/tasks/SrfSendungOverviewPageTaskTest.java
@@ -12,7 +12,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
-@Ignore(value = "The URLs aren't avialable!")
 public class SrfSendungOverviewPageTaskTest extends SrfTaskTestBase {
 
   @Test
