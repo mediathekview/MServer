feb513e2a3fd547b3f7df1980a0b007a5e02a717
pidoubleyou <pidoubleyou@arcor.de>
2019-05-13 22:27:17 +0200

    ZDF: nur vorhandenes Video in Ergebnis aufnehmen

--- src/main/java/mServer/crawler/sender/newsearch/ZDFSearchPageTask.java
+++ src/main/java/mServer/crawler/sender/newsearch/ZDFSearchPageTask.java
@@ -3,9 +3,7 @@ package mServer.crawler.sender.newsearch;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
-import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -17,54 +15,59 @@ import de.mediathekview.mlib.tool.Log;
 import mServer.tool.MserverDaten;
 
 /**
- * Task to parse the response of a search page. 
+ * Task to parse the response of a search page.
  */
 public class ZDFSearchPageTask extends RecursiveTask<Collection<VideoDTO>> {
 
-    private static final long serialVersionUID = 1L;
-    
-    private static final String JSON_ELEMENT_RESULTS = "http://zdf.de/rels/search/results";
-    
-    private final JsonObject searchResult;
-    private final Gson gson;
-    
-    private static final Type ZDFENTRYDTO_COLLECTION_TYPE  = new TypeToken<Collection<ZDFEntryDTO>>()
-            {
-            }.getType();
+  private static final long serialVersionUID = 1L;
 
-    public ZDFSearchPageTask(JsonObject aSearchResult) {
-        searchResult = aSearchResult;
-        
-        gson = new GsonBuilder()
-                .registerTypeAdapter(ZDFEntryDTO.class, new ZDFEntryDTODeserializer())
-                .create();
-    }    
-    
-    @Override
-    protected Collection<VideoDTO> compute() {
-        
-        Collection<VideoDTO> filmList = new ArrayList<>();
-        if(!Config.getStop()) {
-            Collection<ZDFEntryTask> subTasks = new ArrayList<>();
+  private static final String JSON_ELEMENT_RESULTS = "http://zdf.de/rels/search/results";
 
-            Collection<ZDFEntryDTO> zdfEntryDTOList = gson.fromJson(searchResult.getAsJsonArray(JSON_ELEMENT_RESULTS), ZDFENTRYDTO_COLLECTION_TYPE);
-            zdfEntryDTOList.forEach(zdfEntryDTO -> {
-                if(zdfEntryDTO != null) {
-                    final ZDFEntryTask entryTask = new ZDFEntryTask(zdfEntryDTO);
-                    entryTask.fork();
-                    subTasks.add(entryTask);
-                    if (MserverDaten.debug)
-                        Log.sysLog("EntryTask " + entryTask.hashCode() + " added.");
-                }
-            });
+  private final JsonObject searchResult;
+  private final Gson gson;
 
+  private static final Type ZDFENTRYDTO_COLLECTION_TYPE = new TypeToken<Collection<ZDFEntryDTO>>() {
+  }.getType();
 
-            // wait till entry tasks are finished
-            subTasks.forEach(t -> filmList.add(t.join()));
-            if (MserverDaten.debug)
-                Log.sysLog("All EntryTasks finished.");
+  public ZDFSearchPageTask(JsonObject aSearchResult) {
+    searchResult = aSearchResult;
+
+    gson = new GsonBuilder()
+            .registerTypeAdapter(ZDFEntryDTO.class, new ZDFEntryDTODeserializer())
+            .create();
+  }
+
+  @Override
+  protected Collection<VideoDTO> compute() {
+
+    Collection<VideoDTO> filmList = new ArrayList<>();
+    if (!Config.getStop()) {
+      Collection<ZDFEntryTask> subTasks = new ArrayList<>();
+
+      Collection<ZDFEntryDTO> zdfEntryDTOList = gson.fromJson(searchResult.getAsJsonArray(JSON_ELEMENT_RESULTS), ZDFENTRYDTO_COLLECTION_TYPE);
+      zdfEntryDTOList.forEach(zdfEntryDTO -> {
+        if (zdfEntryDTO != null) {
+          final ZDFEntryTask entryTask = new ZDFEntryTask(zdfEntryDTO);
+          entryTask.fork();
+          subTasks.add(entryTask);
+          if (MserverDaten.debug) {
+            Log.sysLog("EntryTask " + entryTask.hashCode() + " added.");
+          }
         }
-        
-        return filmList;
-    }    
+      });
+
+      // wait till entry tasks are finished
+      subTasks.forEach(t -> {
+        VideoDTO video = t.join();
+        if (video != null) {
+          filmList.add(t.join());
+        }
+      });
+      if (MserverDaten.debug) {
+        Log.sysLog("All EntryTasks finished.");
+      }
+    }
+
+    return filmList;
+  }
 }
