1273b125226e642a076af831bf21244f25523379
Nicklas Wiegandt <nicklas@wiegandt.eu>
2019-05-23 02:45:09 +0200

    #476 Begon to change the 3Sat crawler for the new mediathek

--- src/main/java/de/mediathekview/mserver/crawler/zdf/ZdfCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/zdf/ZdfCrawler.java
@@ -1,100 +1,41 @@
 package de.mediathekview.mserver.crawler.zdf;
 
-import de.mediathekview.mlib.daten.Film;
 import de.mediathekview.mlib.daten.Sender;
 import de.mediathekview.mlib.messages.listener.MessageListener;
 import de.mediathekview.mserver.base.config.MServerConfigManager;
-import de.mediathekview.mserver.base.messages.ServerMessages;
-import de.mediathekview.mserver.crawler.basic.AbstractCrawler;
-import de.mediathekview.mserver.crawler.basic.CrawlerUrlDTO;
-import de.mediathekview.mserver.crawler.zdf.tasks.ZdfDayPageTask;
-import de.mediathekview.mserver.crawler.zdf.tasks.ZdfFilmDetailTask;
-import de.mediathekview.mserver.crawler.zdf.tasks.ZdfIndexPageTask;
 import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 import java.util.Collection;
-import java.util.Set;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveTask;
 
-public class ZdfCrawler extends AbstractCrawler {
-
-  private static final Logger LOG = LogManager.getLogger(ZdfCrawler.class);
+public class ZdfCrawler extends AbstractZdfCrawler {
 
   public ZdfCrawler(
-      final ForkJoinPool aForkJoinPool,
-      final Collection<MessageListener> aMessageListeners,
-      final Collection<SenderProgressListener> aProgressListeners,
-      final MServerConfigManager rootConfig) {
+          final ForkJoinPool aForkJoinPool,
+          final Collection<MessageListener> aMessageListeners,
+          final Collection<SenderProgressListener> aProgressListeners,
+          final MServerConfigManager rootConfig) {
     super(aForkJoinPool, aMessageListeners, aProgressListeners, rootConfig);
+  }
+
+  @Override
+  protected @NotNull String getUrlBase() {
+    return ZdfConstants.URL_BASE;
+  }
+
+  @Override
+  protected String getApiUrlBase() {
+    return ZdfConstants.URL_API_BASE;
+  }
+
+  @Override
+  protected @NotNull String getUrlDay() {
+    return ZdfConstants.URL_DAY;
   }
 
   @Override
   public Sender getSender() {
     return Sender.ZDF;
-  }
-
-  @Override
-  protected RecursiveTask<Set<Film>> createCrawlerTask() {
-
-    try {
-      final ZdfConfiguration configuration = loadConfiguration();
-
-      final ConcurrentLinkedQueue<ZdfEntryDto> shows = new ConcurrentLinkedQueue<>();
-      shows.addAll(getDaysEntries(configuration));
-
-      printMessage(
-          ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
-      getAndSetMaxCount(shows.size());
-
-      return new ZdfFilmDetailTask(this, shows, configuration.getVideoAuthKey());
-    } catch (final InterruptedException | ExecutionException ex) {
-      LOG.fatal("Exception in ZDF crawler.", ex);
-    }
-    return null;
-  }
-
-  private ZdfConfiguration loadConfiguration() throws ExecutionException, InterruptedException {
-    final ZdfIndexPageTask task = new ZdfIndexPageTask(this);
-    return forkJoinPool.submit(task).get();
-  }
-
-  private Set<ZdfEntryDto> getDaysEntries(final ZdfConfiguration configuration)
-      throws InterruptedException, ExecutionException {
-    final ZdfDayPageTask dayTask =
-        new ZdfDayPageTask(this, getDayUrls(), configuration.getSearchAuthKey());
-    final Set<ZdfEntryDto> shows = forkJoinPool.submit(dayTask).get();
-
-    printMessage(
-        ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
-
-    return shows;
-  }
-
-  private ConcurrentLinkedQueue<CrawlerUrlDTO> getDayUrls() {
-    final ConcurrentLinkedQueue<CrawlerUrlDTO> urls = new ConcurrentLinkedQueue<>();
-    for (int i = 0;
-        i
-            <= crawlerConfig.getMaximumDaysForSendungVerpasstSection()
-                + crawlerConfig.getMaximumDaysForSendungVerpasstSectionFuture();
-        i++) {
-
-      final LocalDateTime local =
-          LocalDateTime.now()
-              .plus(crawlerConfig.getMaximumDaysForSendungVerpasstSectionFuture(), ChronoUnit.DAYS)
-              .minus(i, ChronoUnit.DAYS);
-      final String date = local.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-      final String url = String.format(ZdfConstants.URL_DAY, date, date);
-      urls.add(new CrawlerUrlDTO(url));
-    }
-
-    return urls;
   }
 }
--- src/main/java/de/mediathekview/mserver/crawler/zdf/json/ZdfDayPageDeserializer.java
+++ src/main/java/de/mediathekview/mserver/crawler/zdf/json/ZdfDayPageDeserializer.java
@@ -1,13 +1,9 @@
 package de.mediathekview.mserver.crawler.zdf.json;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import com.google.gson.*;
 import de.mediathekview.mserver.base.utils.UrlUtils;
-import de.mediathekview.mserver.crawler.zdf.ZdfConstants;
 import de.mediathekview.mserver.crawler.zdf.ZdfEntryDto;
+
 import java.lang.reflect.Type;
 import java.util.Optional;
 
@@ -16,7 +12,8 @@ public class ZdfDayPageDeserializer implements JsonDeserializer<ZdfDayPageDto> {
   private static final String JSON_ELEMENT_ENTRIES = "http://zdf.de/rels/search/results";
   private static final String JSON_ELEMENT_MAIN_VIDEO_CONTENT = "mainVideoContent";
   private static final String JSON_ELEMENT_TARGET = "http://zdf.de/rels/target";
-  private static final String JSON_ELEMENT_VIDEO_PAGE_TEASER = "http://zdf.de/rels/content/video-page-teaser";
+  private static final String JSON_ELEMENT_VIDEO_PAGE_TEASER =
+      "http://zdf.de/rels/content/video-page-teaser";
 
   private static final String JSON_ATTRIBUTE_CANONICAL = "canonical";
   private static final String JSON_ATTRIBUTE_NEXT = "next";
@@ -24,9 +21,17 @@ public class ZdfDayPageDeserializer implements JsonDeserializer<ZdfDayPageDto> {
 
   private static final String PLACEHOLDER_PLAYER_ID = "{playerId}";
   private static final String PLAYER_ID = "ngplayer_2_3";
+  private final String apiUrlBase;
+
+  public ZdfDayPageDeserializer(final String aApiUrlBase) {
+
+    apiUrlBase = aApiUrlBase;
+  }
 
   @Override
-  public ZdfDayPageDto deserialize(final JsonElement aJsonElement, final Type aTypeOfT,
+  public ZdfDayPageDto deserialize(
+      final JsonElement aJsonElement,
+      final Type aTypeOfT,
       final JsonDeserializationContext aContext) {
 
     final ZdfDayPageDto dayPageDto = new ZdfDayPageDto();
@@ -41,9 +46,10 @@ public class ZdfDayPageDeserializer implements JsonDeserializer<ZdfDayPageDto> {
   private void parseNextPage(final ZdfDayPageDto aDayPageDtp, final JsonObject aJsonObject) {
     if (aJsonObject.has(JSON_ATTRIBUTE_NEXT)) {
       String url = aJsonObject.get(JSON_ATTRIBUTE_NEXT).getAsString();
-      url = UrlUtils.addDomainIfMissing(url, ZdfConstants.URL_API_BASE)
-          // replase type parameter because the link with types as numbers returns no results!
-          .replaceFirst("&types=\\d+&", "&types=page-video&");
+      url =
+          UrlUtils.addDomainIfMissing(url, apiUrlBase)
+              // replase type parameter because the link with types as numbers returns no results!
+              .replaceFirst("&types=\\d+&", "&types=page-video&");
       aDayPageDtp.setNextPageUrl(url);
     }
   }
@@ -53,12 +59,13 @@ public class ZdfDayPageDeserializer implements JsonDeserializer<ZdfDayPageDto> {
       final JsonElement resultsElement = aJsonObject.get(JSON_ELEMENT_ENTRIES);
       if (resultsElement.isJsonArray()) {
         final JsonArray resultsArray = resultsElement.getAsJsonArray();
-        resultsArray.forEach(result -> {
-          final Optional<ZdfEntryDto> dto = parseSearchEntry(result.getAsJsonObject());
-          if (dto.isPresent()) {
-            aDayPageDto.addEntry(dto.get());
-          }
-        });
+        resultsArray.forEach(
+            result -> {
+              final Optional<ZdfEntryDto> dto = parseSearchEntry(result.getAsJsonObject());
+              if (dto.isPresent()) {
+                aDayPageDto.addEntry(dto.get());
+              }
+            });
       }
     }
   }
@@ -82,8 +89,8 @@ public class ZdfDayPageDeserializer implements JsonDeserializer<ZdfDayPageDto> {
       String canonical = target.get(JSON_ATTRIBUTE_CANONICAL).getAsString();
       String template = mainVideoTarget.get(JSON_ATTRIBUTE_TEMPLATE).getAsString();
 
-      canonical = UrlUtils.addDomainIfMissing(canonical, ZdfConstants.URL_API_BASE);
-      template = UrlUtils.addDomainIfMissing(template, ZdfConstants.URL_API_BASE);
+      canonical = UrlUtils.addDomainIfMissing(canonical, apiUrlBase);
+      template = UrlUtils.addDomainIfMissing(template, apiUrlBase);
       template = template.replace(PLACEHOLDER_PLAYER_ID, PLAYER_ID);
 
       final ZdfEntryDto dto = new ZdfEntryDto(canonical, template);
@@ -95,7 +102,8 @@ public class ZdfDayPageDeserializer implements JsonDeserializer<ZdfDayPageDto> {
 
   private JsonObject getTarget(final JsonObject aResultObject) {
     JsonObject targetObject = aResultObject.getAsJsonObject(JSON_ELEMENT_TARGET);
-    if (!targetObject.has(JSON_ELEMENT_MAIN_VIDEO_CONTENT) && targetObject.has(JSON_ELEMENT_VIDEO_PAGE_TEASER)) {
+    if (!targetObject.has(JSON_ELEMENT_MAIN_VIDEO_CONTENT)
+        && targetObject.has(JSON_ELEMENT_VIDEO_PAGE_TEASER)) {
       targetObject = targetObject.getAsJsonObject(JSON_ELEMENT_VIDEO_PAGE_TEASER);
     }
 
@@ -105,7 +113,8 @@ public class ZdfDayPageDeserializer implements JsonDeserializer<ZdfDayPageDto> {
   private JsonObject getMainVideoContentTarget(final JsonObject aTargetObject) {
 
     if (aTargetObject.has(JSON_ELEMENT_MAIN_VIDEO_CONTENT)) {
-      JsonObject mainVideoContentObject = aTargetObject.getAsJsonObject(JSON_ELEMENT_MAIN_VIDEO_CONTENT);
+      final JsonObject mainVideoContentObject =
+          aTargetObject.getAsJsonObject(JSON_ELEMENT_MAIN_VIDEO_CONTENT);
       if (mainVideoContentObject.has(JSON_ELEMENT_TARGET)) {
         return mainVideoContentObject.getAsJsonObject(JSON_ELEMENT_TARGET);
       }
