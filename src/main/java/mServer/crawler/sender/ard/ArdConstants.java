1420198b9631330096e5bc38087b472ad1c5f471
pidoubleyou <pidoubleyou@arcor.de>
2019-06-16 16:15:41 +0200

    ARD: neuer Query-Hash

--- src/main/java/mServer/crawler/sender/ard/ArdConstants.java
+++ src/main/java/mServer/crawler/sender/ard/ArdConstants.java
@@ -3,15 +3,15 @@ package mServer.crawler.sender.ard;
 public class ArdConstants {
 
   public static final String BASE_URL = "https://api.ardmediathek.de/public-gateway";
-  
+
   public static final String DEFAULT_CLIENT = "ard";
   public static final String DEFAULT_DEVICE = "pc";
 
   public static final int QUERY_DAY_SEARCH_VERSION = 1;
   public static final String QUERY_DAY_SEARCH_HASH = "a62c5366a603ad60a46e4b0dd0f52b1855d401026016a639bbbfbe88e2118989";
-  
+
   public static final int QUERY_FILM_VERSION = 1;
-  public static final String QUERY_FILM_HASH = "b69efa74d0e2623a9104fb94c9ed2e8f1418a68f6457594126d719a2e8ca7174";
+  public static final String QUERY_FILM_HASH = "a9a9b15083dd3bf249264a7ff5d9e1010ec5d861539bc779bb1677a4a37872da";
 
   public static final int QUERY_TOPICS_VERSION = 1;
   public static final String QUERY_TOPICS_HASH = "fdbab76da7d6aeb1ae859e1758dd1db068824dbf1623c02bc4c5f61facb474c2";
@@ -23,5 +23,6 @@ public class ArdConstants {
 
   public static final String BASE_URL_SUBTITLES = "https://classic.ardmediathek.de";
 
-  private ArdConstants() {}
+  private ArdConstants() {
+  }
 }
