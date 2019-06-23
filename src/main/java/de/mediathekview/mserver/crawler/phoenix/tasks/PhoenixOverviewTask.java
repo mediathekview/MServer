05a8e9007dee5e3f74d2f7586b8757d91769179a
pidoubleyou <pidoubleyou@arcor.de>
2019-06-23 11:29:12 +0200

    PHOENIX: Workaround für Paging-Problem aus Master übernommen (vgl. #482)

--- src/main/java/de/mediathekview/mserver/crawler/phoenix/tasks/PhoenixOverviewTask.java
+++ src/main/java/de/mediathekview/mserver/crawler/phoenix/tasks/PhoenixOverviewTask.java
@@ -17,38 +17,39 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class PhoenixOverviewTask extends ZdfTaskBase<CrawlerUrlDTO, CrawlerUrlDTO> {
 
   private static final Type OPTIONAL_OVERVIEW_DTO_TYPE_TOKEN =
-      new TypeToken<Optional<PagedElementListDTO<CrawlerUrlDTO>>>() {}.getType();
+          new TypeToken<Optional<PagedElementListDTO<CrawlerUrlDTO>>>() {
+          }.getType();
 
   private final String baseUrl;
   private final int subpage;
 
   public PhoenixOverviewTask(
-      final AbstractCrawler aCrawler,
-      final ConcurrentLinkedQueue<CrawlerUrlDTO> aUrlToCrawlDtos,
-      final Optional<String> aAuthKey,
-      final String aBaseUrl) {
+          final AbstractCrawler aCrawler,
+          final ConcurrentLinkedQueue<CrawlerUrlDTO> aUrlToCrawlDtos,
+          final Optional<String> aAuthKey,
+          final String aBaseUrl) {
     this(aCrawler, aUrlToCrawlDtos, aAuthKey, aBaseUrl, 0);
   }
 
   private PhoenixOverviewTask(
-      final AbstractCrawler aCrawler,
-      final ConcurrentLinkedQueue<CrawlerUrlDTO> aUrlToCrawlDtos,
-      final Optional<String> aAuthKey,
-      final String aBaseUrl,
-      final int aSubpage) {
+          final AbstractCrawler aCrawler,
+          final ConcurrentLinkedQueue<CrawlerUrlDTO> aUrlToCrawlDtos,
+          final Optional<String> aAuthKey,
+          final String aBaseUrl,
+          final int aSubpage) {
     super(aCrawler, aUrlToCrawlDtos, aAuthKey);
 
     baseUrl = aBaseUrl;
     subpage = aSubpage;
 
     registerJsonDeserializer(
-        OPTIONAL_OVERVIEW_DTO_TYPE_TOKEN, new PhoenixSendungOverviewDeserializer());
+            OPTIONAL_OVERVIEW_DTO_TYPE_TOKEN, new PhoenixSendungOverviewDeserializer());
   }
 
   @Override
   protected void processRestTarget(final CrawlerUrlDTO aDTO, final WebTarget aTarget) {
     final Optional<PagedElementListDTO<CrawlerUrlDTO>> overviewDtoOptional =
-        deserializeOptional(aTarget, OPTIONAL_OVERVIEW_DTO_TYPE_TOKEN);
+            deserializeOptional(aTarget, OPTIONAL_OVERVIEW_DTO_TYPE_TOKEN);
     if (!overviewDtoOptional.isPresent()) {
       crawler.incrementAndGetErrorCount();
       crawler.updateProgress();
@@ -58,7 +59,9 @@ public class PhoenixOverviewTask extends ZdfTaskBase<CrawlerUrlDTO, CrawlerUrlDT
     final PagedElementListDTO<CrawlerUrlDTO> overviewDto = overviewDtoOptional.get();
     addResults(overviewDto.getElements());
 
-    if (overviewDto.getNextPage().isPresent() && subpage < config.getMaximumSubpages()) {
+    if (overviewDto.getNextPage().isPresent() && subpage < config.getMaximumSubpages()
+            // Workaround to fix paging problem in Phönix-API
+            && !aDTO.getUrl().endsWith(overviewDto.getNextPage().get())) {
       taskResults.addAll(createNewOwnInstance(baseUrl + overviewDto.getNextPage().get()).invoke());
     }
   }
@@ -71,12 +74,12 @@ public class PhoenixOverviewTask extends ZdfTaskBase<CrawlerUrlDTO, CrawlerUrlDT
 
   @Override
   protected AbstractRecrusivConverterTask<CrawlerUrlDTO, CrawlerUrlDTO> createNewOwnInstance(
-      final ConcurrentLinkedQueue<CrawlerUrlDTO> aElementsToProcess) {
+          final ConcurrentLinkedQueue<CrawlerUrlDTO> aElementsToProcess) {
     return new PhoenixOverviewTask(crawler, aElementsToProcess, authKey, baseUrl, subpage + 1);
   }
 
   private AbstractRecrusivConverterTask<CrawlerUrlDTO, CrawlerUrlDTO> createNewOwnInstance(
-      final String aUrl) {
+          final String aUrl) {
     final ConcurrentLinkedQueue<CrawlerUrlDTO> queue = new ConcurrentLinkedQueue<>();
     queue.add(new CrawlerUrlDTO(aUrl));
     return createNewOwnInstance(queue);
