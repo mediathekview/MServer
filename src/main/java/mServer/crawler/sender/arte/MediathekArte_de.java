3ebaccc74307d2fb68037223761343b5ff941d45
pidoubleyou <pidoubleyou@arcor.de>
2019-09-14 09:14:29 +0200

    ARTE: activate crawler

--- src/main/java/mServer/crawler/sender/arte/MediathekArte_de.java
+++ src/main/java/mServer/crawler/sender/arte/MediathekArte_de.java
@@ -104,12 +104,6 @@ public class MediathekArte_de extends MediathekReader {
   @Override
   public void addToList() {
     meldungStart();
-
-    Log.sysLog("ARTE: deactivated...");
-    meldungThreadUndFertig();
-  }
-
-  private void originalAddToList() {
     if (Config.getStop()) {
       meldungThreadUndFertig();
     } else {
