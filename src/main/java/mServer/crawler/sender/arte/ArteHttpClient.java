afcc9284a0737cd5976399717e3d3be0eb2cd59c
pidoubleyou <pidoubleyou@arcor.de>
2019-09-14 09:14:19 +0200

    ARTE: Fix Merge Error

--- src/main/java/mServer/crawler/sender/arte/ArteHttpClient.java
+++ src/main/java/mServer/crawler/sender/arte/ArteHttpClient.java
@@ -18,70 +18,66 @@ import java.util.logging.Level;
  * Hilfsklasse für Arte Requests
  */
 public class ArteHttpClient {
-    public static final String AUTH_HEADER = "Authorization";
-    public static final String AUTH_TOKEN = "Bearer Nzc1Yjc1ZjJkYjk1NWFhN2I2MWEwMmRlMzAzNjI5NmU3NWU3ODg4ODJjOWMxNTMxYzEzZGRjYjg2ZGE4MmIwOA";
-    public static final String USER_AGENT = "User-Agent";
-    public static final String USER_AGENT_VALUE = "Mozilla/5.0";
-    private static final Builder opaBuilder;
-    private static final Builder builder;
 
-    static {
-        opaBuilder = new Request.Builder().addHeader(USER_AGENT, USER_AGENT_VALUE)
-                .addHeader(AUTH_HEADER, AUTH_TOKEN);
+  public static final String AUTH_HEADER = "Authorization";
+  public static final String AUTH_TOKEN = "Bearer Nzc1Yjc1ZjJkYjk1NWFhN2I2MWEwMmRlMzAzNjI5NmU3NWU3ODg4ODJjOWMxNTMxYzEzZGRjYjg2ZGE4MmIwOA";
+  public static final String USER_AGENT = "User-Agent";
+  public static final String USER_AGENT_VALUE = "Mozilla/5.0";
+  private static final Builder BUILDER_OPA;
+  private static final Builder BUILDER;
 
-        builder = new Request.Builder().addHeader(USER_AGENT, USER_AGENT_VALUE);
+  static {
+    BUILDER_OPA = new Request.Builder().addHeader(USER_AGENT, USER_AGENT_VALUE)
+            .addHeader(AUTH_HEADER, AUTH_TOKEN);
 
+    BUILDER = new Request.Builder().addHeader(USER_AGENT, USER_AGENT_VALUE);
+
+  }
+
+  private static Request createRequest(String aUrl) {
+    Builder b;
+    if (aUrl.contains("/opa/")) {
+      b = BUILDER_OPA;
+    } else {
+      b = BUILDER;
     }
 
-    private static Request createRequest(String aUrl) {
-        Builder b;
-        if (aUrl.contains("/opa/"))
-            b = opaBuilder;
-        else
-            b = builder;
+    return b.url(aUrl).build();
+  }
 
-        return b.url(aUrl).build();
-    }
+  public static <T> T executeRequest(Logger logger, Gson gson, String aUrl, Class<T> aDtoType) {
+    T result = null;
 
-    public static <T> T executeRequest(Logger logger, Gson gson, String aUrl, Class<T> aDtoType) {
-        T result = null;
+    java.util.logging.Logger x = java.util.logging.Logger.getLogger(OkHttpClient.class.getName());
+    x.setLevel(Level.FINE);
 
-        java.util.logging.Logger x = java.util.logging.Logger.getLogger(OkHttpClient.class.getName());
-        x.setLevel(Level.FINE);
+    try {
+      // Wartezeit nötig wegen zu vieler paralleler Requests
+      TimeUnit.MILLISECONDS.sleep(200);
 
-        try {
-            // Wartezeit nötig wegen zu vieler paralleler Requests
-            TimeUnit.MILLISECONDS.sleep(200);
+      Request request = createRequest(aUrl);
 
-            Request request = createRequest(aUrl);
+      boolean stop = false;
 
-            boolean stop = false;
-
-            do {
-                try (Response response = MVHttpClient.getInstance().getHttpClient().newCall(request).execute();
-                     ResponseBody body = response.body()) {
-                    //response can be successful but empty...and we have to close both!
-                    if (response.isSuccessful() && body != null) {
-                        result = gson.fromJson(body.string(), aDtoType);
-                        stop = true;
-                    } else {
-                        if (response.code() != 429) {
-                            logger.error(String.format("ARTE Request '%s' failed: %s", aUrl, response.code()));
-                            stop = true;
-                        } else {
-                            // bei 429 (too many requests) warten und nochmal versuchen
-                            try {
-                                TimeUnit.MILLISECONDS.sleep(500);
-                            } catch (InterruptedException ignored) {
-                            }
-                        }
-                    }
-                }
-            } while (!stop);
-
-        } catch (IOException | InterruptedException ex) {
-            logger.error("Beim laden der Filme für Arte kam es zu Verbindungsproblemen.", ex);
-
+      do {
+        try (Response response = MVHttpClient.getInstance().getHttpClient().newCall(request).execute();
+                ResponseBody body = response.body()) {
+          //response can be successful but empty...and we have to close both!
+          if (response.isSuccessful() && body != null) {
+            result = gson.fromJson(body.string(), aDtoType);
+            stop = true;
+          } else {
+            if (response.code() != 429) {
+              logger.error(String.format("ARTE Request '%s' failed: %s", aUrl, response.code()));
+              stop = true;
+            } else {
+              // bei 429 (too many requests) warten und nochmal versuchen
+              try {
+                TimeUnit.MILLISECONDS.sleep(500);
+              } catch (InterruptedException ignored) {
+              }
+            }
+          }
         }
       } while (!stop);
 
@@ -89,4 +85,7 @@ public class ArteHttpClient {
       logger.error("Beim laden der Filme für Arte kam es zu Verbindungsproblemen.", ex);
       Log.errorLog(3895449, ex);
     }
+
+    return result;
+  }
 }
