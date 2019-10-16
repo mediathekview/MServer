1787a1668f1f58abe3158362622f9bd4688f8d38
pidoubleyou <pidoubleyou@arcor.de>
2019-10-16 20:50:26 +0200

    SR: rate limit konfigurierbar

--- src/main/java/mServer/crawler/sender/sr/tasks/SrRateLimitedDocumentTask.java
+++ src/main/java/mServer/crawler/sender/sr/tasks/SrRateLimitedDocumentTask.java
@@ -5,12 +5,13 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import mServer.crawler.sender.MediathekReader;
 import mServer.crawler.sender.base.AbstractDocumentTask;
 import mServer.crawler.sender.base.CrawlerUrlDTO;
+import mServer.tool.MserverDaten;
 
 public abstract class SrRateLimitedDocumentTask<T, D extends CrawlerUrlDTO> extends AbstractDocumentTask<T, D> {
 
   private static final long serialVersionUID = -4077182368484515410L;
 
-  private static final RateLimiter LIMITER = RateLimiter.create(1.0);
+  private static final RateLimiter LIMITER = RateLimiter.create(MserverDaten.getSrRateLimit());
 
   public SrRateLimitedDocumentTask(MediathekReader aCrawler, ConcurrentLinkedQueue<D> aUrlToCrawlDTOs) {
     super(aCrawler, aUrlToCrawlDTOs);
