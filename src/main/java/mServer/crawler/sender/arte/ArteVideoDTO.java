e5ca339a2e3f5fe3056b7d75d826aa8d31b298ce
pidoubleyou <pidoubleyou@arcor.de>
2019-10-16 23:36:56 +0200

    ARTE: Hörfassungen

--- src/main/java/mServer/crawler/sender/arte/ArteVideoDTO.java
+++ src/main/java/mServer/crawler/sender/arte/ArteVideoDTO.java
@@ -6,35 +6,48 @@ import java.util.Map;
 import mServer.crawler.sender.newsearch.Qualities;
 
 public class ArteVideoDTO {
-    private final Map<Qualities,String> videoUrls;
-    private long durationInSeconds;
-    
-    public ArteVideoDTO()
-    {
-        videoUrls=new HashMap<>();
-        durationInSeconds = 0;
-    }
-    
-    public void addVideo(Qualities aQualitie, String aUrl)
-    {
-        videoUrls.put(aQualitie,aUrl);
-    }
-    
-    public Map<Qualities,String> getVideoUrls()
-    {
-        return videoUrls;
-    }
-    
-    public String getUrl(Qualities aQualitie)
-    {
-        return videoUrls.get(aQualitie);
-    }
-    
-    public void setDurationInSeconds(long durationInSeconds) {
-        this.durationInSeconds = durationInSeconds;
-    }
-    
-    public long getDurationInSeconds() {
-        return durationInSeconds;
-    }
+
+  private final Map<Qualities, String> videoUrls;
+  private final Map<Qualities, String> videoUrlsWithSubtitle;
+  private final Map<Qualities, String> videoUrlsWithAudioDescription;
+  private long durationInSeconds;
+
+  public ArteVideoDTO() {
+    videoUrls = new HashMap<>();
+    videoUrlsWithSubtitle = new HashMap<>();
+    videoUrlsWithAudioDescription = new HashMap<>();
+    durationInSeconds = 0;
+  }
+
+  public void addVideo(Qualities aQualitie, String aUrl) {
+    videoUrls.put(aQualitie, aUrl);
+  }
+
+  public void addVideoWithAudioDescription(Qualities qualities, String url) {
+    videoUrlsWithAudioDescription.put(qualities, url);
+  }
+
+  public void addVideoWithSubtitle(Qualities qualities, String url) {
+    videoUrlsWithSubtitle.put(qualities, url);
+  }
+
+  public Map<Qualities, String> getVideoUrls() {
+    return videoUrls;
+  }
+
+  public Map<Qualities, String> getVideoUrlsWithAudioDescription() {
+    return videoUrlsWithAudioDescription;
+  }
+
+  public Map<Qualities, String> getVideoUrlsWithSubtitle() {
+    return videoUrlsWithSubtitle;
+  }
+
+  public void setDurationInSeconds(long durationInSeconds) {
+    this.durationInSeconds = durationInSeconds;
+  }
+
+  public long getDurationInSeconds() {
+    return durationInSeconds;
+  }
 }
