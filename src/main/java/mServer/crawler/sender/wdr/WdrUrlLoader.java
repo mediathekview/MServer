614546c8abc8b51c7d3acc157410fcad6c640307
pidoubleyou <pidoubleyou@arcor.de>
2019-10-20 13:33:34 +0200

    Fiix several memory leaks and remove obsolete code.
    * Fix memory leak: RequestBody is released now
    
    * Remove obsolete member
    
    * remove obsolete rtmp
    
    * Trying to fix travis
    
    * Removed release on master so it will only released on tags.
    
    Advantage: We could first merge the changes to master (via GitHub) then change the version and release it with a tag.

--- src/main/java/mServer/crawler/sender/wdr/WdrUrlLoader.java
+++ src/main/java/mServer/crawler/sender/wdr/WdrUrlLoader.java
@@ -2,36 +2,35 @@ package mServer.crawler.sender.wdr;
 
 import de.mediathekview.mlib.tool.MVHttpClient;
 import java.io.IOException;
-import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class WdrUrlLoader {
-    
-    private final Logger LOG = LogManager.getLogger(WdrUrlLoader.class);
-    
-    public String executeRequest(String aUrl) {
-        String result = "";
 
-        try {
-            MVHttpClient mvhttpClient = MVHttpClient.getInstance();
-            OkHttpClient httpClient = mvhttpClient.getHttpClient();
-            Request request = new Request.Builder()
-                    .url(aUrl).build();
-            try (Response response = httpClient.newCall(request).execute()) {
-                if(response.isSuccessful()) {
-                    result = response.body().string();
-                } else {
-                    LOG.error(String.format("WDR Request '%s' failed: %s", aUrl, response.code()));
-                }
-            }
-            
-        } catch (IOException ex) {
-            LOG.error("Beim laden der Filme für WDR kam es zu Verbindungsproblemen.", ex);
+  private final Logger LOG = LogManager.getLogger(WdrUrlLoader.class);
+
+  public String executeRequest(String aUrl) {
+    String result = "";
+
+    try {
+      Request request = new Request.Builder()
+              .url(aUrl).build();
+      try (Response response = MVHttpClient.getInstance().getHttpClient().newCall(request).execute();
+              ResponseBody body = response.body()) {
+        if (response.isSuccessful() && body != null) {
+          result = body.string();
+        } else {
+          LOG.error(String.format("WDR Request '%s' failed: %s", aUrl, response.code()));
         }
+      }
 
-        return result;
-    }    
+    } catch (IOException ex) {
+      LOG.error("Beim laden der Filme für WDR kam es zu Verbindungsproblemen.", ex);
+    }
+
+    return result;
+  }
 }
