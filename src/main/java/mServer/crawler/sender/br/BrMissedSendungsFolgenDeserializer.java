d7f7dc79f70066e5d0c7fec31fc8fd2b281603ff
pidoubleyou <pidoubleyou@arcor.de>
2020-07-31 16:39:15 +0200

    Fix ClassCastException

--- src/main/java/mServer/crawler/sender/br/BrMissedSendungsFolgenDeserializer.java
+++ src/main/java/mServer/crawler/sender/br/BrMissedSendungsFolgenDeserializer.java
@@ -29,14 +29,15 @@ public class BrMissedSendungsFolgenDeserializer implements JsonDeserializer<BrId
    * Resolves the Sendung ids which are needed to get the Sendung details.<br>
    * The data has this structure:
    * <code>data -> viewer -> broadcastService -> containerToday -> edges[] -> node -> id</code>
+   *
    * @param aElement
    * @param aType
    * @param aContext
-   * @return 
+   * @return
    */
   @Override
   public BrIdsDTO deserialize(final JsonElement aElement, final Type aType,
-      final JsonDeserializationContext aContext) {
+          final JsonDeserializationContext aContext) {
     final BrIdsDTO results = new BrIdsDTO();
 
     final JsonObject baseObject = aElement.getAsJsonObject();
@@ -44,7 +45,8 @@ public class BrMissedSendungsFolgenDeserializer implements JsonDeserializer<BrId
     if (dayEntries.isPresent()) {
       for (final JsonElement dayEntry : dayEntries.get()) {
         final JsonObject dayEntryObject = dayEntry.getAsJsonObject();
-        if (dayEntryObject.has(JSON_ELEMENT_BROADCAST_EVENT)) {
+        if (dayEntryObject.has(JSON_ELEMENT_BROADCAST_EVENT)
+                && !dayEntryObject.get(JSON_ELEMENT_BROADCAST_EVENT).isJsonNull()) {
           final JsonObject broadcastEvent = dayEntryObject.getAsJsonObject(JSON_ELEMENT_BROADCAST_EVENT);
           if (broadcastEvent.has(JSON_ELEMENT_PUBLICATION)) {
             final JsonObject publication = broadcastEvent.getAsJsonObject(JSON_ELEMENT_PUBLICATION);
