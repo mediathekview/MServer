61c1c57b099802c5b2827eed0ac656bcf21e8874
CodingPF <j-doo@gmx.de>
2020-12-12 01:12:24 +0100

    nextPageId is missing even if there are subpages

--- src/main/java/de/mediathekview/mserver/crawler/arte/tasks/ArteSubcategoryVideosTask.java
+++ src/main/java/de/mediathekview/mserver/crawler/arte/tasks/ArteSubcategoryVideosTask.java
@@ -1,6 +1,9 @@
 package de.mediathekview.mserver.crawler.arte.tasks;
 
 import com.google.gson.reflect.TypeToken;
+
+import de.mediathekview.mserver.base.utils.UrlParseException;
+import de.mediathekview.mserver.base.utils.UrlUtils;
 import de.mediathekview.mserver.crawler.arte.ArteConstants;
 import de.mediathekview.mserver.crawler.arte.ArteFilmUrlDto;
 import de.mediathekview.mserver.crawler.arte.ArteLanguage;
@@ -11,18 +14,20 @@ import de.mediathekview.mserver.crawler.basic.AbstractRecursiveConverterTask;
 import de.mediathekview.mserver.crawler.basic.TopicUrlDTO;
 
 import javax.ws.rs.client.WebTarget;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.lang.reflect.Type;
-import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ArteSubcategoryVideosTask extends ArteTaskBase<ArteFilmUrlDto, TopicUrlDTO> {
-
+  private static final Logger LOG = LogManager.getLogger(ArteSubcategoryVideosTask.class);
   private static final Type SENDUNG_OVERVIEW_TYPE_TOKEN =
       new TypeToken<ArteSendungOverviewDto>() {}.getType();
 
-  private final int pageNumber;
   private final ArteLanguage language;
   private final String baseUrl;
 
@@ -30,24 +35,14 @@ public class ArteSubcategoryVideosTask extends ArteTaskBase<ArteFilmUrlDto, Topi
       final AbstractCrawler crawler,
       final Queue<TopicUrlDTO> urlToCrawlDTOs,
       final String baseUrl,
-      final ArteLanguage language,
-      final int pageNumber) {
+      final ArteLanguage language) {
     super(crawler, urlToCrawlDTOs, ArteConstants.AUTH_TOKEN);
 
     registerJsonDeserializer(
         SENDUNG_OVERVIEW_TYPE_TOKEN, new ArteSubcategoryVideosDeserializer(language));
 
     this.baseUrl = baseUrl;
-    this.pageNumber = pageNumber;
     this.language = language;
-  }
-
-  public ArteSubcategoryVideosTask(
-      final AbstractCrawler aCrawler,
-      final Queue<TopicUrlDTO> aUrlToCrawlDtos,
-      final String aBaseUrl,
-      final ArteLanguage aLanguage) {
-    this(aCrawler, aUrlToCrawlDtos, aBaseUrl, aLanguage, 1);
   }
 
   @Override
@@ -55,9 +50,15 @@ public class ArteSubcategoryVideosTask extends ArteTaskBase<ArteFilmUrlDto, Topi
     final ArteSendungOverviewDto result = deserialize(aTarget, SENDUNG_OVERVIEW_TYPE_TOKEN);
     if (result != null) {
       taskResults.addAll(result.getUrls());
-
-      final Optional<String> nextPageId = result.getNextPageId();
-      if (nextPageId.isPresent() && pageNumber < crawler.getCrawlerConfig().getMaximumSubpages()) {
+      //
+      int nextPageId = 0;
+      try {
+        nextPageId = Integer.parseInt(UrlUtils.getUrlParameterValue(aDTO.getUrl(),"page").get());
+      } catch (UrlParseException|NumberFormatException e) {
+        LOG.error("Failed to parse page from url {} error {}",aDTO.getUrl(), e.getMessage());
+      }
+      if (result.getUrls().size() == ArteConstants.SUBCATEGORY_LIMIT 
+          && nextPageId < crawler.getCrawlerConfig().getMaximumSubpages()) {
         // the nextPageId cannot be used to load the next page because authorization is not valid
         // => build new url
         final String url =
@@ -66,7 +67,7 @@ public class ArteSubcategoryVideosTask extends ArteTaskBase<ArteFilmUrlDto, Topi
                 baseUrl,
                 language.getLanguageCode().toLowerCase(),
                 aDTO.getTopic(),
-                pageNumber + 1);
+                nextPageId + 1);
         processNextPage(url, aDTO.getTopic());
       }
     }
@@ -83,6 +84,6 @@ public class ArteSubcategoryVideosTask extends ArteTaskBase<ArteFilmUrlDto, Topi
   protected AbstractRecursiveConverterTask<ArteFilmUrlDto, TopicUrlDTO> createNewOwnInstance(
       final Queue<TopicUrlDTO> aElementsToProcess) {
     return new ArteSubcategoryVideosTask(
-        crawler, aElementsToProcess, baseUrl, language, pageNumber + 1);
+        crawler, aElementsToProcess, baseUrl, language);
   }
 }
