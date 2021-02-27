ab6801d14d695f3e452d9202773da209ea5b64b3
pidoubleyou <pidoubleyou@arcor.de>
2021-02-27 19:04:14 +0100

    add https if protocol is missing in video url

--- src/main/java/mServer/crawler/sender/ard/json/ArdMediaArrayToDownloadUrlsConverter.java
+++ src/main/java/mServer/crawler/sender/ard/json/ArdMediaArrayToDownloadUrlsConverter.java
@@ -151,7 +151,7 @@ public class ArdMediaArrayToDownloadUrlsConverter {
         LOG.debug("Found an Sendung with the old RTMP format: " + url);
       } else {
         final ArdFilmUrlInfoDto info
-                = new ArdFilmUrlInfoDto(UrlUtils.removeParameters(UrlUtils.addProtocolIfMissing(url, "http:")), qualityText);
+                = new ArdFilmUrlInfoDto(UrlUtils.removeParameters(UrlUtils.addProtocolIfMissing(url, "https:")), qualityText);
         if (height.isPresent() && width.isPresent()) {
           info.setResolution(Integer.parseInt(width.get()), Integer.parseInt(height.get()));
         }
