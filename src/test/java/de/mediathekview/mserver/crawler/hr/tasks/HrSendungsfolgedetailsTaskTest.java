07cf2828ed1d07c8599691210d936aec788a42fe
pidoubleyou <pidoubleyou@arcor.de>
2021-03-28 18:57:22 +0200

    fix compile error

--- src/test/java/de/mediathekview/mserver/crawler/hr/tasks/HrSendungsfolgedetailsTaskTest.java
+++ src/test/java/de/mediathekview/mserver/crawler/hr/tasks/HrSendungsfolgedetailsTaskTest.java
@@ -25,6 +25,7 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
 
 @RunWith(Parameterized.class)
 public class HrSendungsfolgedetailsTaskTest extends HrTaskTestBase {
