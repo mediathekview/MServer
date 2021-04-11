35463d255ceb7be8d7455afda98bc01d07c89974
CodingPF <j-doo@gmx.de>
2021-04-11 23:15:20 +0200

    SRF crawler for days

--- src/main/java/de/mediathekview/mserver/crawler/srf/SrfCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/srf/SrfCrawler.java
@@ -9,13 +9,17 @@ import de.mediathekview.mserver.crawler.basic.AbstractCrawler;
 import de.mediathekview.mserver.crawler.basic.CrawlerUrlDTO;
 import de.mediathekview.mserver.crawler.basic.TopicUrlDTO;
 import de.mediathekview.mserver.crawler.srf.tasks.SrfFilmDetailTask;
+import de.mediathekview.mserver.crawler.srf.tasks.SrfScheduleOverviewTask;
 import de.mediathekview.mserver.crawler.srf.tasks.SrfTopicOverviewTask;
 import de.mediathekview.mserver.crawler.srf.tasks.SrfTopicsOverviewTask;
 import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -26,6 +30,9 @@ import java.util.concurrent.RecursiveTask;
 public class SrfCrawler extends AbstractCrawler {
 
   private static final Logger LOG = LogManager.getLogger(SrfCrawler.class);
+
+  private static final DateTimeFormatter ISO_DATE_FORMAT =
+      DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
   public SrfCrawler(
       final ForkJoinPool aForkJoinPool,
@@ -43,27 +50,36 @@ public class SrfCrawler extends AbstractCrawler {
   @Override
   protected RecursiveTask<Set<Film>> createCrawlerTask() {
     try {
-      final Queue<CrawlerUrlDTO> topicsUrls = new ConcurrentLinkedQueue<>();
-      topicsUrls.add(new CrawlerUrlDTO(SrfConstants.OVERVIEW_PAGE_URL));
-      final SrfTopicsOverviewTask overviewTask = new SrfTopicsOverviewTask(this, topicsUrls);
-      final Queue<TopicUrlDTO> topicUrls =
-          new ConcurrentLinkedQueue<>(forkJoinPool.submit(overviewTask).get());
+      final Set<CrawlerUrlDTO> dtos = new HashSet<CrawlerUrlDTO>();
+      //
+      final Queue<CrawlerUrlDTO> schedulePageUrls = createScheduleUrls();
+      final SrfScheduleOverviewTask schedulePageTask = new SrfScheduleOverviewTask(this, schedulePageUrls);
+      final Set<CrawlerUrlDTO> scheduleFilmUrls = forkJoinPool.submit(schedulePageTask).get();
+      dtos.addAll(scheduleFilmUrls);
+      //
+      //@TODO is topic search enabled
+      if (true) {
+        final Queue<CrawlerUrlDTO> topicsUrls = new ConcurrentLinkedQueue<>();
+        topicsUrls.add(new CrawlerUrlDTO(SrfConstants.OVERVIEW_PAGE_URL));
+        final SrfTopicsOverviewTask overviewTask = new SrfTopicsOverviewTask(this, topicsUrls);
+        final Queue<TopicUrlDTO> topicUrls =
+            new ConcurrentLinkedQueue<>(forkJoinPool.submit(overviewTask).get());
+  
+        printMessage(
+            ServerMessages.DEBUG_ALL_SENDUNG_COUNT, getSender().getName(), topicUrls.size());
+  
+        final SrfTopicOverviewTask task =
+            new SrfTopicOverviewTask(this, topicUrls, SrfConstants.BASE_URL);
+        final Set<CrawlerUrlDTO> topicSearchUrls = forkJoinPool.submit(task).get();
 
-      printMessage(
-          ServerMessages.DEBUG_ALL_SENDUNG_COUNT, getSender().getName(), topicUrls.size());
-
-      final SrfTopicOverviewTask task =
-          new SrfTopicOverviewTask(this, topicUrls, SrfConstants.BASE_URL);
-      forkJoinPool.execute(task);
-
-      final Queue<CrawlerUrlDTO> dtos = new ConcurrentLinkedQueue<>();
-      dtos.addAll(task.join());
-
+        dtos.addAll(topicSearchUrls);
+      }
+      //
       printMessage(
           ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), dtos.size());
       getAndSetMaxCount(dtos.size());
 
-      return new SrfFilmDetailTask(this, dtos);
+      return new SrfFilmDetailTask(this, new ConcurrentLinkedQueue<>(dtos));
 
     } catch (final InterruptedException | ExecutionException ex) {
       LOG.fatal("Exception in SRF crawler.", ex);
@@ -71,4 +87,16 @@ public class SrfCrawler extends AbstractCrawler {
     }
     return null;
   }
+
+  private Queue<CrawlerUrlDTO> createScheduleUrls() {
+    final Queue<CrawlerUrlDTO> scheduleUrls = new ConcurrentLinkedQueue<>();
+    final LocalDateTime now = LocalDateTime.now();
+    for (int i = 0; i <= crawlerConfig.getMaximumDaysForSendungVerpasstSection(); i++) {
+      final String day = now.minusDays(i).format(ISO_DATE_FORMAT);
+      final String url = String.format(SrfConstants.SCHEDULE_PER_DAY, day);
+      scheduleUrls.offer(new CrawlerUrlDTO(url));
+    }
+    return scheduleUrls;
+  }
+  
 }
