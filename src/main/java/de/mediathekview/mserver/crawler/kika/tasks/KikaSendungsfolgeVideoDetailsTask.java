ccc39969c9610560666cabf5c66ff9b06ca6e2bf
pidoubleyou <pidoubleyou@arcor.de>
2022-04-18 18:55:34 +0200

    #821 kika: fix geo

--- src/main/java/de/mediathekview/mserver/crawler/kika/tasks/KikaSendungsfolgeVideoDetailsTask.java
+++ src/main/java/de/mediathekview/mserver/crawler/kika/tasks/KikaSendungsfolgeVideoDetailsTask.java
@@ -2,6 +2,7 @@ package de.mediathekview.mserver.crawler.kika.tasks;
 
 import de.mediathekview.mlib.daten.*;
 import de.mediathekview.mserver.base.utils.DateUtils;
+import de.mediathekview.mserver.base.utils.GeoLocationGuesser;
 import de.mediathekview.mserver.base.utils.HtmlDocumentUtils;
 import de.mediathekview.mserver.crawler.basic.AbstractCrawler;
 import de.mediathekview.mserver.crawler.basic.AbstractUrlTask;
@@ -296,13 +297,7 @@ public class KikaSendungsfolgeVideoDetailsTask extends AbstractUrlTask<Film, Kik
       return;
     }
 
-    GeoLocations geoLocation = GeoLocations.GEO_NONE;
-    if (url.get().getUrl().getHost().contains("pmdgeokika")) {
-      geoLocation = GeoLocations.GEO_DE;
-    }
-
-    final Collection<GeoLocations> geoLocations = new ArrayList<>();
-    geoLocations.add(geoLocation);
+    final Collection<GeoLocations> geoLocations = GeoLocationGuesser.getGeoLocations(Sender.KIKA, url.get().toString());
     newFilm.setGeoLocations(geoLocations);
   }
 
