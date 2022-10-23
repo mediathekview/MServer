418d0ebea4c70782b7185ace9beac722e43f861a
ForBai <thelars42@gmail.com>
2022-10-23 13:26:59 +0200

    Updated to JUnit5 and used assertJ for assertions

--- src/test/java/de/mediathekview/mserver/crawler/swr/SwrUrlOptimizerTest.java
+++ src/test/java/de/mediathekview/mserver/crawler/swr/SwrUrlOptimizerTest.java
@@ -5,20 +5,19 @@ import de.mediathekview.mserver.base.config.MServerConfigManager;
 import de.mediathekview.mserver.crawler.ard.ArdCrawler;
 import de.mediathekview.mserver.crawler.ard.ArdUrlOptimizer;
 import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
-import de.mediathekview.mserver.testhelper.WireMockTestBase;
-import org.junit.Test;
+import de.mediathekview.mserver.testhelper.WireMockTestBaseJUnit5;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.ForkJoinPool;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-public class SwrUrlOptimizerTest extends WireMockTestBase {
+class SwrUrlOptimizerTest extends WireMockTestBaseJUnit5 {
 
   @Test
-  public void optimizeHdUrlTestFullHdExists() {
+  void optimizeHdUrlTestFullHdExists() {
     final String url = getWireMockBaseUrlSafe() + "/845421.xl.mp4";
     final String expectedUrl = getWireMockBaseUrlSafe() + "/845421.xxl.mp4";
     setupHeadResponse("/845421.xxl.mp4", 200);
@@ -26,28 +25,28 @@ public class SwrUrlOptimizerTest extends WireMockTestBase {
     final ArdUrlOptimizer target = new ArdUrlOptimizer(createCrawler());
     final String actualUrl = target.optimizeHdUrl(url);
 
-    assertThat(actualUrl, equalTo(expectedUrl));
+    assertThat(actualUrl).isEqualTo(expectedUrl);
   }
 
   @Test
-  public void optimizeHdUrlTestFullHdDoesNotExists() {
+  void optimizeHdUrlTestFullHdDoesNotExists() {
     final String url = getWireMockBaseUrlSafe() + "/845421.xl.mp4";
     setupHeadResponse("/845421.xxl.mp4", 404);
 
     final ArdUrlOptimizer target = new ArdUrlOptimizer(createCrawler());
     final String actualUrl = target.optimizeHdUrl(url);
 
-    assertThat(actualUrl, equalTo(url));
+    assertThat(actualUrl).isEqualTo(url);
   }
 
   @Test
-  public void optimizeHdUrlTestNoUrlToOptimize() {
+  void optimizeHdUrlTestNoUrlToOptimize() {
     final String url = getWireMockBaseUrlSafe() + "/78946584.l.mp4";
 
     final ArdUrlOptimizer target = new ArdUrlOptimizer(createCrawler());
     final String actualUrl = target.optimizeHdUrl(url);
 
-    assertThat(actualUrl, equalTo(url));
+    assertThat(actualUrl).isEqualTo(url);
   }
 
   protected ArdCrawler createCrawler() {
