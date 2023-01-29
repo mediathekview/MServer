bc40b8ebd7344e9bdee0fa52c6e692e8f1248806
pidoubleyou <pidoubleyou@arcor.de>
2023-01-29 19:58:31 +0100

    add another hd url

--- src/main/java/mServer/crawler/sender/zdf/ZdfVideoUrlOptimizer.java
+++ src/main/java/mServer/crawler/sender/zdf/ZdfVideoUrlOptimizer.java
@@ -37,6 +37,7 @@ public class ZdfVideoUrlOptimizer {
   private static final String HD_3360_36_15 = "3360k_p36v15.mp4";
   private static final String HD_3360_36_17 = "3360k_p36v17.mp4";
   private static final String HD_6628_61_17 = "6628k_p61v17.mp4";
+  private static final String HD_6660_37_17 = "6660k_p37v17.mp4";
 
   private static final Map<String, String[]> NORMAL_OPTIMIZE = new HashMap<>();
   private static final Map<String, String[]> HD_OPTIMIZE = new HashMap<>();
@@ -53,8 +54,8 @@ public class ZdfVideoUrlOptimizer {
     NORMAL_OPTIMIZE.put(NORMAL_1496_13_14, new String[]{NORMAL_2328_35_14, NORMAL_2296_14_14});
     NORMAL_OPTIMIZE.put(NORMAL_1628_13_15, new String[]{NORMAL_2360_35_15});
 
-    HD_OPTIMIZE.put(NORMAL_2360_35_17, new String[]{HD_6628_61_17, HD_3360_36_17});
-    HD_OPTIMIZE.put(NORMAL_1628_13_17, new String[]{HD_6628_61_17, HD_3360_36_17});
+    HD_OPTIMIZE.put(NORMAL_2360_35_17, new String[]{HD_6660_37_17, HD_6628_61_17, HD_3360_36_17});
+    HD_OPTIMIZE.put(NORMAL_1628_13_17, new String[]{HD_6660_37_17, HD_6628_61_17, HD_3360_36_17});
     HD_OPTIMIZE.put(NORMAL_1456_13_12, new String[]{HD_3328_12, HD_3256});
     HD_OPTIMIZE.put(NORMAL_2256_14_12, new String[]{HD_3328_12, HD_3256});
     HD_OPTIMIZE.put(NORMAL_2328_35_12, new String[]{HD_3328_12, HD_3256});
