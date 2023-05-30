7d511fa4d5f2d77c3c086466f1882e31ce0b7c42
Alexander F <alex@elaon.de>
2023-05-31 01:14:58 +0200

    Umbau auf neue Filtermethode

--- src/main/java/mServer/crawler/BannedFilmFilter.java
+++ src/main/java/mServer/crawler/BannedFilmFilter.java
@@ -11,7 +11,15 @@ public class BannedFilmFilter {
     if (film.arr[DatenFilm.FILM_TITEL].equalsIgnoreCase("Wir haben genug - Wirtschaft ohne Wachstum")) {
       return true;
     }
-    if (film.arr[DatenFilm.FILM_TITEL].equalsIgnoreCase("Auslegung der Wirklichkeit - Georg Stefan Troller - Dokumentarfilm von Ruth Rieser, Österreich 2021")) {
+    // if (film.arr[DatenFilm.FILM_TITEL].equalsIgnoreCase("Auslegung der Wirklichkeit - Georg Stefan Troller - Dokumentarfilm von Ruth Rieser, Österreich 2021")) {
+    //   return true;
+    // }
+    final String title = film.arr[DatenFilm.FILM_TITEL].toLowerCase();
+    if (title.contains("auslegung der wirklichkeit") 
+            && title.contains("georg stefan troller") 
+            && title.contains("dokumentarfilm von ruth rieser") 
+            && title.contains("österreich 2021"))
+    {
       return true;
     }
     
