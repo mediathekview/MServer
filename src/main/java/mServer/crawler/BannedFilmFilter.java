0688aed199564fbe4c587203c018c148f0b7ef44
codingPF <j-doo@gmx.de>
2023-06-09 16:22:05 +0200

    Update BannedFilmFilter.java
    un-neest objcet in try-with-resource to ensure they are closed indv.

--- src/main/java/mServer/crawler/BannedFilmFilter.java
+++ src/main/java/mServer/crawler/BannedFilmFilter.java
@@ -20,55 +20,59 @@ public class BannedFilmFilter {
   private List<String> bannedTitles = new ArrayList<String>();
 	
   public BannedFilmFilter() {
-	Log.progress("create BannedFilmFilter from " + MserverDaten.system[MserverKonstanten.SYSTEM_BANNEDFILMLIST_NR] );
-	bannedTitles = new ArrayList<String>();
-	try (BufferedReader reader = new BufferedReader(new InputStreamReader(getInputStreamFromPath(MserverDaten.system[MserverKonstanten.SYSTEM_BANNEDFILMLIST_NR])))) {
-		String line = "";
-		while ((line = reader.readLine()) != null) {
+  	Log.progress("create BannedFilmFilter from " + MserverDaten.system[MserverKonstanten.SYSTEM_BANNEDFILMLIST_NR] );
+  	bannedTitles = new ArrayList<String>();
+  	try (
+  	        InputStream is = getInputStreamFromPath(MserverDaten.system[MserverKonstanten.SYSTEM_BANNEDFILMLIST_NR]);
+  			    InputStreamReader isr = new InputStreamReader(is);
+  			    BufferedReader reader = new BufferedReader(isr);
+  	) {
+  		String line = "";
+  		while ((line = reader.readLine()) != null) {
         if (line.trim().length() > 0) {
           bannedTitles.add(line.trim());
           Log.progress("add entry to bannedFilmList");
         }
       }
-    }catch (FileNotFoundException e) {
-        Log.errorLog(-1, e);
+    } catch (FileNotFoundException e) {
+      Log.errorLog(-1, e);
     } catch (IOException e) {
-    	Log.errorLog(-1, e);
+      Log.errorLog(-1, e);
     }
   }
 	
   public boolean isBanned(final DatenFilm film) {
-  	for (String title : bannedTitles) {
-	  	if (film.arr[DatenFilm.FILM_TITEL].toUpperCase().contains(title.toUpperCase())) {
-		  	return true;
-		  }
-	  }
+	for (String title : bannedTitles) {
+		if (film.arr[DatenFilm.FILM_TITEL].toUpperCase().contains(title.toUpperCase())) {
+			return true;
+		}
+	}
     return false;
   }
   
   public static InputStream getInputStreamFromPath(String path) throws IOException {
-	  InputStream is;
-	  String protocol = path.replaceFirst("^(\\w+):.+$", "$1").toLowerCase();
-	  switch (protocol) {
-	    case "http":
-	    case "https":
-	      HttpURLConnection connection = (HttpURLConnection) new URL(path).openConnection();
-	      int code = connection.getResponseCode();
-	      if (code >= 400) throw new IOException("Server returned error code #" + code);
-	      is = connection.getInputStream();
-	      String contentEncoding = connection.getContentEncoding();
-	      if (contentEncoding != null && contentEncoding.equalsIgnoreCase("gzip"))
-	        is = new GZIPInputStream(is);
-	        break;
-	    case "file":
-	      is = new URL(path).openStream();
-	        break;
-	    case "classpath":
-	      is = Thread.currentThread().getContextClassLoader().getResourceAsStream(path.replaceFirst("^\\w+:", ""));
-	      break;
-	    default:
-	      throw new IOException("Missed or unsupported protocol in path '" + path + "'");
-	  }
-	  return is;
+	    InputStream is;
+	    String protocol = path.replaceFirst("^(\\w+):.+$", "$1").toLowerCase();
+	    switch (protocol) {
+	        case "http":
+	        case "https":
+	            HttpURLConnection connection = (HttpURLConnection) new URL(path).openConnection();
+	            int code = connection.getResponseCode();
+	            if (code >= 400) throw new IOException("Server returned error code #" + code);
+	            is = connection.getInputStream();
+	            String contentEncoding = connection.getContentEncoding();
+	            if (contentEncoding != null && contentEncoding.equalsIgnoreCase("gzip"))
+	                is = new GZIPInputStream(is);
+	            break;
+	        case "file":
+	            is = new URL(path).openStream();
+	            break;
+	        case "classpath":
+	            is = Thread.currentThread().getContextClassLoader().getResourceAsStream(path.replaceFirst("^\\w+:", ""));
+	            break;
+	        default:
+	            throw new IOException("Missed or unsupported protocol in path '" + path + "'");
+	    }
+	    return is;
 	}
 }
