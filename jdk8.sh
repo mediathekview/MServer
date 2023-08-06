cbe3ba30ddd91521b269b231dd328bb10673d7cd
Alexander F <alex@elaon.de>
2023-08-07 00:22:48 +0200

    Abfrage Versionsdatei korrigiert

---
+++ jdk8.sh
@@ -0,0 +1,18 @@
+# einlesen mit:
+# source jdk11.sh
+
+#PFAD="/usr/lib/jvm/java-8-openjdk-amd64"
+
+#export J2SDKDIR=$PFAD
+#export J2REDIR=$PFAD
+#export PATH=$PATH:${PFAD}/bin:${PFAD}/db/bin
+#export JAVA_HOME=${PFAD}
+#export DERBY_HOME=${PFAD}/db
+
+
+# with sdkman
+
+sdk use java 8.0.382.fx-zulu
+
+# list sdk's for java
+sdk list java
