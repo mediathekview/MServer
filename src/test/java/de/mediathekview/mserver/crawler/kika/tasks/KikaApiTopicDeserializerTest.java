0626c6c68fb1e5b2879d7eb2448e70dc23b97f5e
ForBai <thelars42@gmail.com>
2023-10-01 12:40:18 +0200

    Updated KikaApiTopicDeserializerTest to JUnit5

--- src/test/java/de/mediathekview/mserver/crawler/kika/tasks/KikaApiTopicDeserializerTest.java
+++ src/test/java/de/mediathekview/mserver/crawler/kika/tasks/KikaApiTopicDeserializerTest.java
@@ -1,69 +1,34 @@
 package de.mediathekview.mserver.crawler.kika.tasks;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
-
 import de.mediathekview.mserver.crawler.kika.json.KikaApiTopicDto;
 import de.mediathekview.mserver.crawler.kika.json.KikaApiTopicPageDeserializer;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
-import static org.junit.Assert.assertEquals;
-
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Set;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-
-@RunWith(Parameterized.class)
 public class KikaApiTopicDeserializerTest {
 
-  private String jsonFile = "";
-  private int numberOfExpectedRecords = 0;
-  private boolean hasSubpage = false;
-  private boolean hasError = false;
-  
-
-  public KikaApiTopicDeserializerTest(String jsonFile, int numberOfExpectedRecords, boolean subpage, boolean error) {
-    this.jsonFile = jsonFile;
-    this.numberOfExpectedRecords = numberOfExpectedRecords;
-    this.hasSubpage = subpage;
-    this.hasError = error;
-  }
-
-  @Parameterized.Parameters
-  public static Object[][] data() {
-    return new Object[][] {
-          {
-            "/kika/KikaApiTopic1.json",
-            10,
-            true,
-            false
-          },
-          {
-            "/kika/KikaApiTopic2.json",
-            11,
-            false,
-            false
-          },
-          {
-            "/kika/KikaApiError.json",
-            0,
-            false,
-            true
-          }
-        };
-
-  }
-
-  @Test
-  public void testDeserializeBrand() throws URISyntaxException, IOException {
+  @ParameterizedTest
+  @MethodSource("getTestData")
+  void testDeserializeBrand(
+      final String jsonFile,
+      final int numberOfExpectedRecords,
+      final boolean hasSubpage,
+      final boolean hasError)
+      throws URISyntaxException, IOException {
     final Type KikaApiTopicDtoType = new TypeToken<Set<KikaApiTopicDto>>() {}.getType();
     //
     final Gson gson =
@@ -83,5 +48,11 @@ public class KikaApiTopicDeserializerTest {
     assertEquals(hasError, aKikaApiTopicDto.getErrorCode().isPresent());
     //
   }
-  
+
+  static Stream<Arguments> getTestData() {
+    return Stream.of(
+        arguments("/kika/KikaApiTopic1.json", 10, true, false),
+        arguments("/kika/KikaApiTopic2.json", 11, false, false),
+        arguments("/kika/KikaApiError.json", 0, false, true));
+  }
 }
