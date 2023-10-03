d86a11582ae97513ccca982c8e2a926ff15b8018
pidoubleyou <pidoubleyou@arcor.de>
2023-10-03 08:56:03 +0200

    remove br crawler

--- src/main/java/de/mediathekview/mserver/crawler/br/json/BrVideo.java
+++
@@ -1,7 +0,0 @@
-package de.mediathekview.mserver.crawler.br.json;
-
-import de.mediathekview.mlib.daten.FilmUrl;
-import de.mediathekview.mlib.daten.Resolution;
-
-public record BrVideo(Resolution resolution, FilmUrl url) {
-}
