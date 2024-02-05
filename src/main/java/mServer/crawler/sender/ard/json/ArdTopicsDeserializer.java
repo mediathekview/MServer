16a648215813ac1682dcc2b18e172b14402a4864
pidoubleyou <pidoubleyou@arcor.de>
2024-02-05 22:18:21 +0100

    use new experimental topics-urls

---
+++ src/main/java/mServer/crawler/sender/ard/json/ArdTopicsDeserializer.java
@@ -0,0 +1,62 @@
+package mServer.crawler.sender.ard.json;
+
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import mServer.crawler.sender.ard.ArdConstants;
+import mServer.crawler.sender.base.CrawlerUrlDTO;
+import mServer.crawler.sender.base.JsonUtils;
+
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+public class ArdTopicsDeserializer implements JsonDeserializer<Set<CrawlerUrlDTO>> {
+  private static final String ELEMENT_WIDGETS = "widgets";
+  private static final String ELEMENT_LINKS = "links";
+  private static final String ELEMENT_SELF = "self";
+
+  private static final String ATTRIBUTE_ID = "id";
+
+  private final String sender;
+
+  public ArdTopicsDeserializer(String sender) {
+    this.sender = sender;
+  }
+
+  @Override
+  public Set<CrawlerUrlDTO> deserialize(
+          JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) {
+    final Set<CrawlerUrlDTO> result = new HashSet<>();
+
+    if (JsonUtils.hasElements(jsonElement, ELEMENT_WIDGETS)) {
+      final JsonArray widgets = jsonElement.getAsJsonObject().getAsJsonArray(ELEMENT_WIDGETS);
+      widgets.forEach(widget -> parseWidget(widget.getAsJsonObject()).ifPresent(result::add));
+    }
+
+    return result;
+  }
+
+  private Optional<CrawlerUrlDTO> parseWidget(final JsonElement compilation) {
+    if (JsonUtils.hasElements(compilation, ELEMENT_LINKS)) {
+      final JsonElement selfLink =
+              compilation.getAsJsonObject().get(ELEMENT_LINKS).getAsJsonObject().get(ELEMENT_SELF);
+      final Optional<String> id =
+              JsonUtils.getAttributeAsString(selfLink.getAsJsonObject(), ATTRIBUTE_ID);
+      if (id.isPresent()) {
+        return Optional.of(
+                new CrawlerUrlDTO(
+                        String.format(
+                                ArdConstants.TOPICS_COMPILATION_URL,
+                                sender,
+                                id.get(),
+                                ArdConstants.TOPICS_COMPILATION_PAGE_SIZE)));
+      }
+    }
+
+    return Optional.empty();
+  }
+}
