02e381d9533a845dcedef75f58cb1c9152ffdc50
pidoubleyou <22942659+pidoubleyou@users.noreply.github.com>
2025-07-13 12:53:24 +0200

    new ZDF Mediathek (#1049)

--- src/main/java/de/mediathekview/mserver/crawler/zdf/ZdfCrawler.java
+++ src/main/java/de/mediathekview/mserver/crawler/zdf/ZdfCrawler.java
@@ -1,51 +1,40 @@
 package de.mediathekview.mserver.crawler.zdf;
 
+import de.mediathekview.mlib.daten.Film;
 import de.mediathekview.mlib.daten.Sender;
 import de.mediathekview.mlib.messages.listener.MessageListener;
 import de.mediathekview.mserver.base.config.MServerConfigManager;
+import de.mediathekview.mserver.base.messages.ServerMessages;
+import de.mediathekview.mserver.crawler.basic.AbstractCrawler;
 import de.mediathekview.mserver.crawler.basic.CrawlerUrlDTO;
-import de.mediathekview.mserver.crawler.zdf.tasks.ZdfDayPageHtmlTask;
-import de.mediathekview.mserver.crawler.zdf.tasks.ZdfLetterListHtmlTask;
-import de.mediathekview.mserver.crawler.zdf.tasks.ZdfTopicPageHtmlTask;
-import de.mediathekview.mserver.crawler.zdf.tasks.ZdfTopicsPageHtmlTask;
+import de.mediathekview.mserver.crawler.zdf.tasks.*;
 import de.mediathekview.mserver.progress.listeners.SenderProgressListener;
-import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Collection;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
-public class ZdfCrawler extends AbstractZdfCrawler {
+public class ZdfCrawler extends AbstractCrawler {
 
-  private static final int MAXIMUM_DAYS_HTML_PAST = 7;
+  private static final Logger LOG = LogManager.getLogger(ZdfCrawler.class);
+  private static final int MAX_LETTER_PAGEGS = 27;
+
+  private static final String AUTH_KEY = "aa3noh4ohz9eeboo8shiesheec9ciequ9Quah7el";
 
   public ZdfCrawler(
-      final ForkJoinPool aForkJoinPool,
-      final Collection<MessageListener> aMessageListeners,
-      final Collection<SenderProgressListener> aProgressListeners,
-      final MServerConfigManager rootConfig) {
-    super(aForkJoinPool, aMessageListeners, aProgressListeners, rootConfig, ZdfConstants.PARTNER_TO_SENDER);
-  }
-
-  @Override
-  protected @NotNull String getUrlBase() {
-    return ZdfConstants.URL_BASE;
-  }
-
-  @Override
-  protected String getApiUrlBase() {
-    return ZdfConstants.URL_API_BASE;
-  }
-
-  @Override
-  protected @NotNull String getUrlDay() {
-    return ZdfConstants.URL_DAY;
+      ForkJoinPool aForkJoinPool,
+      Collection<MessageListener> aMessageListeners,
+      Collection<SenderProgressListener> aProgressListeners,
+      MServerConfigManager rootConfig) {
+    super(aForkJoinPool, aMessageListeners, aProgressListeners, rootConfig);
   }
 
   @Override
@@ -54,52 +43,138 @@ public class ZdfCrawler extends AbstractZdfCrawler {
   }
 
   @Override
-  public Queue<CrawlerUrlDTO> getTopicsEntries() throws ExecutionException, InterruptedException {
+  protected RecursiveTask<Set<Film>> createCrawlerTask() {
 
-    final Queue<CrawlerUrlDTO> letterListUrl = new ConcurrentLinkedQueue<>();
-    letterListUrl.add(new CrawlerUrlDTO(ZdfConstants.URL_TOPICS));
+    try {
 
-    final ZdfLetterListHtmlTask letterTask = new ZdfLetterListHtmlTask(this, letterListUrl);
-    final Set<CrawlerUrlDTO> letterUrls = forkJoinPool.submit(letterTask).get();
+      if (Boolean.TRUE.equals(crawlerConfig.getTopicsSearchEnabled())) {
+        final Set<ZdfFilmDto> shows = new HashSet<>();
 
-    final ZdfTopicsPageHtmlTask topicsTask =
-        new ZdfTopicsPageHtmlTask(this, new ConcurrentLinkedQueue<>(letterUrls));
-    final Set<CrawlerUrlDTO> topicsUrls = forkJoinPool.submit(topicsTask).get();
+        ZdfLetterPageTask letterPageTask =
+            new ZdfLetterPageTask(this, createLetterPageUrls(), AUTH_KEY);
+        final Set<ZdfTopicUrlDto> topicUrls = forkJoinPool.submit(letterPageTask).get();
 
-    final ZdfTopicPageHtmlTask topicTask =
-        new ZdfTopicPageHtmlTask(this, new ConcurrentLinkedQueue<>(topicsUrls));
-    return new ConcurrentLinkedQueue<>(forkJoinPool.submit(topicTask).get());
+        printMessage(
+            ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), topicUrls.size());
+
+        final ZdfPubFormTask pubFormTask = new ZdfPubFormTask(this, createPubFormUrls(), AUTH_KEY);
+        final Set<ZdfPubFormResult> pubFormUrls = forkJoinPool.submit(pubFormTask).get();
+
+        printMessage(
+            ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT,
+            getSender().getName() + " - PubForm:",
+            pubFormUrls.size());
+
+        pubFormUrls.forEach(
+            pubFormResult -> {
+              topicUrls.addAll(pubFormResult.getTopics().getElements());
+              shows.addAll(pubFormResult.getFilms());
+            });
+        printMessage(
+            ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT,
+            getSender().getName() + " - PubForm-Topics integrated: ",
+            topicUrls.size());
+
+        ZdfTopicSeasonTask topicSeasonTask =
+            new ZdfTopicSeasonTask(this, new ConcurrentLinkedQueue<>(topicUrls), AUTH_KEY);
+        shows.addAll(forkJoinPool.submit(topicSeasonTask).get());
+
+        printMessage(
+            ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+
+        return new ZdfFilmTask(this, new ConcurrentLinkedQueue<>(shows), AUTH_KEY);
+      } else {
+        final ZdfConfiguration configuration = loadConfiguration();
+        if (configuration.getSearchAuthKey().isPresent()
+            && configuration.getVideoAuthKey().isPresent()) {
+          Set<CrawlerUrlDTO> shows = new HashSet<>(getDaysEntries(configuration));
+          printMessage(
+                  ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+
+          return new ZdfFilmDetailTask(
+              this,
+              getApiUrlBase(),
+              new ConcurrentLinkedQueue<>(shows),
+              configuration.getVideoAuthKey().orElse(""), ZdfConstants.PARTNER_TO_SENDER);
+        }
+      }
+    } catch (final InterruptedException ex) {
+      LOG.debug("{} crawler interrupted.", getSender().getName(), ex);
+      Thread.currentThread().interrupt();
+    } catch (final ExecutionException ex) {
+      LOG.fatal("Exception in {} crawler.", getSender().getName(), ex);
+    }
+    return null;
   }
 
-  @Override
-  protected Collection<CrawlerUrlDTO> getExtraDaysEntries()
-      throws ExecutionException, InterruptedException {
-
-    final ZdfDayPageHtmlTask dayTask =
-        new ZdfDayPageHtmlTask(getApiUrlBase(), this, getExtraDayUrls());
-    return forkJoinPool.submit(dayTask).get();
+  private Queue<ZdfPubFormDto> createPubFormUrls() {
+    Queue<ZdfPubFormDto> urls = new ConcurrentLinkedQueue<>();
+    ZdfConstants.SPECIAL_COLLECTION_IDS.forEach(
+        (collectionId, topic) -> {
+          final String url =
+              ZdfUrlBuilder.buildTopicNoSeasonUrl(
+                  ZdfConstants.EPISODES_PAGE_SIZE, collectionId, ZdfConstants.NO_CURSOR);
+          urls.add(new ZdfPubFormDto(topic, collectionId, url));
+        });
+    return urls;
   }
 
-  private Queue<CrawlerUrlDTO> getExtraDayUrls() {
+  private Queue<ZdfLetterDto> createLetterPageUrls() {
+    final Queue<ZdfLetterDto> urls = new ConcurrentLinkedQueue<>();
+    for (int i = 0; i < MAX_LETTER_PAGEGS; i++) {
+      urls.add(new ZdfLetterDto(i, ZdfUrlBuilder.buildLetterPageUrl(ZdfConstants.NO_CURSOR, i)));
+    }
+
+    return urls;
+  }
+
+  protected ZdfConfiguration loadConfiguration() throws ExecutionException, InterruptedException {
+    final ZdfIndexPageTask task = new ZdfIndexPageTask(this, getUrlBase());
+    return forkJoinPool.submit(task).get();
+  }
+
+  private Set<CrawlerUrlDTO> getDaysEntries(final ZdfConfiguration configuration)
+          throws InterruptedException, ExecutionException {
+    final ZdfDayPageTask dayTask =
+            new ZdfDayPageTask(
+                    this, getApiUrlBase(), getDayUrls(), configuration.getSearchAuthKey().orElse(null));
+    final Set<CrawlerUrlDTO> shows = forkJoinPool.submit(dayTask).get();
+
+    printMessage(
+            ServerMessages.DEBUG_ALL_SENDUNG_FOLGEN_COUNT, getSender().getName(), shows.size());
+
+    return shows;
+  }
+
+  private Queue<CrawlerUrlDTO> getDayUrls() {
     final Queue<CrawlerUrlDTO> urls = new ConcurrentLinkedQueue<>();
-    for (int i = 0; i <= getMaximumDaysPast(); i++) {
+    for (int i = 0;
+         i
+                 <= crawlerConfig.getMaximumDaysForSendungVerpasstSection()
+                 + crawlerConfig.getMaximumDaysForSendungVerpasstSectionFuture();
+         i++) {
 
-      final LocalDateTime local = LocalDateTime.now().minus(i, ChronoUnit.DAYS);
+      final LocalDateTime local =
+              LocalDateTime.now()
+                      .plus(crawlerConfig.getMaximumDaysForSendungVerpasstSectionFuture(), ChronoUnit.DAYS)
+                      .minus(i, ChronoUnit.DAYS);
       final String date = local.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-      final String url = String.format(ZdfConstants.URL_HTML_DAY, date);
+      final String url = String.format(getUrlDay(), date, date);
       urls.add(new CrawlerUrlDTO(url));
     }
 
     return urls;
   }
 
-  private int getMaximumDaysPast() {
-    final Integer maximumDaysForSendungVerpasstSection =
-        crawlerConfig.getMaximumDaysForSendungVerpasstSection();
-    if (maximumDaysForSendungVerpasstSection == null
-        || maximumDaysForSendungVerpasstSection > MAXIMUM_DAYS_HTML_PAST) {
-      return MAXIMUM_DAYS_HTML_PAST;
-    }
-    return maximumDaysForSendungVerpasstSection;
+  private @NotNull String getUrlBase() {
+    return ZdfConstants.URL_BASE;
+  }
+
+  private String getApiUrlBase() {
+    return ZdfConstants.URL_API_BASE;
+  }
+
+  private @NotNull String getUrlDay() {
+    return ZdfConstants.URL_DAY;
   }
 }
---
+++ src/main/java/de/mediathekview/mserver/crawler/zdf/json/ZdfTopicSeasonDeserializer.java
@@ -0,0 +1,68 @@
+package de.mediathekview.mserver.crawler.zdf.json;
+
+import com.google.gson.*;
+import de.mediathekview.mserver.base.utils.JsonUtils;
+import de.mediathekview.mserver.crawler.basic.PagedElementListDTO;
+import de.mediathekview.mserver.crawler.zdf.ZdfFilmDto;
+import java.lang.reflect.Type;
+import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ZdfTopicSeasonDeserializer extends ZdfTopicBaseClass
+    implements JsonDeserializer<PagedElementListDTO<ZdfFilmDto>> {
+
+  private static final Logger LOG = LogManager.getLogger(ZdfTopicSeasonDeserializer.class);
+
+  @Override
+  public PagedElementListDTO<ZdfFilmDto> deserialize(
+      JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
+      throws JsonParseException {
+    final PagedElementListDTO<ZdfFilmDto> films = new PagedElementListDTO<>();
+
+    JsonObject rootNode = jsonElement.getAsJsonObject();
+    final JsonObject data = rootNode.getAsJsonObject("data");
+    if (data.isJsonNull()) {
+      LOG.error("ZdfTopicSeasonDeserializer: No data found in response");
+      return films;
+    }
+
+    if (data.has("smartCollectionByCanonical")) {
+      final Optional<JsonElement> optionalNodesElement =
+          JsonUtils.getElement(data, "smartCollectionByCanonical", "seasons", "nodes");
+      if (optionalNodesElement.isPresent() && optionalNodesElement.get().isJsonArray()) {
+        final JsonArray seasonNodes = optionalNodesElement.get().getAsJsonArray();
+        for (JsonElement element : seasonNodes) {
+          final JsonObject episodes = element.getAsJsonObject().getAsJsonObject("episodes");
+          final JsonArray episodeNodes = episodes.getAsJsonArray("nodes");
+          addFilms(films, episodeNodes);
+          films.setNextPage(parseNextPage(episodes.getAsJsonObject("pageInfo")));
+        }
+      }
+    } else if (data.has("metaCollectionContent")) {
+      final JsonObject metaCollectionContent = data.getAsJsonObject("metaCollectionContent");
+      final JsonArray collectionNodes = metaCollectionContent.getAsJsonArray("smartCollections");
+      addFilms(films, collectionNodes);
+      films.setNextPage(parseNextPage(metaCollectionContent.getAsJsonObject("pageInfo")));
+    } else {
+      LOG.error("ZdfTopicSeasonDeserializer: No valid entry nodes found");
+    }
+    return films;
+  }
+
+  private void addFilms(PagedElementListDTO<ZdfFilmDto> films, JsonArray episodeNodes) {
+    for (JsonElement episode : episodeNodes) {
+      films.addElements(deserializeMovie(episode));
+    }
+  }
+
+  private Optional<String> parseNextPage(JsonObject pageInfo) {
+    if (!pageInfo.isJsonNull()) {
+      final Optional<String> hasNextPage = JsonUtils.getAttributeAsString(pageInfo, "hasNextPage");
+      if (hasNextPage.isPresent() && hasNextPage.get().equals("true")) {
+        return JsonUtils.getAttributeAsString(pageInfo, "endCursor");
+      }
+    }
+    return Optional.empty();
+  }
+}
