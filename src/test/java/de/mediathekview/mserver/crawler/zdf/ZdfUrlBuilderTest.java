2f85891f709278aa0d769223d45741ae349677c2
pidoubleyou <22942659+pidoubleyou@users.noreply.github.com>
2026-07-06 23:02:53 +0200

    fix sha topic page extension

--- src/test/java/de/mediathekview/mserver/crawler/zdf/ZdfUrlBuilderTest.java
+++ src/test/java/de/mediathekview/mserver/crawler/zdf/ZdfUrlBuilderTest.java
@@ -20,13 +20,13 @@ class ZdfUrlBuilderTest {
 
   @Test
   void testBuildTopicSeasonUrl() {
-    assertEquals("https://api.zdf.de/graphql?operationName=seasonByCanonical&variables=%7B%22seasonIndex%22%3A18%2C%22episodesPageSize%22%3A24%2C%22canonical%22%3A%22soko-wismar-104%22%2C%22sortBy%22%3A%5B%7B%22field%22%3A%22EDITORIAL_DATE%22%2C%22direction%22%3A%22DESC%22%7D%5D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%229412a0f4ac55dc37d46975d461ec64bfd14380d815df843a1492348f77b5c99a%22%7D%7D",
+    assertEquals("https://api.zdf.de/graphql?operationName=seasonByCanonical&variables=%7B%22seasonIndex%22%3A18%2C%22episodesPageSize%22%3A24%2C%22canonical%22%3A%22soko-wismar-104%22%2C%22sortBy%22%3A%5B%7B%22field%22%3A%22EDITORIAL_DATE%22%2C%22direction%22%3A%22DESC%22%7D%5D%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2281237cafa2f0176d351b21bff20cdcb5e5755092a0bd42d7271018c5440a4493%22%7D%7D",
             ZdfUrlBuilder.buildTopicSeasonUrl(18, 24, "soko-wismar-104"));
   }
 
   @Test
   void testBuildTopicSeasonUrlWithCursor() {
-    assertEquals("https://api.zdf.de/graphql?operationName=seasonByCanonical&variables=%7B%22seasonIndex%22%3A18%2C%22episodesPageSize%22%3A24%2C%22canonical%22%3A%22soko-wismar-104%22%2C%22sortBy%22%3A%5B%7B%22field%22%3A%22EDITORIAL_DATE%22%2C%22direction%22%3A%22DESC%22%7D%5D%2C%22episodesAfter%22%3A%22WyJmZTlmZTc5ZDU1IiwiMjAyMy0xMi0wOFQxOTo0NjowNi4wOTIwMDArMDA6MDAiLCJwYWdlLXZpZGVvLWFyZF92aWRlb19hcmRfZFhKdU9tRnlaRHB3ZFdKc2FXTmhkR2x2YmpvMVlqSXhZV1E1TmpFeFpqVTRNalZqIl0%3D%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%229412a0f4ac55dc37d46975d461ec64bfd14380d815df843a1492348f77b5c99a%22%7D%7D",
+    assertEquals("https://api.zdf.de/graphql?operationName=seasonByCanonical&variables=%7B%22seasonIndex%22%3A18%2C%22episodesPageSize%22%3A24%2C%22canonical%22%3A%22soko-wismar-104%22%2C%22sortBy%22%3A%5B%7B%22field%22%3A%22EDITORIAL_DATE%22%2C%22direction%22%3A%22DESC%22%7D%5D%2C%22episodesAfter%22%3A%22WyJmZTlmZTc5ZDU1IiwiMjAyMy0xMi0wOFQxOTo0NjowNi4wOTIwMDArMDA6MDAiLCJwYWdlLXZpZGVvLWFyZF92aWRlb19hcmRfZFhKdU9tRnlaRHB3ZFdKc2FXTmhkR2x2YmpvMVlqSXhZV1E1TmpFeFpqVTRNalZqIl0%3D%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2281237cafa2f0176d351b21bff20cdcb5e5755092a0bd42d7271018c5440a4493%22%7D%7D",
             ZdfUrlBuilder.buildTopicSeasonUrl(18, 24, "soko-wismar-104", "WyJmZTlmZTc5ZDU1IiwiMjAyMy0xMi0wOFQxOTo0NjowNi4wOTIwMDArMDA6MDAiLCJwYWdlLXZpZGVvLWFyZF92aWRlb19hcmRfZFhKdU9tRnlaRHB3ZFdKc2FXTmhkR2x2YmpvMVlqSXhZV1E1TmpFeFpqVTRNalZqIl0="));
   }
 
